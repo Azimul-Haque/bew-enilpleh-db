064580cb1862e050b9381c22572b44bd2a026d6c
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 04:35:13 +0600

    Commit Sun 05/12/2024  4:35:13.26 27714

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -90,7 +90,7 @@ class APIController extends Controller
     {
         if($softtoken == env('SOFT_TOKEN'))
         {
-            $hospitals = Cache::remember('hospitals'.$hospital_type . $district_id, 30 * 24 * 60 * 60, function () use ($hospital_type, $district_id, $upazilla_id) {
+            $hospitals = Cache::remember('hospitals'.$hospital_type . $district_id . $upazilla_id, 30 * 24 * 60 * 60, function () use ($hospital_type, $district_id, $upazilla_id) {
                  $hospitals = Hospital::where('hospital_type', $hospital_type)
                              ->where('district_id', $district_id)
                              ->where('upazilla_id', $upazilla_id)
