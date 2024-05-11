41f5d3df2452abced29e7b4b8badd816b153be7a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 03:31:22 +0600

    Commit Sun 05/12/2024  3:31:22.24 8786

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -39,10 +39,6 @@
                   <th>নাম</th>
                   <th>লোকেশন</th>
                   <th>
-                    <div class="progress progress-xs">
-                      <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                    </div>
-                  </th>
                   <th><span class="badge bg-danger">55%</span></th>
                 </tr>
               </thead>
