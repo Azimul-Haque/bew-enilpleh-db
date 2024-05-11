4899e1c735bd20c5ccfe417a5a236972437d9e9f
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 03:51:27 +0600

    Commit Sun 05/12/2024  3:51:27.26 31154

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -90,7 +90,7 @@
                                     </div>
                                     <div class="input-group mb-3">
                                       <select name="upazilla" id="upazilla" class="form-control" required>
-                                          <option selected="" disabled="" value="{{ $hospital->upazilla_id }}">{{ $hospital->upazilla->name_bangla }}</option>
+                                          <option selected="" value="{{ $hospital->upazilla_id }}">{{ $hospital->upazilla->name_bangla }}</option>
                                       </select>
                                       <div class="input-group-append">
                                           <div class="input-group-text"><span class="fas fa-map-marked-alt"></span></div>
