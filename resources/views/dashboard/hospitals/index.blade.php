9fefbddecc5c2856e56df0a20aaf74f244566bcb
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 03:07:06 +0600

    Commit Sun 05/12/2024  3:07:06.25 21646

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -332,7 +332,6 @@
                         <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
                     </div>
                   </div>
-
                   <div class="input-group mb-3">
                     <select name="upazilla" id="upazilla" class="form-control" required>
                         <option selected="" disabled="" value="">উপজেলা নির্বাচন করুন</option>
@@ -341,7 +340,6 @@
                         <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
                     </div>
                   </div>
-
                   <div class="input-group mb-3">
                       <input type="text"
                              name="name"
@@ -351,6 +349,14 @@
                       <div class="input-group-append">
                           <div class="input-group-text"><span class="fas fa-user"></span></div>
                       </div>
+                  </div>
+                  <div class="input-group mb-3">
+                    <select name="upazilla" id="upazilla" class="form-control" required>
+                        <option selected="" disabled="" value="">উপজেলা নির্বাচন করুন</option>
+                    </select>
+                    <div class="input-group-append">
+                        <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
+                    </div>
                   </div>
 
                   <div class="input-group mb-3">
