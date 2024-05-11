9ccd499ae7bc16ec4e2c86d08aa8662d15d9e94b
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 01:44:01 +0600

    Commit Sun 05/12/2024  1:44:01.24 30896

--- database/migrations/2024_05_12_013401_create_upazillas_table.php
+++ database/migrations/2024_05_12_013401_create_upazillas_table.php
@@ -15,6 +15,7 @@ class CreateUpazillasTable extends Migration
     {
         Schema::create('upazillas', function (Blueprint $table) {
             $table->increments('id');
+            $table->number('district_id');
             $table->string('name');
             $table->string('name_bangla');
         });
