999e0f91e0d70c010c71eb75601b5ed2d5206b59
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 02:55:31 +0600

    Commit Sun 05/12/2024  2:55:31.29 19748

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/testapi', 'APIController@test')->name('api.test');
 
-Route::post('/getupazilla/{district_id}', 'APIController@getUpazillasAPI')->name('api.getupazillas');
+Route::get('/getupazilla/{district_id}', 'APIController@getUpazillasAPI')->name('api.getupazillas');
 
 
 
