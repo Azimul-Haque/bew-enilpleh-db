ae3c85687d780d35b8be729c8223f71b5449cb87
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 03:35:14 +0600

    Commit Sun 05/12/2024  3:35:14.31 17843

--- app/District.php
+++ app/District.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class District extends Model
 {
     public $timestamps = false;
+
+    public function hospitals(){
+        return $this->hasMany('App\Blog');
+    }
 }
