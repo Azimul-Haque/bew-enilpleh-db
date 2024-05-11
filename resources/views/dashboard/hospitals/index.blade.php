dbce3dca5e1e75e92bb5ffdac0457fca2e842e62
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 02:45:55 +0600

    Commit Sun 05/12/2024  2:45:55.24 9852

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -323,7 +323,7 @@
 
                   <div class="input-group mb-3">
                     <select name="status" class="form-control" required>
-                        <option selected="" disabled="" value="">স্ট্যাটাস</option>
+                        <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
                         <option value="1" @if($course->status == 1) selected @endif>Active</option>
                         <option value="0" @if($course->status == 0) selected @endif>In-active</option>
                     </select>
@@ -331,7 +331,7 @@
                         <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
                     </div>
                   </div>
-                  
+
                   <div class="input-group mb-3">
                       <input type="text"
                              name="name"
