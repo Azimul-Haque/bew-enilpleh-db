f28ddfd8c1cf947b615e0f47f4602d7fc3a23cd0
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 03:17:44 +0600

    Commit Sun 05/12/2024  3:17:44.43 17415

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -492,15 +492,6 @@
           });
         });
 
-        $('#adduserrole').change(function () {
-            if($('#adduserrole').val() == 'accountant') {
-                $('#ifaccountant').hide();
-            } else {
-                $('#ifaccountant').show();
-            }
-        });
-
-
         $(document).on('click', '#search-button', function() {
           if($('#search-param').val() != '') {
             var urltocall = '{{ route('dashboard.hospitals') }}' +  '/' + $('#search-param').val();
