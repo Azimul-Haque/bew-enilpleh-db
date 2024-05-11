c545c7f5f75d4e8d0c0215dc1594534f14bd4038
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 03:08:20 +0600

    Commit Sun 05/12/2024  3:08:20.33 21462

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -353,9 +353,9 @@
                   <div class="input-group mb-3">
                     <select name="hospital_type" class="form-control" required>
                         <option selected="" disabled="" value="">হাসপাতালের ধরন</option>
-                        <option value="1">হাসপাতালের ধরন</option>
-                        <option value="2">হাসপাতালের ধরন</option>
-                        <option value="3">হাসপাতালের ধরন</option>
+                        <option value="1">মেডিকেল কলেজ ও হাসপাতাল</option>
+                        <option value="2">প্রাইভেট হাসপাতাল</option>
+                        <option value="3">স্বাস্থ্য কমপ্লেক্স</option>
                     </select>
                     <div class="input-group-append">
                         <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
