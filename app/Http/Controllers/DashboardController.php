bf0c30b38877f4d27cbfd7bdd4a0d65de74341af
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 02:40:50 +0600

    Commit Sun 05/12/2024  2:40:50.26 2515

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -129,7 +129,7 @@ class DashboardController extends Controller
     public function getHospitals()
     {
         $hospitalscount = User::count();
-        $hospitals = Hospital::->orderBy('id', 'desc')->paginate(10);
+        $hospitals = Hospital::orderBy('id', 'desc')->paginate(10);
         
         return view('dashboard.hospitals.index')
                             ->withHospitalscount($hospitalscount)
