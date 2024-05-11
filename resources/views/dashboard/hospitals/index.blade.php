adf0e055e5014e89ed4a76185ec8dda963065826
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 02:47:02 +0600

    Commit Sun 05/12/2024  2:47:02.28 11649

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -325,7 +325,7 @@
                     <select name="status" class="form-control" required>
                         <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
                         @foreach($districts as $district)
-                          <option value="{{ $district->id }}" @endif>{{ $district->name_bangla }}</option>
+                          <option value="{{ $district->id }}">{{ $district->name_bangla }}</option>
                         @endforeach
                     </select>
                     <div class="input-group-append">
