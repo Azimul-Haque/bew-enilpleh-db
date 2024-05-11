4f8e24796131c830a60c3c91b86a3544291fdcf5
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 02:45:42 +0600

    Commit Sun 05/12/2024  2:45:42.29 15583

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -322,6 +322,17 @@
                   @csrf
 
                   <div class="input-group mb-3">
+                    <select name="status" class="form-control" required>
+                        <option selected="" disabled="" value="">স্ট্যাটাস</option>
+                        <option value="1" @if($course->status == 1) selected @endif>Active</option>
+                        <option value="0" @if($course->status == 0) selected @endif>In-active</option>
+                    </select>
+                    <div class="input-group-append">
+                        <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
+                    </div>
+                  </div>
+                  
+                  <div class="input-group mb-3">
                       <input type="text"
                              name="name"
                              class="form-control"
