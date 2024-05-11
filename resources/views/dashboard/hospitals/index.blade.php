7f3eb6daaaec26a8fa096c18c42cb8d28e4afbdb
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 03:11:38 +0600

    Commit Sun 05/12/2024  3:11:38.33 10575

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -337,7 +337,7 @@
                         <option selected="" disabled="" value="">উপজেলা নির্বাচন করুন</option>
                     </select>
                     <div class="input-group-append">
-                        <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
+                        <div class="input-group-text"><span class="fas fa-map-marked-alt"></span></div>
                     </div>
                   </div>
                   <div class="input-group mb-3">
