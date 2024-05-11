64c240c6ef01ef22d1c4adec3337d9eecf634f3c
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 02:56:29 +0600

    Commit Sun 05/12/2024  2:56:29.30 13703

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -483,7 +483,7 @@
           $('#upazilla').append('<option value="" selected disabled>উপজেলা লোড হচ্ছে...</option>');
 
           $.ajax({
-            url: "/getupazillas/api/"+$(this).val(), 
+            url: "/getupazillas/"+$(this).val(), 
             type: "GET",
             success: function(result){
               $('#upazilla')
