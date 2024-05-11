568fd9e8a7f150e87c17ad2263446c2292250717
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-12 04:35:57 +0600

    Commit Sun 05/12/2024  4:35:57.29 2084

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -221,7 +221,7 @@ class DashboardController extends Controller
         $hospital->save();
 
         Cache::forget('hospitals'. $request->hospital_type . $request->district_id);
-
+        Cache::forget('hospitals'. $request->hospital_type . $request->district_id . $request->upazilla_id);
         Session::flash('success', 'Hospital updated successfully!');
         return redirect()->back();
     }
