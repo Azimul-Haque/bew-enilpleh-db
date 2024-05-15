4438d1c1a3970f43ca0f2bd800036d3fefd5b7e8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-16 02:52:28 +0600

    Commit Thu 05/16/2024  2:52:28.30 8584

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -50,6 +50,9 @@ class APIController extends Controller
               });
               // return $districts;
               return view('dashboard.hospitals.print')->withDistricts($districts);
+
+              return response()->json($districts, 200, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
             }
             catch (\Exception $e) {
               return $e->getMessage();
