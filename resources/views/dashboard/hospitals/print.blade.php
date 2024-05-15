def4de9b3d1c03e647b2e692d05e496a883f1ad8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-16 02:50:26 +0600

    Commit Thu 05/16/2024  2:50:26.33 19164

--- resources/views/dashboard/hospitals/print.blade.php
+++ resources/views/dashboard/hospitals/print.blade.php
@@ -1,11 +1,3 @@
-@extends('layouts.app')
-@section('title') জেলা তালিকা @endsection
-
-@section('third_party_stylesheets')
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap-datepicker.min.css') }}">
-@endsection
-
 @section('content')
   @section('page-header') জেলা @endsection
     <div class="container-fluid">
@@ -20,8 +12,4 @@
           <!-- /.card-body -->
         </div>
     </div>
-@endsection
-
-@section('third_party_scripts')
-    
 @endsection
