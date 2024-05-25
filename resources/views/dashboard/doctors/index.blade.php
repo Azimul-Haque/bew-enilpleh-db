168ca1a967a1e05865c30ee9f10d10ee725f155f
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:34:11 +0600

    Commit Sun 05/26/2024  3:34:11.24 18267

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -80,9 +80,9 @@
                                     <div class="input-group mb-3">
                                       <select name="district_id" id="district" class="form-control district" required>
                                           <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
-                                          @foreach($districts as $district)
+                                          {{-- @foreach($districts as $district)
                                             <option value="{{ $district->id }}" @if($district->id == $doctor->district_id) selected @endif>{{ $district->name_bangla }}</option>
-                                          @endforeach
+                                          @endforeach --}}
                                       </select>
                                       <div class="input-group-append">
                                           <div class="input-group-text"><span class="fas fa-map"></span></div>
@@ -234,9 +234,9 @@
                   <div class="input-group mb-3">
                     <select name="district_id" id="district" class="form-control district" required>
                         <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
-                        @foreach($districts as $district)
+                        {{-- @foreach($districts as $district)
                           <option value="{{ $district->id }}">{{ $district->name_bangla }}</option>
-                        @endforeach
+                        @endforeach --}}
                     </select>
                     <div class="input-group-append">
                         <div class="input-group-text"><span class="fas fa-map"></span></div>
