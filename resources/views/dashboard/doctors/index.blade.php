0fc59fa0669b676273a2dce60c13deb926d07162
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:49:57 +0600

    Commit Sun 05/26/2024  3:49:57.25 1583

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -310,7 +310,6 @@
 @section('third_party_scripts')
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript">
-
         $('.multiple-select').select2({
           // theme: 'bootstrap4',
         });
