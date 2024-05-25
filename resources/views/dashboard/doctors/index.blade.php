28544efd83fd2a8cada75d01ce74c0c7075ad1c0
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 04:22:34 +0600

    Commit Sun 05/26/2024  4:22:34.30 9312

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -240,23 +240,29 @@
               
                   @csrf
 
-                  <div class="input-group mb-3">
-                    <select name="district_id" id="district" class="form-control district" required>
-                        <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
-                        @foreach($districts as $district)
-                          <option value="{{ $district->id }}">{{ $district->name_bangla }}</option>
-                        @endforeach
-                    </select>
-                    <div class="input-group-append">
-                        <div class="input-group-text"><span class="fas fa-map"></span></div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="input-group mb-3">
+                        <select name="district_id" id="district" class="form-control district" required>
+                            <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
+                            @foreach($districts as $district)
+                              <option value="{{ $district->id }}">{{ $district->name_bangla }}</option>
+                            @endforeach
+                        </select>
+                        <div class="input-group-append">
+                            <div class="input-group-text"><span class="fas fa-map"></span></div>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div class="input-group mb-3">
-                    <select name="upazilla_id" id="upazilla" class="form-control upazilla" required>
-                        <option selected="" disabled="" value="">উপজেলা নির্বাচন করুন</option>
-                    </select>
-                    <div class="input-group-append">
-                        <div class="input-group-text"><span class="fas fa-map-marked-alt"></span></div>
+                    <div class="col-md-6">
+                      <div class="input-group mb-3">
+                        <select name="upazilla_id" id="upazilla" class="form-control upazilla" required>
+                            <option selected="" disabled="" value="">উপজেলা নির্বাচন করুন</option>
+                        </select>
+                        <div class="input-group-append">
+                            <div class="input-group-text"><span class="fas fa-map-marked-alt"></span></div>
+                        </div>
+                      </div>
                     </div>
                   </div>
 
