55c8dddf0ca56b3cdb81a5206969300affb6b143
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 02:42:58 +0600

    Commit Sun 05/26/2024  2:42:58.28 26671

--- database/migrations/2024_05_26_023959_create_doctors_table.php
+++ database/migrations/2024_05_26_023959_create_doctors_table.php
@@ -16,9 +16,9 @@ class CreateDoctorsTable extends Migration
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('degree');
             $table->string('serial');
             $table->string('helpline');
-            $table->string('location');
             $table->timestamps();
         });
     }
