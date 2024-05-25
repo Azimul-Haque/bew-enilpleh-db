b5a65fdfb2f3ec530746ae793c053c1b6ecc5d7d
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 02:53:10 +0600

    Commit Sun 05/26/2024  2:53:10.31 326

--- database/migrations/2024_05_26_024956_create_medicalsymptoms_table.php
+++ database/migrations/2024_05_26_024956_create_medicalsymptoms_table.php
@@ -15,7 +15,8 @@ class CreateMedicalsymptomsTable extends Migration
     {
         Schema::create('medicalsymptoms', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->string('thumbnail')->nullable();
         });
     }
 
