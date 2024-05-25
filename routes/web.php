99f5f97cc61440568fec2c226ad0578a9897cc4d
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:31:51 +0600

    Commit Sun 05/26/2024  3:31:51.32 14710

--- routes/web.php
+++ routes/web.php
@@ -37,11 +37,11 @@ Route::post('/dashboard/hospitals/{id}/update', 'DashboardController@updateHospi
 Route::get('/dashboard/hospitals/{id}/delete', 'DashboardController@deleteHospital')->name('dashboard.hospitals.delete');
 
 // Doctors
-Route::get('/dashboard/doctors', 'DashboardController@getHospitals')->name('dashboard.hospitals');
-Route::get('/dashboard/doctors/{search}', 'DashboardController@getHospitalsSearch')->name('dashboard.hospitals.search');
-Route::post('/dashboard/doctors/store', 'DashboardController@storeHospital')->name('dashboard.hospitals.store');
-Route::post('/dashboard/doctors/{id}/update', 'DashboardController@updateHospital')->name('dashboard.hospitals.update');
-Route::get('/dashboard/doctors/{id}/delete', 'DashboardController@deleteHospital')->name('dashboard.hospitals.delete');
+Route::get('/dashboard/doctors', 'DashboardController@getHospitals')->name('dashboard.doctors');
+Route::get('/dashboard/doctors/{search}', 'DashboardController@getHospitalsSearch')->name('dashboard.doctors.search');
+Route::post('/dashboard/doctors/store', 'DashboardController@storeHospital')->name('dashboard.doctors.store');
+Route::post('/dashboard/doctors/{id}/update', 'DashboardController@updateHospital')->name('dashboard.doctors.update');
+Route::get('/dashboard/doctors/{id}/delete', 'DashboardController@deleteHospital')->name('dashboard.doctors.delete');
 
 
 
