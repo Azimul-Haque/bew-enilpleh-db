118da95ab78c9627f8fc8f4189949487087add98
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:40:47 +0600

    Commit Sun 05/26/2024  3:40:47.26 23443

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -275,7 +275,7 @@
                   </div>
                   
                   <div class="input-group mb-3">
-                    <select name="medicaldepartment" class="form-control select2" multiple required>
+                    <select name="medicaldepartment" class="form-control multiple-select" multiple="multiple" required>
                         <option selected="" disabled="" value="">বিভাগ</option>
                         @foreach($medicaldepartments as $medicaldepartment)
                           <option value="{{ $medicaldepartment->id }}">{{ $medicaldepartment->name }}</option>
