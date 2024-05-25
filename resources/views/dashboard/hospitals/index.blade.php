7b471f0a6083e80455fc1eaf497cdbfefda4b7a6
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:42:20 +0600

    Commit Sun 05/26/2024  3:42:20.45 1357

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -2,8 +2,8 @@
 @section('title') ড্যাশবোর্ড | হাসপাতাল তালিকা @endsection
 
 @section('third_party_stylesheets')
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap-datepicker.min.css') }}">
+   {{--  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap-datepicker.min.css') }}"> --}}
 @endsection
 
 @section('content')
