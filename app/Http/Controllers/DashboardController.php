c4e2f6c783c9f1e209d2ad716e7b1735d43b2f5f
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:23:07 +0600

    Commit Sun 05/26/2024  3:23:07.26 10954

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -256,6 +256,7 @@ class DashboardController extends Controller
                             ->withDoctorscount($doctorscount)
                             ->withDoctors($doctors)
                             ->withMedicaldepartments($medicaldepartments)
+                            ->withmedicalsymptom($medicalsymptom)
     }
 
 
