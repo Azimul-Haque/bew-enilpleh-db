88b9bffd74b36a153ba3474f9b7b0cec6f344373
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:08:12 +0600

    Commit Sun 05/26/2024  3:08:12.27 29395

--- app/Doctor.php
+++ app/Doctor.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    public function doctormedicaldepartments(){
+    public function doctormedicaldepartment(){
         return $this->hasMany('App\Doctormedicaldepartment');
     }
 }
