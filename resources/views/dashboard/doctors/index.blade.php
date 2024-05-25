41a7e2904fa7594f9ba0e54dfb6fa2fd7904d283
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:56:24 +0600

    Commit Sun 05/26/2024  3:56:24.33 3067

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -301,7 +301,7 @@
                   </div>
                   
                   <div style="margin-bottom: 15px;">
-                    <select name="hospitals[]" class="form-control multiple-select" multiple="multiple" data-placeholder="হাসপাতাল (প্রয়োজনে একাধিক সিলেক্ট করা যাবে)" required>
+                    <select name="hospitals[]" class="form-control multiple-select" multiple="multiple" data-placeholder="ডাক্তার যে হাসপাতালের সাথে সম্পৃক্ত হাসপাতাল (প্রয়োজনে একাধিক সিলেক্ট করা যাবে)" required>
                         
                         @foreach($medicalsymptoms as $medicalsymptom)
                           <option value="{{ $medicalsymptom->id }}">{{ $medicalsymptom->name }}</option>
