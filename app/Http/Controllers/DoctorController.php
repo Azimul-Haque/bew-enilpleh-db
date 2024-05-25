d0db11a0ac7444866a90b23b71c3a0a51f0b4d57
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 04:39:13 +0600

    Commit Sun 05/26/2024  4:39:13.29 2619

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -4,6 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+use App\District;
+use App\Upazilla;
+use App\Hospital;
+use App\Doctor;
+use App\Doctorimage;
+use App\Doctorhospital;
+use App\Medicaldepartment;
+use App\Medicalsymptom;
+use App\Doctormedicaldepartment;
+use App\Doctormedicalsymptom;
+
+use Carbon\Carbon;
+use DB;
+use Hash;
+use Auth;
+use Image;
+use File;
+use Session;
+use Artisan;
+// use Redirect;
+use OneSignal;
+use Purifier;
+use Cache;
+
 class DoctorController extends Controller
 {
     public function index()
