66467d958a988a35910f012c372662bc2bc3cf1e
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 02:53:48 +0600

    Commit Sun 05/26/2024  2:53:48.25 13616

--- database/migrations/2024_05_26_025006_create_doctormedicalsymptoms_table.php
+++ database/migrations/2024_05_26_025006_create_doctormedicalsymptoms_table.php
@@ -16,7 +16,7 @@ class CreateDoctormedicalsymptomsTable extends Migration
         Schema::create('doctormedicalsymptoms', function (Blueprint $table) {
             $table->id();
             $table->foreignId('doctor_id')->unsigned();
-            $table->foreignId('medicaldepartment_id')->unsigned();
+            $table->foreignId('medicalsymptoms_id')->unsigned();
             $table->timestamps();
         });
     }
