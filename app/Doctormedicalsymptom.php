2a7ce16e1635b312663ca03c81fe74c56dbe0f6a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:11:01 +0600

    Commit Sun 05/26/2024  3:11:01.28 23155

--- app/Doctormedicalsymptom.php
+++ app/Doctormedicalsymptom.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctormedicalsymptom extends Model
 {
-    //
+    public function doctor(){
+        return $this->belongsTo('App\Doctor');
+    }
+
+    public function medicaldepartment(){
+        return $this->belongsTo('App\Medicaldepartment');
+    }
 }
