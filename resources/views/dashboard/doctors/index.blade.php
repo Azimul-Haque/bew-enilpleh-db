b7b8ad23bf7c938d53945d21d54a8a3e1c87164c
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 04:21:14 +0600

    Commit Sun 05/26/2024  4:21:14.25 3922

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -434,12 +434,12 @@
         $("#image").change(function(){
             readURL(this);
             var filesize = parseInt((this.files[0].size)/1024);
-            if(filesize > 1000) {
+            if(filesize > 2000) {
               $("#image").val('');
               // toastr.warning('File size is: '+filesize+' Kb. try uploading less than 300Kb', 'WARNING').css('width', '400px;');
               Toast.fire({
                   icon: 'warning',
-                  title: 'File size is: '+filesize+' Kb. try uploading less than 1MB'
+                  title: 'File size is: '+filesize+' Kb. try uploading less than 2MB'
               })
               setTimeout(function() {
               $("#img-upload").attr('src', '{{ asset('images/placeholder.png') }}');
