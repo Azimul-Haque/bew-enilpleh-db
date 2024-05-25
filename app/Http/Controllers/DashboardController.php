96f62bce1ed8366f623a3e709e18511a41d83431
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 04:03:09 +0600

    Commit Sun 05/26/2024  4:03:09.28 15348

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -149,16 +149,16 @@ class DashboardController extends Controller
     public function getHospitalsSearch($search)
     {
         $hospitalscount = Hospital::where('name', 'LIKE', "%$search%")
-                              ->orWhere('telephone', 'LIKE', "%$search%")
-                              ->orWhere('mobile', 'LIKE', "%$search%")
-                              ->orWhereHas('district', function ($query) use ($search){
-                                  $query->where('name', 'like', '%'.$search.'%');
-                                  $query->orWhere('name_bangla', 'like', '%'.$search.'%');
-                              })->orWhereHas('upazilla', function ($query) use ($search){
-                                  $query->where('name', 'like', '%'.$search.'%');
-                                  $query->orWhere('name_bangla', 'like', '%'.$search.'%');
-                              })
-                              ->orderBy('id', 'desc')
+                                  ->orWhere('telephone', 'LIKE', "%$search%")
+                                  ->orWhere('mobile', 'LIKE', "%$search%")
+                                  ->orWhereHas('district', function ($query) use ($search){
+                                      $query->where('name', 'like', '%'.$search.'%');
+                                      $query->orWhere('name_bangla', 'like', '%'.$search.'%');
+                                  })->orWhereHas('upazilla', function ($query) use ($search){
+                                      $query->where('name', 'like', '%'.$search.'%');
+                                      $query->orWhere('name_bangla', 'like', '%'.$search.'%');
+                                  })
+                                  ->orderBy('id', 'desc')
                                   ->count();
         $hospitals = Hospital::where('name', 'LIKE', "%$search%")
                               ->orWhere('telephone', 'LIKE', "%$search%")
