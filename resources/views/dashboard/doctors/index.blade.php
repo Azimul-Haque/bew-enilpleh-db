2985fa1acab0ace7840277bcd8dedb84fe083ab4
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 04:14:20 +0600

    Commit Sun 05/26/2024  4:14:20.34 26592

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -196,7 +196,7 @@
                               </div>
                               <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                                <a href="{{ route('dashboard.hospitals.delete', $doctor->id) }}" class="btn btn-danger">ডিলেট করুন</a>
+                                <a href="{{ route('dashboard.doctors.delete', $doctor->id) }}" class="btn btn-danger">ডিলেট করুন</a>
                               </div>
                             </div>
                           </div>
