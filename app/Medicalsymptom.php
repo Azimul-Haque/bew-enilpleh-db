acc64ea738f90b24af1a9ae93ab9c9657c8d896a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:13:51 +0600

    Commit Sun 05/26/2024  3:13:51.33 15524

--- app/Medicalsymptom.php
+++ app/Medicalsymptom.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Medicalsymptom extends Model
 {
     public $timestamps = false;
+
+    public function doctormedicaldepartment(){
+        return $this->hasOne('App\Doctormedicaldepartment');
+    }
 }
