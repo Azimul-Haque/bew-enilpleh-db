33053004ad09b0da6010921703fd2d3d9e166f8b
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 04:08:08 +0600

    Commit Sun 05/26/2024  4:08:08.30 24110

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -244,7 +244,7 @@
                     <select name="district_id" id="district" class="form-control district" required>
                         <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
                         @foreach($districts as $district)
-                          <option value="{{ $district->id }}" @if($district->id == $hospital->district_id) selected @endif>{{ $district->name_bangla }}</option>
+                          <option value="{{ $district->id }}">{{ $district->name_bangla }}</option>
                         @endforeach
                     </select>
                     <div class="input-group-append">
@@ -253,7 +253,7 @@
                   </div>
                   <div class="input-group mb-3">
                     <select name="upazilla_id" id="upazilla" class="form-control upazilla" required>
-                        <option selected="" value="{{ $hospital->upazilla_id }}">{{ $hospital->upazilla->name_bangla }}</option>
+                        <option selected="" disabled="" value="">উপজেলা নির্বাচন করুন</option>
                     </select>
                     <div class="input-group-append">
                         <div class="input-group-text"><span class="fas fa-map-marked-alt"></span></div>
