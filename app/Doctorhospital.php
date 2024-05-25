b377b6e788ef10190c9a40b4b45e785f9824cf21
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:16:01 +0600

    Commit Sun 05/26/2024  3:16:01.26 13656

--- app/Doctorhospital.php
+++ app/Doctorhospital.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctorhospital extends Model
 {
-    //
+    public function doctor(){
+        return $this->belongsTo('App\Doctor');
+    }
+
+    public function medicaldepartment(){
+        return $this->belongsTo('App\Medicaldepartment');
+    }
 }
