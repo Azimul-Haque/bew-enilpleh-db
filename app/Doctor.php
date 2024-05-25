129a9ff98db520629ad35a76a9be9672e4950283
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-26 03:11:31 +0600

    Commit Sun 05/26/2024  3:11:31.25 13873

--- app/Doctor.php
+++ app/Doctor.php
@@ -9,4 +9,8 @@ class Doctor extends Model
     public function doctormedicaldepartments(){
         return $this->hasMany('App\Doctormedicaldepartment');
     }
+
+    public function doctormedicalsymptoms(){
+        return $this->hasMany('App\Doctormedicalsymptom');
+    }
 }
