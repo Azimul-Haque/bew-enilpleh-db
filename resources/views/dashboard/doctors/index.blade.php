b689f118f70ddbdc8c8ae0ee5a60a733d0370866
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-27 00:22:14 +0600

    Commit Mon 05/27/2024  0:22:14.25 13980

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -331,7 +331,7 @@
                   </div> 
                   
                   <div style="margin-bottom: 15px;">
-                    <select name="medicalsymtoms[]" class="form-control multiple-select" multiple="multiple" data-placeholder="লক্ষণ (প্রয়োজনে একাধিক সিলেক্ট করা যাবে)" required>
+                    <select name="medicalsymptoms[]" class="form-control multiple-select" multiple="multiple" data-placeholder="লক্ষণ (প্রয়োজনে একাধিক সিলেক্ট করা যাবে)" required>
                         
                         @foreach($medicalsymptoms as $medicalsymptom)
                           <option value="{{ $medicalsymptom->id }}">{{ $medicalsymptom->name }}</option>
