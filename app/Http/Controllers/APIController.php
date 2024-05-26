7de5f56cdc1f153cb8bf63932a56745b8c2875f1
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-27 04:01:29 +0600

    Commit Mon 05/27/2024  4:01:29.24 26250

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -238,7 +238,7 @@ class APIController extends Controller
             // dd($courses);
             return response()->json([
                 'success' => true,
-                'doctors' => $doctorstoreturn,
+                'doctors' => $doctors,
             ]);
         } else {
             return response()->json([
