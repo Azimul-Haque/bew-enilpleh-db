2d797ae8a5ac0de5a7904cc69564e21e04f4f7e0
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-27 04:10:30 +0600

    Commit Mon 05/27/2024  4:10:30.27 27743

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -212,6 +212,7 @@ class APIController extends Controller
                         $doctormedicaldepartment->serial = $doctormedicaldepartment->doctor->serial;
                         $doctormedicaldepartment->helpline = $doctormedicaldepartment->doctor->helpline;
                         $doctormedicaldepartment->image = $doctormedicaldepartment->doctor->doctorimage ? $doctormedicaldepartment->doctor->doctorimage->image : '';
+                        $doctormedicaldepartment->makeHidden('medicaldepartment_id', 'doctor_id', 'created_at', 'updated_at');
                         $doctormedicaldepartment->doctor->makeHidden('medicaldepartment_id', 'doctor_id', 'created_at', 'updated_at');
                         $doctorstoreturn->push($doctormedicaldepartment);
                         // dd($doctorstoreturn);
