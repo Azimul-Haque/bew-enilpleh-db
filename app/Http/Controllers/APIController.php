2bf2a5f4b408f15647d62187f4623604842db4ef
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-27 03:38:37 +0600

    Commit Mon 05/27/2024  3:38:37.28 32509

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -198,17 +198,17 @@ class APIController extends Controller
         {
             if($datatype == 'departmentwise') {
                 $doctors = Cache::remember('doctors'.$medicalitemid . $datatype . $district_id, 30 * 24 * 60 * 60, function () use ($medicalitemid, $datatype, $district_id) {
-                     $doctors = Doctor::where('hospital_type', $hospital_type)
-                                 ->where('district_id', $district_id)
-                                 ->orderBy('id', 'desc')
-                                 ->get();
-                                 // dd($doctors);
-                     foreach($doctors as $hospital) {
-                         $hospital->district = $hospital->district->name_bangla;
-                         $hospital->upazilla = $hospital->upazilla->name_bangla;
-                         $hospital->makeHidden('district', 'upazilla');
-                     }
-                     return $doctors;
+                    $doctors = Doctor::where('hospital_type', $hospital_type)
+                        ->where('district_id', $district_id)
+                        ->orderBy('id', 'desc')
+                        ->get();
+                    // dd($doctors);
+                    foreach($doctors as $hospital) {
+                        $hospital->district = $hospital->district->name_bangla;
+                        $hospital->upazilla = $hospital->upazilla->name_bangla;
+                        $hospital->makeHidden('district', 'upazilla');
+                    }
+                    return $doctors;
                 });
             } else { // symptomwise
 
