9fd726ab7b9967b0ab75a86c7bac5d3c323a59da
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-27 03:33:30 +0600

    Commit Mon 05/27/2024  3:33:30.35 13063

--- routes/api.php
+++ routes/api.php
@@ -26,8 +26,8 @@ Route::get('/gethospitals/{softtoken}/{hospital_type}/{district_id}', 'APIContro
 Route::get('/gethospitals/{softtoken}/{hospital_type}/{district_id}/{upazilla_id}', 'APIController@getHospitalsUpazilla')->name('api.gethospitalsupazilla');
 Route::get('/getmedicaldepartments/{softtoken}', 'APIController@getMedicalDepartments')->name('api.getmedicaldepartments');
 Route::get('/getmedicalsymptoms/{softtoken}', 'APIController@getMedicalSymptoms')->name('api.getmedicalsymptoms');
-Route::get('/getdoctors/{softtoken}/{medicalitemid}/datatype/{district_id}', 'APIController@getHospitalsDistrict')->name('api.gethospitalsdistrict');
-Route::get('/getdoctors/{softtoken}/{medicalitemid}/datatype/{district_id}/{upazilla_id}', 'APIController@getHospitalsUpazilla')->name('api.gethospitalsupazilla');
+Route::get('/getdoctors/{softtoken}/{medicalitemid}/datatype/{district_id}', 'APIController@getDoctorsDistrict')->name('api.gethospitalsdistrict');
+Route::get('/getdoctors/{softtoken}/{medicalitemid}/datatype/{district_id}/{upazilla_id}', 'APIController@getDoctorsUpazilla')->name('api.gethospitalsupazilla');
 
 
 Route::post('/generateotp', 'APIController@generateOTP')->name('api.generateotp');
