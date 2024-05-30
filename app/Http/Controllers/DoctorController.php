d89d99e7e6d268271d60e771178308e61e28a8db
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-31 03:09:36 +0600

    Commit Fri 05/31/2024  3:09:36.25 22477

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -131,7 +131,7 @@ class DoctorController extends Controller
                 $doctormedicaldepartment->medicaldepartment_id = $medicaldepartment_id;
                 $doctormedicaldepartment->save();
 
-                Cache::forget('doctors'. $$doctor->id);
+                Cache::forget('doctors'. $$medicaldepartment_id);
             }            
         }
 
@@ -167,7 +167,7 @@ class DoctorController extends Controller
 
         Cache::forget('doctors'. $request->district_id);
         Cache::forget('doctors'. $request->district_id . $request->upazilla_id);
-        Cache::forget('hospitaldoctors'. $$doctor->id);
+        Cache::forget('hospitaldoctors'. $doctor->id);
         Session::flash('success', 'Doctors added successfully!');
         return redirect()->route('dashboard.doctors');
     }
