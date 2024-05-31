5803f78241979cdb2563a4e5dcc10f8875ec0ce8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-01 03:51:08 +0600

    Commit Sat 06/01/2024  3:51:08.33 25797

--- resources/views/dashboard/eshebas/index.blade.php
+++ resources/views/dashboard/eshebas/index.blade.php
@@ -229,7 +229,8 @@
                   </div>
                   <div class="form-group ">
                       <label for="image">ছবি (প্রয়োজনে, ৩০০ x ৩০০ সাইজের, ২ মেগাবাইটের মধ্যে)</label><br/>
-                      <input type="file" id="image" name="image" accept="image/*">
+                      <input type="file"  name="image" accept="image/*"> 
+                      {{-- id="image" --}}
                   </div>
                   <center>
                       <img src="{{ asset('images/placeholder.png')}}" id='img-upload' style="width: 250px; height: auto;" class="img-responsive" />
