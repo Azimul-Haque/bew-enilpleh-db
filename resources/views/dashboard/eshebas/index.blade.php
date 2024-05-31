59bc509086d893c287413cd182ec0f914c9a2e57
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-01 03:09:03 +0600

    Commit Sat 06/01/2024  3:09:03.35 8824

--- resources/views/dashboard/eshebas/index.blade.php
+++ resources/views/dashboard/eshebas/index.blade.php
@@ -76,25 +76,6 @@
                                     @csrf
 
                                     <div class="input-group mb-3">
-                                      <select name="district_id" id="district" class="form-control district" required>
-                                          <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
-                                          @foreach($districts as $district)
-                                            <option value="{{ $district->id }}" @if($district->id == $esheba->district_id) selected @endif>{{ $district->name_bangla }}</option>
-                                          @endforeach
-                                      </select>
-                                      <div class="input-group-append">
-                                          <div class="input-group-text"><span class="fas fa-map"></span></div>
-                                      </div>
-                                    </div>
-                                    <div class="input-group mb-3">
-                                      <select name="upazilla_id" id="upazilla" class="form-control upazilla" required>
-                                          <option selected="" value="{{ $esheba->upazilla_id }}">{{ $esheba->upazilla->name_bangla }}</option>
-                                      </select>
-                                      <div class="input-group-append">
-                                          <div class="input-group-text"><span class="fas fa-map-marked-alt"></span></div>
-                                      </div>
-                                    </div>
-                                    <div class="input-group mb-3">
                                         <input type="text"
                                                name="name"
                                                class="form-control"
