ef3627723715ebe138b6f510aa4b5d5589e1800f
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-01 01:42:20 +0600

    Commit Sat 06/01/2024  1:42:20.22 24481

--- app/Http/Controllers/AmbulanceController.php
+++ app/Http/Controllers/AmbulanceController.php
@@ -130,6 +130,7 @@ class AmbulanceController extends Controller
         // image upload
         if($request->hasFile('image')) {
             $image_path = public_path('images/ambulances/'. $ambulance->ambulanceimage->image);
+            dd($image_path);
             if(File::exists($image_path)) {
                 File::delete($image_path);
             }
