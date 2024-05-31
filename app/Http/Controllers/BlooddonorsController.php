78b5504e1e737c371448efe24330d958408199ca
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-31 21:02:02 +0600

    Commit Fri 05/31/2024 21:02:02.37 2944

--- app/Http/Controllers/BlooddonorsController.php
+++ app/Http/Controllers/BlooddonorsController.php
@@ -95,8 +95,8 @@ class BlooddonorsController extends Controller
         
         // Cache::forget('doctors'. $request->district_id);
         // Cache::forget('doctors'. $request->district_id . $request->upazilla_id);
-        Session::flash('success', 'Doctors added successfully!');
-        return redirect()->route('dashboard.doctors');
+        Session::flash('success', 'Blood Donor added successfully!');
+        return redirect()->route('dashboard.blooddonors');
     }
 
 }
