a8cba61bf9e269d268febd8dc4128c09f28bb1eb
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-01 02:45:57 +0600

    Commit Sat 06/01/2024  2:45:57.26 8772

--- app/Esheba.php
+++ app/Esheba.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Esheba extends Model
 {
-    //
+    public function district(){
+        return $this->belongsTo('App\District');
+    }
+
+    public function upazilla(){
+        return $this->belongsTo('App\Upazilla');
+    }
+
+    public function ambulanceimage(){
+        return $this->hasOne('App\Ambulanceimage');
+    }
 }
