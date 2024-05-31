00d5a2809144045b508db8a79129d885647df5ae
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-31 22:56:01 +0600

    Commit Fri 05/31/2024 22:56:01.37 9973

--- app/Ambulanceimage.php
+++ app/Ambulanceimage.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Ambulanceimage extends Model
 {
     public $timestamps = false;
+
+    public function ambulance(){
+        return $this->belongsTo('App\Ambulance');
+    }
 }
