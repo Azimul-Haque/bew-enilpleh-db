2a51534df582720a9559e85fe3324807cddb42ed
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-31 21:13:05 +0600

    Commit Fri 05/31/2024 21:13:05.38 27969

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -348,7 +348,7 @@ class APIController extends Controller
         }
     }
 
-    public function getHospitalsDistrict($softtoken, $hospital_type, $district_id)
+    public function getBloodDonorsDistrict($softtoken, $hospital_type, $district_id)
     {
         if($softtoken == env('SOFT_TOKEN'))
         {
