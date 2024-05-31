2a9e208b9b531f7fbb468a9cbe0fa166d8af546c
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-31 20:49:45 +0600

    Commit Fri 05/31/2024 20:49:45.35 719

--- resources/views/dashboard/blooddonors/index.blade.php
+++ resources/views/dashboard/blooddonors/index.blade.php
@@ -46,7 +46,6 @@
                   <tr>
                     <td>
                       {{ $blooddonor->name }}
-                      <small class="text-black-50"><i class="fas fa-phone"></i> {{ $blooddonor->telephone }}</small>
                       <small class="text-black-50"><i class="fas fa-mobile"></i> {{ $blooddonor->mobile }}</small><br/>
                       <span class="badge bg-success">{{ hospital_type($blooddonor->hospital_type) }}</span>
                     </td>
