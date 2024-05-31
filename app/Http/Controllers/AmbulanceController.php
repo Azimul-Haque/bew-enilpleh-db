8ca7c48e7473e3d3ae1ed389c27ad33248cc6ff0
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-31 22:57:00 +0600

    Commit Fri 05/31/2024 22:57:00.34 31256

--- app/Http/Controllers/AmbulanceController.php
+++ app/Http/Controllers/AmbulanceController.php
@@ -28,4 +28,99 @@ class AmbulanceController extends Controller
         parent::__construct();
         $this->middleware(['admin'])->only('index', 'indexSearch');
     }
+
+    public function index()
+    {
+        $blooddonorscount = Blooddonor::count();
+        $blooddonors = Blooddonor::orderBy('id', 'desc')->paginate(10);
+
+        $districts = District::all();
+                
+        return view('dashboard.blooddonors.index')
+                            ->withBlooddonorscount($blooddonorscount)
+                            ->withBlooddonors($blooddonors)
+                            ->withDistricts($districts);
+    }
+
+    public function indexSearch($search)
+    {
+        $blooddonorscount = Blooddonor::where('name', 'LIKE', "%$search%")
+                                  ->orWhere('mobile', 'LIKE', "%$search%")
+                                  ->orWhereHas('district', function ($query) use ($search){
+                                      $query->where('name', 'like', '%'.$search.'%');
+                                      $query->orWhere('name_bangla', 'like', '%'.$search.'%');
+                                  })->orWhereHas('upazilla', function ($query) use ($search){
+                                      $query->where('name', 'like', '%'.$search.'%');
+                                      $query->orWhere('name_bangla', 'like', '%'.$search.'%');
+                                  })
+                                  ->count();
+        $blooddonors = Blooddonor::where('name', 'LIKE', "%$search%")
+                                  ->orWhere('mobile', 'LIKE', "%$search%")
+                                  ->orWhereHas('district', function ($query) use ($search){
+                                      $query->where('name', 'like', '%'.$search.'%');
+                                      $query->orWhere('name_bangla', 'like', '%'.$search.'%');
+                                  })->orWhereHas('upazilla', function ($query) use ($search){
+                                      $query->where('name', 'like', '%'.$search.'%');
+                                      $query->orWhere('name_bangla', 'like', '%'.$search.'%');
+                                  })
+                                  ->orderBy('id', 'desc')
+                                  ->paginate(10);
+
+        $districts = District::all();
+        
+        return view('dashboard.blooddonors.index')
+                            ->withBlooddonorscount($blooddonorscount)
+                            ->withBlooddonors($blooddonors)
+                            ->withDistricts($districts);
+    }
+
+    public function storeBloodDonor(Request $request)
+    {
+        $this->validate($request,array(
+            'district_id'            => 'required',
+            'upazilla_id'            => 'required',
+            'name'                => 'required|string|max:191',
+            'category'            => 'required',
+            'mobile'              => 'required|string|max:191',
+        ));
+
+        $blooddonor = new Blooddonor;
+        $blooddonor->district_id = $request->district_id;
+        $blooddonor->upazilla_id = $request->upazilla_id;
+        $blooddonor->name = $request->name;
+        $blooddonor->category = $request->category;
+        $blooddonor->mobile = $request->mobile;
+        $blooddonor->save();
+
+        
+        Cache::forget('blooddonors'. $request->category . $request->district_id);
+        Cache::forget('blooddonors'. $request->category . $request->district_id. $request->upazilla_id);
+        Session::flash('success', 'Blood Donor added successfully!');
+        return redirect()->route('dashboard.blooddonors');
+    }
+
+    public function updateBloodDonor(Request $request, $id)
+    {
+        $this->validate($request,array(
+            'district_id'         => 'required',
+            'upazilla_id'         => 'required',
+            'name'                => 'required|string|max:191',
+            'category'            => 'required',
+            'mobile'              => 'required|string|max:191',
+        ));
+
+        $blooddonor = Blooddonor::find($id);;
+        $blooddonor->district_id = $request->district_id;
+        $blooddonor->upazilla_id = $request->upazilla_id;
+        $blooddonor->name = $request->name;
+        $blooddonor->category = $request->category;
+        $blooddonor->mobile = $request->mobile;
+        $blooddonor->save();
+
+        
+        Cache::forget('blooddonors'. $request->category . $request->district_id);
+        Cache::forget('blooddonors'. $request->category . $request->district_id. $request->upazilla_id);
+        Session::flash('success', 'Blood Donor updated successfully!');
+        return redirect()->route('dashboard.blooddonors');
+    }
 }
