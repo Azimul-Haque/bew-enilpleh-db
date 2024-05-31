6328566fa6fe6f08cd9c15549aa57af769f7db05
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-01 02:47:46 +0600

    Commit Sat 06/01/2024  2:47:46.30 14357

--- app/Eshebaimage.php
+++ app/Eshebaimage.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Eshebaimage extends Model
 {
+    public $timestamps = false;
+    
     public function esheba(){
         return $this->belongsTo('App\Esheba');
     }
