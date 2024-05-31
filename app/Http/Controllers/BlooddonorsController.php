f099c8622696e830593410c10ebede9e134e4b65
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-31 21:01:51 +0600

    Commit Fri 05/31/2024 21:01:51.30 8750

--- app/Http/Controllers/BlooddonorsController.php
+++ app/Http/Controllers/BlooddonorsController.php
@@ -92,59 +92,7 @@ class BlooddonorsController extends Controller
         $blooddonor->mobile = $request->mobile;
         $blooddonor->save();
 
-        if(isset($request->medicaldepartments)){
-            foreach($request->medicaldepartments as $medicaldepartment_id) {
-                $doctormedicaldepartment = new Doctormedicaldepartment;
-                $doctormedicaldepartment->doctor_id = $doctor->id;
-                $doctormedicaldepartment->medicaldepartment_id = $medicaldepartment_id;
-                $doctormedicaldepartment->save();
-
-                Cache::forget('doctors'. $medicaldepartment_id . 'departmentwise' . $request->district_id);
-                Cache::forget('doctors'. $medicaldepartment_id . 'departmentwise' . $request->district_id . $request->upazilla_id);
-
-                Cache::forget('doctors'. $medicaldepartment_id . 'symptomwise' . $request->district_id);
-                Cache::forget('doctors'. $medicaldepartment_id . 'symptomwise' . $request->district_id . $request->upazilla_id);
-            }            
-        }
-
-        if(isset($request->medicalsymptoms)){
-            foreach($request->medicalsymptoms as $medicalsymptom_id) {
-                $doctormedicalsymptom = new Doctormedicalsymptom;
-                $doctormedicalsymptom->doctor_id = $doctor->id;
-                $doctormedicalsymptom->medicalsymptom_id = $medicalsymptom_id;
-                $doctormedicalsymptom->save();
-
-                Cache::forget('doctors'. $medicalsymptom_id . 'departmentwise' . $request->district_id);
-                Cache::forget('doctors'. $medicalsymptom_id . 'departmentwise' . $request->district_id . $request->upazilla_id);
-
-                Cache::forget('doctors'. $medicalsymptom_id . 'symptomwise' . $request->district_id);
-                Cache::forget('doctors'. $medicalsymptom_id . 'symptomwise' . $request->district_id . $request->upazilla_id);
-            }            
-        }
-
-        if(isset($request->hospitals)){
-            foreach($request->hospitals as $hospital_id) {
-                $doctorhospital = new Doctorhospital;
-                $doctorhospital->doctor_id = $doctor->id;
-                $doctorhospital->hospital_id = $hospital_id;
-                $doctorhospital->save();
-
-                Cache::forget('hospitaldoctors'. $hospital_id);
-            }            
-        }
-
-        // image upload
-        if($request->hasFile('image')) {
-            $image    = $request->file('image');
-            $filename = random_string(5) . time() .'.' . "webp";
-            $location = public_path('images/doctors/'. $filename);
-            Image::make($image)->resize(350, null, function ($constraint) { $constraint->aspectRatio(); })->save($location);
-            $doctorimage              = new Doctorimage;
-            $doctorimage->doctor_id = $doctor->id;
-            $doctorimage->image       = $filename;
-            $doctorimage->save();
-        }
-
+        
         Cache::forget('doctors'. $request->district_id);
         Cache::forget('doctors'. $request->district_id . $request->upazilla_id);
         Session::flash('success', 'Doctors added successfully!');
