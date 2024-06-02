3afcbcef136762f997774d0939205c392f7ad7f2
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-03 03:14:29 +0600

    Commit Mon 06/03/2024  3:14:29.26 20190

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,7 @@ Route::post('/dashboard/eshebas/{id}/update', 'EshebaController@updateEsheba')->
 Route::get('/dashboard/eshebas/{id}/delete', 'EshebaController@deleteEsheba')->name('dashboard.eshebas.delete');
 
 // Ambulances
-Route::get('/dashboard/eshebas', 'AdminandothersController@index')->name('dashboard.eshebas');
+Route::get('/dashboard/admins', 'AdminandothersController@index')->name('dashboard.admins');
 Route::get('/dashboard/eshebas/{search}', 'AdminandothersController@indexSearch')->name('dashboard.eshebas.search');
 Route::post('/dashboard/eshebas/store', 'AdminandothersController@storeEsheba')->name('dashboard.eshebas.store');
 Route::post('/dashboard/eshebas/{id}/update', 'AdminandothersController@updateEsheba')->name('dashboard.eshebas.update');
