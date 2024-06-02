d2b8ffe561d1213963eb6376cb03c9a7fcfe9c9f
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-03 03:45:12 +0600

    Commit Mon 06/03/2024  3:45:12.26 1861

--- database/migrations/2024_06_03_031319_create_admins_table.php
+++ database/migrations/2024_06_03_031319_create_admins_table.php
@@ -15,6 +15,9 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->integer('district_id')->unsigned();
+            $table->string('name');
+            $table->string('mobile');
             $table->timestamps();
         });
     }
