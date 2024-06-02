1f5e714a1c9cfaf05a41c79bab2ecfa15050f290
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-03 03:41:48 +0600

    Commit Mon 06/03/2024  3:41:48.28 10749

--- resources/views/dashboard/admins/index.blade.php
+++ resources/views/dashboard/admins/index.blade.php
@@ -6,7 +6,6 @@
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="text/javascript" src="{{ asset('js/jquery-for-dp.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
 @endsection
 
 @section('content')
