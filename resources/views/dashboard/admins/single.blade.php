e5c7ca62532d79a457b682ebcdd098ebd85531dc
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-03 03:51:47 +0600

    Commit Mon 06/03/2024  3:51:47.48 26071

--- resources/views/dashboard/admins/single.blade.php
+++ resources/views/dashboard/admins/single.blade.php
@@ -169,7 +169,7 @@
       <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header bg-success">
-            <h5 class="modal-title" id="addUserModalLabel">নতুন প্রশাসন কর্মকর্তা যোগ</h5>
+            <h5 class="modal-title" id="addUserModalLabel">নতুন প্রশাসন কর্মকর্তা যোগ (জেলা: <strong>{{ $district->name_bangla }}</strong>)</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
