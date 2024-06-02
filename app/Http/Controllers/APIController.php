9500539e8ece03eb7bca0264dca1b8060fb2bcb7
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-03 04:23:50 +0600

    Commit Mon 06/03/2024  4:23:50.35 16174

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -504,7 +504,7 @@ class APIController extends Controller
         {
             $admins = Cache::remember('admins'  . $district_id, 30 * 24 * 60 * 60, function () use ($district_id) {
                  $admins = Admin::where('district_id', $district_id)
-                                 ->orderBy('id', 'desc')
+                                 ->orderBy('id', 'asv')
                                  ->get();
                  foreach($admins as $admin) {
                        $admin->makeHidden('id', 'district_id', 'created_at', 'updated_at');
