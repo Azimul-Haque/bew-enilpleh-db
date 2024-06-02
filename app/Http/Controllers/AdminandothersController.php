028b7d5ae3fdbe5e72d4b512b30a34935a5b77ee
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-03 03:57:01 +0600

    Commit Mon 06/03/2024  3:57:01.28 2161

--- app/Http/Controllers/AdminandothersController.php
+++ app/Http/Controllers/AdminandothersController.php
@@ -63,7 +63,7 @@ class AdminandothersController extends Controller
                             ->withEshebas($eshebas);
     }
 
-    public function storeEsheba(Request $request)
+    public function storeAdmin(Request $request)
     {
         $this->validate($request,array(
             'name'                => 'required|string|max:191',
