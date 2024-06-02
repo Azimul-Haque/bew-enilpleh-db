5c32ab41a610429740a0205d1161393812271ea5
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-03 03:48:54 +0600

    Commit Mon 06/03/2024  3:48:54.28 15364

--- resources/views/dashboard/admins/single.blade.php
+++ resources/views/dashboard/admins/single.blade.php
@@ -185,7 +185,7 @@
                              name="name"
                              class="form-control"
                              value="{{ old('name') }}"
-                             placeholder="প্রশাসন কর্মকর্তার পদবি" required>
+                             placeholder="প্রশাসন কর্মকর্তার পদবি (যেমন: জেলা প্রশাসক, এনডিসি ইত্যাদি)" required>
                       <div class="input-group-append">
                           <div class="input-group-text"><span class="fas fa-hand-holding-medical"></span></div>
                       </div>
