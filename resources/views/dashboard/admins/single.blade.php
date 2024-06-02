1ad8e806a093695f33bdbacb2e2ea82f6ae154d4
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-03 03:53:06 +0600

    Commit Mon 06/03/2024  3:53:06.30 7934

--- resources/views/dashboard/admins/single.blade.php
+++ resources/views/dashboard/admins/single.blade.php
@@ -221,7 +221,7 @@
 
         $(document).on('click', '#search-button', function() {
           if($('#search-param').val() != '') {
-            var urltocall = '{{ route('dashboard.admins') }}' +  '/' + $('#search-param').val();
+            var urltocall = '{{ route('dashboard.admins.districtwise', $district->id) }}' +  '/' + $('#search-param').val();
             location.href= urltocall;
           } else {
             $('#search-param').css({ "border": '#FF0000 2px solid'});
@@ -234,7 +234,7 @@
         $("#search-param").keyup(function(e) {
           if(e.which == 13) {
             if($('#search-param').val() != '') {
-              var urltocall = '{{ route('dashboard.admins') }}' +  '/' + $('#search-param').val();
+              var urltocall = '{{ route('dashboard.admins.districtwise', $district->id) }}' +  '/' + $('#search-param').val();
               location.href= urltocall;
             } else {
               $('#search-param').css({ "border": '#FF0000 2px solid'});
