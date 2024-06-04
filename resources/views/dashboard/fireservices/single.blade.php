93d6af9820cd960b8ade1588b34dfe83dd69285e
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-05 02:17:38 +0600

    Commit Wed 06/05/2024  2:17:38.25 22538

--- resources/views/dashboard/fireservices/single.blade.php
+++ resources/views/dashboard/fireservices/single.blade.php
@@ -170,7 +170,7 @@
                              name="name"
                              class="form-control"
                              value="{{ old('name') }}"
-                             placeholder="ফায়ার সার্ভিস কর্মকর্তার পদবি (যেমন: সহকারী পরিচালক, টাংগাইল ফায়ার স্টেশন ইত্যাদি ইত্যাদি)" required>
+                             placeholder="ফায়ার সার্ভিস কর্মকর্তার পদবি (যেমন: সহকারী পরিচালক, টাঙ্গাইল ফায়ার স্টেশন ইত্যাদি ইত্যাদি)" required>
                       <div class="input-group-append">
                           <div class="input-group-text"><span class="fas fa-user-tie"></span></div>
                       </div>
