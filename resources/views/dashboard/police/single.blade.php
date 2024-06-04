ca27fa7e9f88c5a11f49e82a07af91f5eea4071a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-05 01:06:47 +0600

    Commit Wed 06/05/2024  1:06:47.23 32309

--- resources/views/dashboard/police/single.blade.php
+++ resources/views/dashboard/police/single.blade.php
@@ -77,7 +77,7 @@
                                                name="name"
                                                class="form-control"
                                                value="{{ $police->name }}"
-                                               placeholder="পুলিশ কর্মকর্তার নাম" required>
+                                               placeholder="পুলিশ কর্মকর্তার পদবি (যেমন: পুলিশ সুপার, অতিরিক্ত পুলিশ সুপার, সহকারী পুলিশ সুপার, ওসি ইত্যাদি)" required>
                                         <div class="input-group-append">
                                             <div class="input-group-text"><span class="fas fa-user-tie"></span></div>
                                         </div>
