1a30d24ce0664498d26571b8dfc85e5f2d85c6b3
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-05 00:57:07 +0600

    Commit Wed 06/05/2024  0:57:07.24 11069

--- resources/views/dashboard/police/single.blade.php
+++ resources/views/dashboard/police/single.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title') ড্যাশবোর্ড | প্রশাসন কর্মকর্তাগণমকর্তা তালিকা @endsection
+@section('title') ড্যাশবোর্ড | পুলিশ কর্মকর্তাগণমকর্তা তালিকা @endsection
 
 @section('third_party_stylesheets')
    {{--  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
@@ -7,16 +7,16 @@
 @endsection
 
 @section('content')
-  @section('page-header') প্রশাসন কর্মকর্তা তালিকা / {{ $district->name_bangla }} জেলা (মোট {{ bangla($adminscount) }} টি) @endsection
+  @section('page-header') পুলিশ কর্মকর্তা তালিকা / {{ $district->name_bangla }} জেলা (মোট {{ bangla($adminscount) }} টি) @endsection
     <div class="container-fluid">
     <div class="card">
           <div class="card-header">
-            <h3 class="card-title">প্রশাসন কর্মকর্তা তালিকা</h3>
+            <h3 class="card-title">পুলিশ কর্মকর্তা তালিকা</h3>
 
             <div class="card-tools">
               <form class="form-inline form-group-lg" action="">
                 <div class="form-group">
-                  <input type="search-param" class="form-control form-control-sm" placeholder="প্রশাসন কর্মকর্তা খুঁজুন" id="search-param" required>
+                  <input type="search-param" class="form-control form-control-sm" placeholder="পুলিশ কর্মকর্তা খুঁজুন" id="search-param" required>
                 </div>
                 <button type="button" id="search-button" class="btn btn-default btn-sm" style="margin-left: 5px;">
                   <i class="fas fa-search"></i> খুঁজুন
@@ -62,7 +62,7 @@
                         <div class="modal-dialog" role="document">
                           <div class="modal-content">
                             <div class="modal-header bg-primary">
-                              <h5 class="modal-title" id="editUserModalLabel">প্রশাসন কর্মকর্তা তথ্য হালনাগাদ</h5>
+                              <h5 class="modal-title" id="editUserModalLabel">পুলিশ কর্মকর্তা তথ্য হালনাগাদ</h5>
                               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                               </button>
@@ -77,7 +77,7 @@
                                                name="name"
                                                class="form-control"
                                                value="{{ $admin->name }}"
-                                               placeholder="প্রশাসন কর্মকর্তার নাম" required>
+                                               placeholder="পুলিশ কর্মকর্তার নাম" required>
                                         <div class="input-group-append">
                                             <div class="input-group-text"><span class="fas fa-user-tie"></span></div>
                                         </div>
@@ -87,7 +87,7 @@
                                                name="mobile"
                                                value="{{ $admin->mobile }}"
                                                class="form-control"
-                                               placeholder="প্রশাসন কর্মকর্তার মোবাইল নম্বর" required>
+                                               placeholder="পুলিশ কর্মকর্তার মোবাইল নম্বর" required>
                                         <div class="input-group-append">
                                             <div class="input-group-text"><span class="fas fa-mobile"></span></div>
                                         </div>
@@ -116,13 +116,13 @@
                           <div class="modal-dialog" role="document">
                             <div class="modal-content">
                               <div class="modal-header bg-danger">
-                                <h5 class="modal-title" id="deleteUserModalLabel">প্রশাসন কর্মকর্তা ডিলেট</h5>
+                                <h5 class="modal-title" id="deleteUserModalLabel">পুলিশ কর্মকর্তা ডিলেট</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                   <span aria-hidden="true">&times;</span>
                                 </button>
                               </div>
                               <div class="modal-body">
-                                আপনি কি নিশ্চিতভাবে এই প্রশাসন কর্মকর্তাকে ডিলেট করতে চান?<br/>
+                                আপনি কি নিশ্চিতভাবে এই পুলিশ কর্মকর্তাকে ডিলেট করতে চান?<br/>
                                 <center>
                                     <big><b>{{ $admin->name }}</b></big><br/>
                                     <small><i class="fas fa-phone"></i> {{ $admin->mobile }}</small>
@@ -155,7 +155,7 @@
       <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header bg-success">
-            <h5 class="modal-title" id="addUserModalLabel">নতুন প্রশাসন কর্মকর্তা যোগ (জেলা: <strong>{{ $district->name_bangla }}</strong>)</h5>
+            <h5 class="modal-title" id="addUserModalLabel">নতুন পুলিশ কর্মকর্তা যোগ (জেলা: <strong>{{ $district->name_bangla }}</strong>)</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -170,7 +170,7 @@
                              name="name"
                              class="form-control"
                              value="{{ old('name') }}"
-                             placeholder="প্রশাসন কর্মকর্তার পদবি (যেমন: জেলা প্রশাসক, এনডিসি ইত্যাদি)" required>
+                             placeholder="পুলিশ কর্মকর্তার পদবি (যেমন: জেলা প্রশাসক, এনডিসি ইত্যাদি)" required>
                       <div class="input-group-append">
                           <div class="input-group-text"><span class="fas fa-user-tie"></span></div>
                       </div>
@@ -180,7 +180,7 @@
                              name="mobile"
                              value="{{ old('mobile') }}"
                              class="form-control"
-                             placeholder="প্রশাসন কর্মকর্তার মোবাইল নম্বর" required>
+                             placeholder="পুলিশ কর্মকর্তার মোবাইল নম্বর" required>
                       <div class="input-group-append">
                           <div class="input-group-text"><span class="fas fa-mobile"></span></div>
                       </div>
