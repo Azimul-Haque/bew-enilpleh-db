89efad8dc246803bbb24b73149087cbff4b83e7d
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-05 00:55:08 +0600

    Commit Wed 06/05/2024  0:55:08.33 27630

--- app/Http/Controllers/AdminandothersController.php
+++ app/Http/Controllers/AdminandothersController.php
@@ -162,6 +162,7 @@ class AdminandothersController extends Controller
     {
         $this->validate($request,array(
             'name'                => 'required|string|max:191',
+            'statiion_type'       => 'required|integer',
             'mobile'              => 'required|string|max:191',
         ));
 
