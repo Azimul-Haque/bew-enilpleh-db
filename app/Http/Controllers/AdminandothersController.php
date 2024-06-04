58805736e3b84eb0089689504e96be044bb6a231
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-05 00:43:23 +0600

    Commit Wed 06/05/2024  0:43:23.24 9547

--- app/Http/Controllers/AdminandothersController.php
+++ app/Http/Controllers/AdminandothersController.php
@@ -101,4 +101,77 @@ class AdminandothersController extends Controller
         Session::flash('success', 'Admin officer updated successfully!');
         return redirect()->route('dashboard.admins.districtwise', $district_id);
     }
+
+    public function policeIndex()
+    {
+        $districts = District::all();
+                
+        return view('dashboard.admins.index')
+                            ->withDistricts($districts);
+    }
+
+    public function policeIndexSingle($district_id)
+    {
+        $district = District::find($district_id);
+        $adminscount = Admin::where('district_id', $district_id)->count();
+        $admins = Admin::where('district_id', $district_id)->orderBy('id', 'desc')->paginate(10);
+                
+        return view('dashboard.admins.single')
+                            ->withDistrict($district)
+                            ->withAdminscount($adminscount)
+                            ->withAdmins($admins);
+    }
+
+    public function indexSearch($district_id, $search)
+    {
+        $district = District::find($district_id);
+        $adminscount = Admin::where('name', 'LIKE', "%$search%")
+                            ->orWhere('mobile', 'LIKE', "%$search%")->count()
+                            ;
+        $admins = Admin::where('name', 'LIKE', "%$search%")
+                       ->orWhere('mobile', 'LIKE', "%$search%")
+                       ->orderBy('id', 'desc')
+                       ->paginate(10);
+                
+        return view('dashboard.admins.single')
+                            ->withDistrict($district)
+                            ->withAdminscount($adminscount)
+                            ->withAdmins($admins);
+    }
+
+    public function storeAdmin(Request $request, $district_id)
+    {
+        $this->validate($request,array(
+            'name'                => 'required|string|max:191',
+            'mobile'              => 'required|string|max:191',
+        ));
+
+        $admin = new Admin;
+        $admin->district_id = $district_id;
+        $admin->name = $request->name;
+        $admin->mobile = $request->mobile;
+        $admin->save();
+
+        Cache::forget('admins' . $district_id);
+        Session::flash('success', 'Admin officer added successfully!');
+        return redirect()->route('dashboard.admins.districtwise', $district_id);
+    }
+
+    public function updateAdmin(Request $request, $district_id, $id)
+    {
+        $this->validate($request,array(
+            'name'                => 'required|string|max:191',
+            'mobile'              => 'required|string|max:191',
+        ));
+
+        $admin = Admin::find($id);
+        $admin->district_id = $district_id;
+        $admin->name = $request->name;
+        $admin->mobile = $request->mobile;
+        $admin->save();
+
+        Cache::forget('admins' . $district_id);
+        Session::flash('success', 'Admin officer updated successfully!');
+        return redirect()->route('dashboard.admins.districtwise', $district_id);
+    }
 }
