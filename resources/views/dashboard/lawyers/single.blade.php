3c89dda0892057cbe955c80349e24baf2973d7b0
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-14 01:21:03 +0600

    Commit Fri 06/14/2024  1:21:03.29 18776

--- resources/views/dashboard/lawyers/single.blade.php
+++ resources/views/dashboard/lawyers/single.blade.php
@@ -38,6 +38,7 @@
                 <tr>
                   <th>নাম</th>
                   <th>মোবাইল নম্বর</th>
+                  <th>ধরন ও কোর্ট</th>
                   <th align="right">কার্যক্রম</th>
                 </tr>
               </thead>
