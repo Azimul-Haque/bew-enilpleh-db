1656eb96d9c57d8009ae1d420973cb8b0701dea5
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-14 01:20:34 +0600

    Commit Fri 06/14/2024  1:20:34.31 13354

--- routes/web.php
+++ routes/web.php
@@ -92,7 +92,7 @@ Route::get('/dashboard/fireservices/{district_id}/{id}/delete', 'AdminandothersC
 Route::get('/dashboard/lawyers', 'AdminandothersController@lawyerIndex')->name('dashboard.lawyers');
 Route::get('/dashboard/lawyers/{district_id}', 'AdminandothersController@lawyerIndexSingle')->name('dashboard.lawyers.districtwise');
 Route::get('/dashboard/lawyers/{district_id}/{search}', 'AdminandothersController@lawyersIndexSearch')->name('dashboard.lawyers.districtwise.search');
-Route::post('/dashboard/lawyers/{district_id}/store', 'AdminandothersController@storeLawyers')->name('dashboard.lawyers.store');
+Route::post('/dashboard/lawyers/{district_id}/store', 'AdminandothersController@storeLawyer')->name('dashboard.lawyers.store');
 Route::post('/dashboard/lawyers/{district_id}/{id}/update', 'AdminandothersController@updateLawyers')->name('dashboard.lawyers.update');
 Route::get('/dashboard/lawyers/{district_id}/{id}/delete', 'AdminandothersController@deleteLawyers')->name('dashboard.lawyers.delete');
 
