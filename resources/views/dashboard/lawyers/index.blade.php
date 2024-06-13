b34ba8d56790a73aebba2f51c55b95de5b1c06aa
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-14 01:03:57 +0600

    Commit Fri 06/14/2024  1:03:57.29 32725

--- resources/views/dashboard/lawyers/index.blade.php
+++ resources/views/dashboard/lawyers/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title') ড্যাশবোর্ড | ফায়ার সার্ভিস কর্মকর্তাগণ @endsection
+@section('title') ড্যাশবোর্ড | আইনজীবীগণ @endsection
 
 @section('third_party_stylesheets')
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -35,7 +35,7 @@
                     <tr>
                         @foreach ($chunk as $district)
                             <td>
-                              <a href="{{ route('dashboard.fireservices.districtwise', $district->id) }}" rel="tooltip" title="" data-original-title="{{ $district->name_bangla }} জেলার ফায়ার সার্ভিস কর্মকর্তা তালিকা দেখতে ক্লিক করুন">{{ $district->name_bangla }} <small>({{ bangla($district->fireservices->count()) }} জন কর্মকর্তা)</small></a>
+                              <a href="{{ route('dashboard.fireservices.districtwise', $district->id) }}" rel="tooltip" title="" data-original-title="{{ $district->name_bangla }} জেলার আইনজীবী তালিকা দেখতে ক্লিক করুন">{{ $district->name_bangla }} <small>({{ bangla($district->fireservices->count()) }} জন কর্মকর্তা)</small></a>
                             </td>
                         @endforeach
                         @if ($chunk->count() < 5)
