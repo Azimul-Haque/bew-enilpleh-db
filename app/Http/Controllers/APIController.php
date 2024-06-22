68c1b2da5dd11d82ae02d7c04651ed1fd5df3d93
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-22 23:41:05 +0600

    Commit Sat 06/22/2024 23:41:05.27 21139

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -602,11 +602,11 @@ class APIController extends Controller
                                  ->orderBy('id', 'desc')
                                  ->get();
                                  // dd($rentacars);
-                 foreach($rentacars as $ambulance) {
-                     // $ambulance->districtname = $ambulance->district->name_bangla;
-                     // $ambulance->upazillaname = $ambulance->upazilla->name_bangla;
-                     $ambulance->image = $ambulance->ambulanceimage ? $ambulance->ambulanceimage->image : '';
-                     $ambulance->makeHidden('district', 'upazilla', 'ambulanceimage', 'created_at', 'updated_at');
+                 foreach($rentacars as $rentacar) {
+                     // $rentacar->districtname = $rentacar->district->name_bangla;
+                     // $rentacar->upazillaname = $rentacar->upazilla->name_bangla;
+                     $rentacar->image = $rentacar->rentacarimage ? $rentacar->rentacarimage->image : '';
+                     $rentacar->makeHidden('district', 'upazilla', 'ambulanceimage', 'created_at', 'updated_at');
                  }
                  return $rentacars;
             });
