5e7c4a99c5619c6ecdf57e644d39bfea4d0c0439
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 01:55:24 +0600

    Commit Sun 06/23/2024  1:55:24.33 30980

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -114,6 +114,15 @@
 </li>
 @endif
 
+@if(Auth::user()->role == 'admin')
+<li class="nav-item">
+    <a href="{{ route('dashboard.rabs') }}" class="nav-link {{ Request::is('dashboard/rabs') ? 'active' : '' }} {{ Request::is('dashboard/rabs/*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-chalkboard-teacher"></i>
+        <p>র‍্যাব</p>
+    </a>
+</li>
+@endif
+
 {{-- @if(Auth::user()->role == 'admin')
 <li class="nav-item">
     <a href="{{ route('dashboard.payments') }}" class="nav-link {{ Request::is('dashboard/payments') ? 'active' : '' }} {{ Request::is('dashboard/payments/*') ? 'active' : '' }}">
