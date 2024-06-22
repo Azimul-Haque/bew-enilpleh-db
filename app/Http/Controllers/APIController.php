2a02aa2ed1dc7291acbb9e44e7c0eb5edb3b4033
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 04:39:10 +0600

    Commit Sun 06/23/2024  4:39:10.31 18126

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -654,7 +654,7 @@ class APIController extends Controller
         if($softtoken == env('SOFT_TOKEN'))
         {
             $rabs = Cache::remember('rabs'  . $district_id, 30 * 24 * 60 * 60, function () use ($district_id) {
-                $rabs = Rab::where('district_id', $district_id)->first();
+                $battalion = Rab::where('district_id', $district_id)->first();
                     foreach($rabs as $fireservice) {
                         $fireservice->makeHidden('id', 'district_id', 'created_at', 'updated_at');
                     }
