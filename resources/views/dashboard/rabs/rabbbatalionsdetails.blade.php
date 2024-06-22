fc51a37979858847d025a6ac2748505aef03308b
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 03:35:54 +0600

    Commit Sun 06/23/2024  3:35:54.31 29609

--- resources/views/dashboard/rabs/rabbbatalionsdetails.blade.php
+++ resources/views/dashboard/rabs/rabbbatalionsdetails.blade.php
@@ -69,7 +69,7 @@
                                 <span aria-hidden="true">&times;</span>
                               </button>
                             </div>
-                            <form method="post" action="{{ route('dashboard.rentacars.update', [$district->id, $rabbattallionofficer->id]) }}" enctype="multipart/form-data">
+                            <form method="post" action="{{ route('dashboard.rabbattalionsdetails.update', [$district->id, $rabbattallionofficer->id]) }}" enctype="multipart/form-data">
                               <div class="modal-body">
                                 
                                     @csrf
