d11c0150d06432b1951340736fd0c64c97934a16
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 01:58:51 +0600

    Commit Sun 06/23/2024  1:58:51.27 30269

--- resources/views/dashboard/rabs/index.blade.php
+++ resources/views/dashboard/rabs/index.blade.php
@@ -37,7 +37,9 @@
                             <td>
                               <a href="{{ route('dashboard.coachings.districtwise', $district->id) }}" rel="tooltip" title="" data-original-title="{{ $district->name_bangla }} জেলার কোচিং সেন্টারের তালিকা দেখতে ক্লিক করুন">{{ $district->name_bangla }} <small>({{ bangla($district->coachings->count()) }} টি কোচিং সেন্টার)</small></a>
                             </td>
-                            <td></td>
+                            <td>
+                              বাটন
+                            </td>
                         @endforeach
                         @if ($chunk->count() < 3)
                             @for ($i = 0; $i < 3 - $chunk->count(); $i++)
