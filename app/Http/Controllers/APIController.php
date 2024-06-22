6d6d5499971ef9aa008092c5a3dd14e752bca67c
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 04:44:19 +0600

    Commit Sun 06/23/2024  4:44:19.30 5619

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -663,7 +663,7 @@ class APIController extends Controller
                 $rabdata = collect();
                 $rabdata->battalion_name = $rab->rabbattalion->name;
                 $rabdata->battalion_details = $rab->rabbattalion->details;
-                $rabdata->battalion_map = $rab->rabbattalion->details;
+                $rabdata->battalion_map = $rab->rabbattalion->map;
                 dd($rabdata);
                 foreach($rabs as $fireservice) {
                     $fireservice->makeHidden('id', 'district_id', 'created_at', 'updated_at');
