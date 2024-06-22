90cc5fbfcad43c5f6a8a9d9153fd2e39e4c9f6c0
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 01:59:32 +0600

    Commit Sun 06/23/2024  1:59:32.29 25658

--- resources/views/dashboard/rabs/index.blade.php
+++ resources/views/dashboard/rabs/index.blade.php
@@ -8,51 +8,55 @@
 @section('content')
   @section('page-header') জেলা তালিকা  @endsection
     <div class="container-fluid">
-    <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">জেলা তালিকা</h3>
+      <div class="row">
+        <div class="col-md-9">
+          <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">জেলা তালিকা</h3>
 
-            <div class="card-tools"></div>
-          </div>
-          <!-- /.card-header -->
-          <div class="card-body p-0">
-            <table class="table table-condensed">
-              {{-- <thead>
-                <tr>
-                  <th>জেলার নাম</th>
-                </tr>
-              </thead> --}}
-              <tbody>
-                {{-- @foreach($districts as $district)
-                  <tr>
-                    <td style="font-size: 14px;">
-                      {{ $district->name }}
-                    </td>
-                  </tr>
-                @endforeach --}}
+                  <div class="card-tools"></div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body p-0">
+                  <table class="table table-condensed">
+                    {{-- <thead>
+                      <tr>
+                        <th>জেলার নাম</th>
+                      </tr>
+                    </thead> --}}
+                    <tbody>
+                      {{-- @foreach($districts as $district)
+                        <tr>
+                          <td style="font-size: 14px;">
+                            {{ $district->name }}
+                          </td>
+                        </tr>
+                      @endforeach --}}
 
-                @foreach ($districts->chunk(3) as $chunk)
-                    <tr>
-                        @foreach ($chunk as $district)
-                            <td>
-                              <a href="{{ route('dashboard.coachings.districtwise', $district->id) }}" rel="tooltip" title="" data-original-title="{{ $district->name_bangla }} জেলার কোচিং সেন্টারের তালিকা দেখতে ক্লিক করুন">{{ $district->name_bangla }} <small>({{ bangla($district->coachings->count()) }} টি কোচিং সেন্টার)</small></a>
-                            </td>
-                            <td>
-                              বাটন
-                            </td>
-                        @endforeach
-                        @if ($chunk->count() < 3)
-                            @for ($i = 0; $i < 3 - $chunk->count(); $i++)
-                                <td></td>
-                            @endfor
-                        @endif
-                    </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </div>
-          <!-- /.card-body -->
+                      @foreach ($districts->chunk(3) as $chunk)
+                          <tr>
+                              @foreach ($chunk as $district)
+                                  <td>
+                                    <a href="{{ route('dashboard.coachings.districtwise', $district->id) }}" rel="tooltip" title="" data-original-title="{{ $district->name_bangla }} জেলার কোচিং সেন্টারের তালিকা দেখতে ক্লিক করুন">{{ $district->name_bangla }} <small>({{ bangla($district->coachings->count()) }} টি কোচিং সেন্টার)</small></a>
+                                  </td>
+                                  <td>
+                                    বাটন
+                                  </td>
+                              @endforeach
+                              @if ($chunk->count() < 3)
+                                  @for ($i = 0; $i < 3 - $chunk->count(); $i++)
+                                      <td></td>
+                                  @endfor
+                              @endif
+                          </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
         </div>
+      </div>
     </div>
 
 @endsection
