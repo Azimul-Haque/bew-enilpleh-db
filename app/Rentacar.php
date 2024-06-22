1c32eee0171cbfb18061d9508a52c150f8385f40
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-22 23:17:16 +0600

    Commit Sat 06/22/2024 23:17:16.31 29550

--- app/Rentacar.php
+++ app/Rentacar.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rentacar extends Model
 {
-    //
+    public function district(){
+        return $this->belongsTo('App\District');
+    }
+
+    public function rentacarimage(){
+        return $this->hasOne('App\Rentacarimage');
+    }
 }
