ae685919fc1737cdafe612f3880f16bde31865f5
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 01:23:49 +0600

    Commit Sun 06/23/2024  1:23:49.30 18971

--- resources/views/dashboard/lawyers/single.blade.php
+++ resources/views/dashboard/lawyers/single.blade.php
@@ -254,7 +254,7 @@
 
         $(document).on('click', '#search-button', function() {
           if($('#search-param').val() != '') {
-            var urltocall = '{{ route('dashboard.fireservices.districtwise', $district->id) }}' +  '/' + $('#search-param').val();
+            var urltocall = '{{ route('dashboard.lawyers.districtwise', $district->id) }}' +  '/' + $('#search-param').val();
             location.href= urltocall;
           } else {
             $('#search-param').css({ "border": '#FF0000 2px solid'});
@@ -267,7 +267,7 @@
         $("#search-param").keyup(function(e) {
           if(e.which == 13) {
             if($('#search-param').val() != '') {
-              var urltocall = '{{ route('dashboard.fireservices.districtwise', $district->id) }}' +  '/' + $('#search-param').val();
+              var urltocall = '{{ route('dashboard.lawyers.districtwise', $district->id) }}' +  '/' + $('#search-param').val();
               location.href= urltocall;
             } else {
               $('#search-param').css({ "border": '#FF0000 2px solid'});
