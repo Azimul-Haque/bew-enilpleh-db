45641d3f1757aaa9a7997bfb697f55635a3a0b47
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 02:00:50 +0600

    Commit Sun 06/23/2024  2:00:50.27 7062

--- resources/views/dashboard/rabs/index.blade.php
+++ resources/views/dashboard/rabs/index.blade.php
@@ -56,7 +56,156 @@
                 <!-- /.card-body -->
               </div>
         </div>
-        <div class="col-md-4"></div>
+        <div class="col-md-4">
+          <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">কোচিং সেন্টার তালিকা</h3>
+
+                  <div class="card-tools">
+                    <form class="form-inline form-group-lg" action="">
+                      <div class="form-group">
+                        <input type="search-param" class="form-control form-control-sm" placeholder="কোচিং সেন্টার খুঁজুন" id="search-param" required>
+                      </div>
+                      <button type="button" id="search-button" class="btn btn-default btn-sm" style="margin-left: 5px;">
+                        <i class="fas fa-search"></i> খুঁজুন
+                      </button>
+                      {{-- <button type="button" class="btn btn-info btn-sm"  data-toggle="modal" data-target="#addBulkDate" style="margin-left: 5px;">
+                        <i class="fas fa-calendar-alt"></i> বাল্ক মেয়াদ বাড়ান
+                      </button> --}}
+                      <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addUserModal" style="margin-left: 5px;">
+                        <i class="fas fa-plus-square"></i> নতুন
+                      </button>
+                    </form>
+                    
+                  </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body p-0">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th>নাম</th>
+                        <th>মোবাইল নম্বর</th>
+                        <th>ঠিকানা</th>
+                        <th align="right">কার্যক্রম</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach($coachings as $coaching)
+                        <tr>
+                          <td>
+                            {{ $coaching->name }}
+                          </td>
+                          <td>{{ $coaching->mobile }}</td>
+                          <td>{{ $coaching->address }}</td>
+                          <td align="right">
+                            {{-- <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#notifModal{{ $coaching->id }}">
+                              <i class="fas fa-bell"></i>
+                            </button> --}}
+                            <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editUserModal{{ $coaching->id }}">
+                              <i class="fas fa-edit"></i>
+                            </button>
+                            {{-- Edit User Modal Code --}}
+                            {{-- Edit User Modal Code --}}
+                            <!-- Modal -->
+                            <div class="modal fade" id="editUserModal{{ $coaching->id }}" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true" data-backdrop="static">
+                              <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                  <div class="modal-header bg-primary">
+                                    <h5 class="modal-title" id="editUserModalLabel">কোচিং সেন্টার তথ্য হালনাগাদ</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                    </button>
+                                  </div>
+                                  <form method="post" action="{{ route('dashboard.coachings.update', [$district->id, $coaching->id]) }}" enctype="multipart/form-data">
+                                    <div class="modal-body">
+                                      
+                                          @csrf
+
+                                          <div class="input-group mb-3">
+                                              <input type="text"
+                                                     name="name"
+                                                     class="form-control"
+                                                     value="{{ $coaching->name }}"
+                                                     placeholder="কোচিং সেন্টারের নাম" required>
+                                              <div class="input-group-append">
+                                                  <div class="input-group-text"><span class="fas fa-user-tie"></span></div>
+                                              </div>
+                                          </div>
+                                          <div class="input-group mb-3">
+                                              <input type="number"
+                                                     name="mobile"
+                                                     value="{{ $coaching->mobile }}"
+                                                     class="form-control"
+                                                     placeholder="কোচিং সেন্টারের মোবাইল নম্বর" required>
+                                              <div class="input-group-append">
+                                                  <div class="input-group-text"><span class="fas fa-mobile"></span></div>
+                                              </div>
+                                          </div>
+                                          <div class="input-group mb-3">
+                                              <input type="text"
+                                                     name="address"
+                                                     class="form-control"
+                                                     value="{{ $coaching->address }}"
+                                                     placeholder="কোচিং সেন্টারের ঠিকানা" required>
+                                              <div class="input-group-append">
+                                                  <div class="input-group-text"><span class="fas fa-map-marked-alt"></span></div>
+                                              </div>
+                                          </div>
+                                           
+                                    </div>
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
+                                      <button type="submit" class="btn btn-primary">দাখিল করুন</button>
+                                    </div>
+                                  </form>
+                                </div>
+                              </div>
+                            </div>
+                            {{-- Edit User Modal Code --}}
+                            {{-- Edit User Modal Code --}}
+
+                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteUserModal{{ $coaching->id }}">
+                              <i class="fas fa-trash-alt"></i>
+                            </button>
+                          </td>
+                              {{-- Delete User Modal Code --}}
+                              {{-- Delete User Modal Code --}}
+                              <!-- Modal -->
+                              <div class="modal fade" id="deleteUserModal{{ $coaching->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteUserModalLabel" aria-hidden="true" data-backdrop="static">
+                                <div class="modal-dialog" role="document">
+                                  <div class="modal-content">
+                                    <div class="modal-header bg-danger">
+                                      <h5 class="modal-title" id="deleteUserModalLabel">কোচিং সেন্টার ডিলেট</h5>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                      </button>
+                                    </div>
+                                    <div class="modal-body">
+                                      আপনি কি নিশ্চিতভাবে এই কোচিং সেন্টারকে ডিলেট করতে চান?<br/>
+                                      <center>
+                                          <big><b>{{ $coaching->name }}</b></big><br/>
+                                          <small><i class="fas fa-phone"></i> {{ $coaching->mobile }}</small>
+                                      </center>
+                                    </div>
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
+                                      <a href="{{ route('dashboard.coachings.delete', [$district->id, $coaching->id]) }}" class="btn btn-danger">ডিলেট করুন</a>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              {{-- Delete User Modal Code --}}
+                              {{-- Delete User Modal Code --}}
+                        </tr>
+                        
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
+        </div>
       </div>
     </div>
 
