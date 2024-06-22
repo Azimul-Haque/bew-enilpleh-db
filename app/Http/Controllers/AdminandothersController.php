36e1331a9afbbacfebbeb0bf267da8fd46ba81f4
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 04:12:23 +0600

    Commit Sun 06/23/2024  4:12:23.30 31953

--- app/Http/Controllers/AdminandothersController.php
+++ app/Http/Controllers/AdminandothersController.php
@@ -697,7 +697,7 @@ class AdminandothersController extends Controller
         $checkdistrictrab = Rab::where('district_id', $district_id)->first();
 
         
-        if($checkdistrictrab != null || !$checkdistrictrab->isEmpty()) {
+        if($checkdistrictrab != null || $checkdistrictrab->isNotEmpty()) {
             dd($checkdistrictrab);
         }
 
