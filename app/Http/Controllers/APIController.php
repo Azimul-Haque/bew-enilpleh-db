10a1139cbdfcaebb3ee87d9b5ad11019781536a0
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 04:54:12 +0600

    Commit Sun 06/23/2024  4:54:12.26 13269

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -656,7 +656,7 @@ class APIController extends Controller
     {
         if($softtoken == env('SOFT_TOKEN'))
         {
-            $rabdata = Cache::remember('rabs3asd324a'  . $district_id, 30 * 24 * 60 * 60, function () use ($district_id) {
+            $rabdata = Cache::remember('rab33'  . $district_id, 30 * 24 * 60 * 60, function () use ($district_id) {
                 $rab = Rab::where('district_id', $district_id)->first();
                 
                 
