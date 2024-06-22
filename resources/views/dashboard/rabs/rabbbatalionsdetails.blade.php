767251fa697bc5bc6ff48f10b184a4475422671a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 03:08:09 +0600

    Commit Sun 06/23/2024  3:08:09.28 18201

--- resources/views/dashboard/rabs/rabbbatalionsdetails.blade.php
+++ resources/views/dashboard/rabs/rabbbatalionsdetails.blade.php
@@ -7,7 +7,7 @@
 @endsection
 
 @section('content')
-  @section('page-header') র‍্যাব ব্যাটালিয়ন / {{ $rabbattalion->name_bangla }} জেলা (মোট {{ bangla($rentacarscount) }} টি) @endsection
+  @section('page-header') র‍্যাব ব্যাটালিয়ন / {{ $rabbattallion->name }} জেলা (মোট {{ bangla($rabbattallion) }} টি) @endsection
     <div class="container-fluid">
     <div class="card">
           <div class="card-header">
