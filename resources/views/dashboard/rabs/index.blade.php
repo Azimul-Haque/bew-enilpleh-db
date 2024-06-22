92eaa4b6ef895417b6246b92c8bb3f41ed0a3781
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 03:53:12 +0600

    Commit Sun 06/23/2024  3:53:12.60 3399

--- resources/views/dashboard/rabs/index.blade.php
+++ resources/views/dashboard/rabs/index.blade.php
@@ -41,7 +41,7 @@
                                   </td>
                                   <td>
                                     <button type="button" class="btn btn-warning btn-sm"  data-toggle="modal" data-target="#addRabBattalionModal" style="margin-left: 5px;" rel="tooltip" title="" data-original-title="র‍্যাব ব্যাটালিয়ন সংযুক্ত করুন">
-                                      <i class="fas fa-plus-square"></i> র‍্যাব সংযুক্ত
+                                      <i class="fas fa-plus-square"></i> র‍্যাব সংযুক্ত করুন
                                     </button>
                                   </td>
                               @endforeach
