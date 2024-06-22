cfd814189a9ae00f9518dd932b26c6bee2206db8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-22 23:21:40 +0600

    Commit Sat 06/22/2024 23:21:40.22 29697

--- app/Http/Controllers/AdminandothersController.php
+++ app/Http/Controllers/AdminandothersController.php
@@ -390,10 +390,9 @@ class AdminandothersController extends Controller
     public function storeRentacar(Request $request, $district_id)
     {
         $this->validate($request,array(
-            'name'                => 'required|string|max:191',
-            'mobile'              => 'required|string|max:191',
-            'court_type'          => 'required',
-            'court'               => 'required',
+            'name'           => 'required|string|max:191',
+            'mobile'         => 'required|string|max:191',
+            'image'          => 'sometimes',
         ));
 
         $lawyer = new Lawyer;
