08796b80cdb96775b42893abb6d75ec0cae1e118
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-23 03:29:24 +0600

    Commit Sun 06/23/2024  3:29:24.24 6357

--- routes/web.php
+++ routes/web.php
@@ -119,7 +119,7 @@ Route::post('/dashboard/rabs/battalion/{id}/update', 'AdminandothersController@u
 Route::get('/dashboard/rabs/battalion/details/{id}', 'AdminandothersController@detailsRabbattalion')->name('dashboard.rabbattalions.details');
 Route::get('/dashboard/rabs/battalion/details/{id}/{search}', 'AdminandothersController@detailsRabbattalionSearch')->name('dashboard.rabbattalions.details.search');
 Route::post('/dashboard/rabs/battalion/details/{battalion_id}/store', 'AdminandothersController@storeDetailsRabbattalion')->name('dashboard.rabbattalionsdetails.store');
-Route::post('/dashboard/rabs/battalion/details/{battalion_id}/{id}/update', 'AdminandothersController@storeDetailsRabbattalion')->name('dashboard.rabbattalionsdetails.store');
+Route::post('/dashboard/rabs/battalion/details/{battalion_id}/{id}/update', 'AdminandothersController@updateDetailsRabbattalion')->name('dashboard.rabbattalionsdetails.update');
 // Route::get('/dashboard/coachings/{district_id}', 'AdminandothersController@coachingIndexSingle')->name('dashboard.coachings.districtwise');
 // Route::get('/dashboard/coachings/{district_id}/{search}', 'AdminandothersController@coachingIndexSearch')->name('dashboard.coachings.districtwise.search');
 // Route::post('/dashboard/coachings/{district_id}/store', 'AdminandothersController@storeCoaching')->name('dashboard.coachings.store');
