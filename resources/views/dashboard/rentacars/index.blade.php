73a4702e4f5028b3c5affd017cc5c2a7f482da93
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-22 23:30:43 +0600

    Commit Sat 06/22/2024 23:30:43.31 31225

--- resources/views/dashboard/rentacars/index.blade.php
+++ resources/views/dashboard/rentacars/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title') ড্যাশবোর্ড | আইনজীবীগণ @endsection
+@section('title') ড্যাশবোর্ড | রেন্ট-এ-কার @endsection
 
 @section('third_party_stylesheets')
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
