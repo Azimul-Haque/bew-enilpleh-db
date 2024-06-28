95391ca95e1f5b69f8dfc660ee3a1d3130af09b5
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-28 23:06:12 +0600

    Commit Fri 06/28/2024 23:06:12.27 30587

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,6 @@ Route::post('/payment/app/confirm', 'IndexController@paymentSuccessApp')->name('
 Route::get('/payment/app/cancel', 'IndexController@paymentCancelApp')->name('index.payment.cancel.app');
 Route::get('/check/ip', 'IndexController@checkIP')->name('index.check.ip');
 
-
-
-
 // Hospital
 Route::get('/dashboard/hospitals', 'HospitalController@index')->name('dashboard.hospitals');
 Route::get('/dashboard/hospitals/{search}', 'HospitalController@indexSearch')->name('dashboard.hospitals.search');
