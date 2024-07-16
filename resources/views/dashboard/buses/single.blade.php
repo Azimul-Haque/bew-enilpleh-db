8ab9f1972776b3f88b2d2504d579403688ae3805
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-17 02:06:01 +0600

    Commit Wed 07/17/2024  2:06:01.37 29640

--- resources/views/dashboard/buses/single.blade.php
+++ resources/views/dashboard/buses/single.blade.php
@@ -174,6 +174,18 @@
             <div class="modal-body">
               
                   @csrf
+
+                  <div class="input-group mb-3">
+                    <select name="district_id" id="district" class="form-control district" required>
+                        <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
+                        @foreach($districts as $district)
+                          <option value="{{ $district->id }}" @if($district->id == $ambulance->district_id) selected @endif>{{ $district->name_bangla }}</option>
+                        @endforeach
+                    </select>
+                    <div class="input-group-append">
+                        <div class="input-group-text"><span class="fas fa-map"></span></div>
+                    </div>
+                  </div>
                   
                   <div class="input-group mb-3">
                       <input type="text"
