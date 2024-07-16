6adf175adeed7a892a4e378173e96aa909a033e0
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-17 01:42:58 +0600

    Commit Wed 07/17/2024  1:42:58.29 25464

--- app/Bus.php
+++ app/Bus.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bus extends Model
 {
-    //
+    public function fromDistrict()
+    {
+        return $this->belongsTo(District::class, 'from_district', 'district_id');
+    }
+
+    public function toDistrict()
+    {
+        return $this->belongsTo(District::class, 'to_district', 'district_id');
+    }
 }
