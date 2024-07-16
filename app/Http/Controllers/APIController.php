8c2af0e4e27c02a4e2cb7168ef9b97a6ec0f5bc8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-17 03:17:27 +0600

    Commit Wed 07/17/2024  3:17:27.29 3413

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -704,6 +704,7 @@ class APIController extends Controller
                                  ->orderBy('id', 'asc')
                                  ->get();
                  foreach($buses as $bus) {
+                       $bus->from_district = $bus->district->name_bangla;
                        $bus->makeHidden('id', 'district_id', 'created_at', 'updated_at');
                  }
                  return $buses;
