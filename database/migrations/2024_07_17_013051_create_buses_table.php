1ebace0753a3796ab45ad0cb0d740bc79d64c580
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-17 01:49:26 +0600

    Commit Wed 07/17/2024  1:49:26.34 11381

--- database/migrations/2024_07_17_013051_create_buses_table.php
+++ database/migrations/2024_07_17_013051_create_buses_table.php
@@ -22,6 +22,7 @@ class CreateBusesTable extends Migration
             $table->string('bus_type');
             $table->string('fare');
             $table->string('starting_time');
+            $table->string('contact');
             $table->timestamps();
 
             $table->foreign('from_district')->references('district_id')->on('districts');
