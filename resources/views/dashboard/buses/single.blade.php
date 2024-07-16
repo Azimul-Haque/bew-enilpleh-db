dcce0f7f7e2d3cf4386d3e7e531261bec99aed4d
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-17 02:17:00 +0600

    Commit Wed 07/17/2024  2:17:00.21 25603

--- resources/views/dashboard/buses/single.blade.php
+++ resources/views/dashboard/buses/single.blade.php
@@ -217,6 +217,16 @@
                           <div class="input-group-text"><span class="fas fa-user-tie"></span></div>
                       </div>
                   </div>
+                  <div class="input-group mb-3">
+                      <input type="text"
+                             name="bus_type"
+                             class="form-control"
+                             value="{{ old('bus_type') }}"
+                             placeholder="AC/ Non-AC/ " required>
+                      <div class="input-group-append">
+                          <div class="input-group-text"><span class="fas fa-user-tie"></span></div>
+                      </div>
+                  </div>
                   
                   <div class="input-group mb-3">
                       <input type="number"
