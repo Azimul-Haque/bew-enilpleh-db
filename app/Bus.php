cfe2aa2278200ce151092eea4369d8d43849d432
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-17 01:43:26 +0600

    Commit Wed 07/17/2024  1:43:26.30 26756

--- app/Bus.php
+++ app/Bus.php
@@ -8,11 +8,11 @@ class Bus extends Model
 {
     public function fromDistrict()
     {
-        return $this->belongsTo(District::class, 'from_district', 'district_id');
+        return $this->belongsTo('App\District', 'from_district', 'district_id');
     }
 
     public function toDistrict()
     {
-        return $this->belongsTo(District::class, 'to_district', 'district_id');
+        return $this->belongsTo('App\District', 'to_district', 'district_id');
     }
 }
