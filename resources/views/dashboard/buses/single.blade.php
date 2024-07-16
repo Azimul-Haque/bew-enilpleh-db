46b39a165233859c615d1832fa455fc85ee58810
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-17 03:13:32 +0600

    Commit Wed 07/17/2024  3:13:32.32 28078

--- resources/views/dashboard/buses/single.blade.php
+++ resources/views/dashboard/buses/single.blade.php
@@ -80,111 +80,110 @@
                       <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editUserModal{{ $bus->id }}">
                         <i class="fas fa-edit"></i>
                       </button>
-                      {{-- Edit User Modal Code --}}
-                      {{-- Edit User Modal Code --}}
-                      <!-- Modal -->
-                      <div class="modal fade" id="editUserModal{{ $bus->id }}" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true" data-backdrop="static">
-                        <div class="modal-dialog modal-lg" role="document">
-                          <div class="modal-content">
-                            <div class="modal-header bg-primary">
-                              <h5 class="modal-title" id="editUserModalLabel">বাস তথ্য হালনাগাদ</h5>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <form method="post" action="{{ route('dashboard.buses.update', [$district->id, $bus->id]) }}" enctype="multipart/form-data">
-                              <div class="modal-body">
-                                  @csrf
-                                  <h5>হতে জেলা: {{ $district->name_bangla }}</h5>
-                                  <div class="" style="margin-bottom: 15px;">
-                                    <select name="to_district" class="form-control " required>
-                                        <option selected="" disabled="" value="">গন্তব্য জেলা নির্বাচন করুন</option>
-                                        @foreach($districts as $todistrict)
-                                          <option value="{{ $todistrict->id }}" @if($todistrict->id == $bus->toDistrict->id) selected @endif >{{ $todistrict->name_bangla }} - {{ $todistrict->name }}</option>
-                                        @endforeach
-                                    </select>
-                                    {{-- <div class="input-group-append">
-                                        <div class="input-group-text"><span class="fas fa-map"></span></div>
-                                    </div> --}}
-                                  </div>
-                                  
-                                  <div class="input-group mb-3">
-                                      <input type="text"
-                                             name="bus_name"
-                                             class="form-control"
-                                             value="{{ old('bus_name') }}"
-                                             placeholder="বাসের নাম" required>
-                                      <div class="input-group-append">
-                                          <div class="input-group-text"><span class="fas fa-bus"></span></div>
-                                      </div>
-                                  </div>
-                                  <div class="input-group mb-3">
-                                      <input type="text"
-                                             name="route_info"
-                                             class="form-control"
-                                             value="{{ old('route_info') }}"
-                                             placeholder="রুটের তথ্য (যেমন: টাঙ্গাইল হতে দর্শনা ভায়া সিরাজগঞ্জ, উল্লাপাড়া, শাহজাদপুর, কাশিনাথপুর, পাবনা, কুষ্টিয়া, ঝিনাইদহ, চুয়াডাঙ্গা)" required>
-                                      <div class="input-group-append">
-                                          <div class="input-group-text"><span class="fas fa-bus"></span></div>
-                                      </div>
-                                  </div>
-                                  <div class="input-group mb-3">
-                                      <input type="text"
-                                             name="bus_type"
-                                             class="form-control"
-                                             value="{{ old('bus_type') }}"
-                                             placeholder="AC/ Non-AC/ Volvo/ স্ক্যানিয়া/ ডাবল ডেকার ইত্যাদি" required>
-                                      <div class="input-group-append">
-                                          <div class="input-group-text"><span class="fas fa-bus"></span></div>
-                                      </div>
-                                  </div>
-                                  <div class="input-group mb-3">
-                                      <input type="text"
-                                             name="fare"
-                                             class="form-control"
-                                             value="{{ old('fare') }}"
-                                             placeholder="বাস ভাড়া" required>
-                                      <div class="input-group-append">
-                                          <div class="input-group-text"><span class="fas fa-bus"></span></div>
-                                      </div>
-                                  </div>
-                                  <div class="input-group mb-3">
-                                      <input type="text"
-                                             name="starting_time"
-                                             class="form-control"
-                                             value="{{ old('starting_time') }}"
-                                             placeholder="ছাড়ার সময়/ সময়সমূহ (একাধিক হলে কমা দিয়ে লিখুন)" required>
-                                      <div class="input-group-append">
-                                          <div class="input-group-text"><span class="fas fa-bus"></span></div>
-                                      </div>
-                                  </div>
-                                  
-                                  <div class="input-group mb-3">
-                                      <input type="text"
-                                             name="contact"
-                                             value="{{ old('contact') }}"
-                                             class="form-control"
-                                             placeholder="যোগাযোগ" required>
-                                      <div class="input-group-append">
-                                          <div class="input-group-text"><span class="fas fa-mobile"></span></div>
-                                      </div>
-                                  </div>                                              
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                                <button type="submit" class="btn btn-primary">দাখিল করুন</button>
-                              </div>
-                            </form>
-                          </div>
-                        </div>
-                      </div>
-                      {{-- Edit User Modal Code --}}
-                      {{-- Edit User Modal Code --}}
-
                       <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteUserModal{{ $bus->id }}">
                         <i class="fas fa-trash-alt"></i>
                       </button>
                     </td>
+                    {{-- Edit User Modal Code --}}
+                    {{-- Edit User Modal Code --}}
+                    <!-- Modal -->
+                    <div class="modal fade" id="editUserModal{{ $bus->id }}" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true" data-backdrop="static">
+                      <div class="modal-dialog modal-lg" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header bg-primary">
+                            <h5 class="modal-title" id="editUserModalLabel">বাস তথ্য হালনাগাদ</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <form method="post" action="{{ route('dashboard.buses.update', [$district->id, $bus->id]) }}" enctype="multipart/form-data">
+                            <div class="modal-body">
+                                @csrf
+                                <h5>হতে জেলা: {{ $district->name_bangla }}</h5>
+                                <div class="" style="margin-bottom: 15px;">
+                                  <select name="to_district" class="form-control " required>
+                                      <option selected="" disabled="" value="">গন্তব্য জেলা নির্বাচন করুন</option>
+                                      @foreach($districts as $todistrict)
+                                        <option value="{{ $todistrict->id }}" @if($todistrict->id == $bus->toDistrict->id) selected @endif >{{ $todistrict->name_bangla }} - {{ $todistrict->name }}</option>
+                                      @endforeach
+                                  </select>
+                                  {{-- <div class="input-group-append">
+                                      <div class="input-group-text"><span class="fas fa-map"></span></div>
+                                  </div> --}}
+                                </div>
+                                
+                                <div class="input-group mb-3">
+                                    <input type="text"
+                                           name="bus_name"
+                                           class="form-control"
+                                           value="{{ old('bus_name') }}"
+                                           placeholder="বাসের নাম" required>
+                                    <div class="input-group-append">
+                                        <div class="input-group-text"><span class="fas fa-bus"></span></div>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input type="text"
+                                           name="route_info"
+                                           class="form-control"
+                                           value="{{ old('route_info') }}"
+                                           placeholder="রুটের তথ্য (যেমন: টাঙ্গাইল হতে দর্শনা ভায়া সিরাজগঞ্জ, উল্লাপাড়া, শাহজাদপুর, কাশিনাথপুর, পাবনা, কুষ্টিয়া, ঝিনাইদহ, চুয়াডাঙ্গা)" required>
+                                    <div class="input-group-append">
+                                        <div class="input-group-text"><span class="fas fa-bus"></span></div>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input type="text"
+                                           name="bus_type"
+                                           class="form-control"
+                                           value="{{ old('bus_type') }}"
+                                           placeholder="AC/ Non-AC/ Volvo/ স্ক্যানিয়া/ ডাবল ডেকার ইত্যাদি" required>
+                                    <div class="input-group-append">
+                                        <div class="input-group-text"><span class="fas fa-bus"></span></div>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input type="text"
+                                           name="fare"
+                                           class="form-control"
+                                           value="{{ old('fare') }}"
+                                           placeholder="বাস ভাড়া" required>
+                                    <div class="input-group-append">
+                                        <div class="input-group-text"><span class="fas fa-bus"></span></div>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input type="text"
+                                           name="starting_time"
+                                           class="form-control"
+                                           value="{{ old('starting_time') }}"
+                                           placeholder="ছাড়ার সময়/ সময়সমূহ (একাধিক হলে কমা দিয়ে লিখুন)" required>
+                                    <div class="input-group-append">
+                                        <div class="input-group-text"><span class="fas fa-bus"></span></div>
+                                    </div>
+                                </div>
+                                
+                                <div class="input-group mb-3">
+                                    <input type="text"
+                                           name="contact"
+                                           value="{{ old('contact') }}"
+                                           class="form-control"
+                                           placeholder="যোগাযোগ" required>
+                                    <div class="input-group-append">
+                                        <div class="input-group-text"><span class="fas fa-mobile"></span></div>
+                                    </div>
+                                </div>                                              
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
+                              <button type="submit" class="btn btn-primary">দাখিল করুন</button>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                    {{-- Edit User Modal Code --}}
+                    {{-- Edit User Modal Code --}}
                         {{-- Delete User Modal Code --}}
                         {{-- Delete User Modal Code --}}
                         <!-- Modal -->
