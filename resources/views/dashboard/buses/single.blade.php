8d94e26b970c5085894f3436f4090a633fbd9aec
Azimul-Haque <orbachinujbuk@gmail.com>
2024-07-17 02:06:21 +0600

    Commit Wed 07/17/2024  2:06:21.33 4291

--- resources/views/dashboard/buses/single.blade.php
+++ resources/views/dashboard/buses/single.blade.php
@@ -176,7 +176,7 @@
                   @csrf
 
                   <div class="input-group mb-3">
-                    <select name="district_id" id="district" class="form-control district" required>
+                    <select name="from_district" class="form-control district" required>
                         <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
                         @foreach($districts as $district)
                           <option value="{{ $district->id }}" @if($district->id == $ambulance->district_id) selected @endif>{{ $district->name_bangla }}</option>
