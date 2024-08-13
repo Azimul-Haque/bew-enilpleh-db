ded9a118a7c70c6f2c27a6215f05e395b2aa073f
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-14 02:06:23 +0600

    Commit Wed 08/14/2024  2:06:23.29 24039

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -230,6 +230,10 @@ class DoctorController extends Controller
         }
 
         if(isset($request->medicalsymptoms)){
+
+            foreach($doctor->doctormedicalsymptoms as $medicaldepartment) {
+                $medicaldepartment->delete();
+            }
             foreach($request->medicalsymptoms as $medicalsymptom_id) {
                 $doctormedicalsymptom = new Doctormedicalsymptom;
                 $doctormedicalsymptom->doctor_id = $doctor->id;
