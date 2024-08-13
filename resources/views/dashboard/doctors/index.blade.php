c190d5ed6aa7455c5e8b2e3a97766eee57903186
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-14 01:59:33 +0600

    Commit Wed 08/14/2024  1:59:33.28 5619

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -216,10 +216,10 @@
                                           </div>
                                           <center>
                                               @if($ambulance->ambulanceimage != null)
-                                                                                      <img src="{{ asset('images/ambulances/' . $ambulance->ambulanceimage->image)}}" style="width: 250px; height: auto;" class="img-responsive" />
-                                                                                    @else
-                                                                                      <img src="{{ asset('images/placeholder.png')}}" style="width: 250px; height: auto;" class="img-responsive" />
-                                                                                    @endif
+                                                <img src="{{ asset('images/ambulances/' . $ambulance->ambulanceimage->image)}}" style="width: 250px; height: auto;" class="img-responsive" />
+                                              @else
+                                                <img src="{{ asset('images/placeholder.png')}}" style="width: 250px; height: auto;" class="img-responsive" />
+                                              @endif
                                           </center>     
                                       
                                     </div>
