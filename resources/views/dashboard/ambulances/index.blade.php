acb7c67ccdeff28e17de9f6617824109b1dcab28
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-14 01:55:43 +0600

    Commit Wed 08/14/2024  1:55:43.30 3198

--- resources/views/dashboard/ambulances/index.blade.php
+++ resources/views/dashboard/ambulances/index.blade.php
@@ -117,7 +117,7 @@
                                     </div>  
                                     <div class="form-group">
                                         <label for="image">ছবি (প্রয়োজনে, ৩০০ x ৩০০ সাইজের, ২ মেগাবাইটের মধ্যে)</label>
-                                        <input type="file" id="image" name="image" accept="image/*">
+                                        <input type="file" name="image" accept="image/*">
                                     </div>
                                     <center>
                                       @if($ambulance->ambulanceimage != null)
