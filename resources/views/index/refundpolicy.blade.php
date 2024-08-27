7c979bdfebb11e8ab922962369d05156acfc1260
Azimul-Haque <orbachinujbuk@gmail.com>
2024-08-27 23:35:41 +0600

    Commit Tue 08/27/2024 23:35:41.38 7249

--- resources/views/index/refundpolicy.blade.php
+++ resources/views/index/refundpolicy.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.index')
-@section('title') Refund Policy | BCS Exam AID | বিসিএস-সহ সরকারি চাকরির পরীক্ষার প্রস্তুতির জন্য সেরা অনলাইন প্ল্যাটফর্ম @endsection
+@section('title') Refund Policy | Smart BD - Health & Services @endsection
 
 @section('third_party_stylesheets')
 
