e13404517dfc177e83c8632ff918b3b8b37bd61d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 02:13:15 +0600

    Commit Sat 10/26/2024  2:13:15.23 12466

--- resources/views/dashboard/journalists/single.blade.php
+++ resources/views/dashboard/journalists/single.blade.php
@@ -88,17 +88,6 @@
                                         </div>
                                     </div>
                                     <div class="input-group mb-3">
-                                      <select name="court_type" class="form-control" required>
-                                          <option selected="" disabled="" value="">কোর্টের ধরন</option>
-                                          <option value="1" @if($journalist->court_type == 1) selected @endif>ফৌজদারি</option>
-                                          <option value="2" @if($journalist->court_type == 2) selected @endif>দেওয়ানি</option>
-                                          <option value="3" @if($journalist->court_type == 3) selected @endif>ফৌজদারি ও দেওয়ানি</option>
-                                      </select>
-                                      <div class="input-group-append">
-                                          <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
-                                      </div>
-                                    </div>
-                                    <div class="input-group mb-3">
                                         <input type="number"
                                                name="mobile"
                                                value="{{ $journalist->mobile }}"
