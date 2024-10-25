9744db70bf59d8630dd4c3f92f6c62fe111130cf
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 02:20:49 +0600

    Commit Sat 10/26/2024  2:20:49.21 12071

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -784,7 +784,7 @@ class APIController extends Controller
     {
         if($softtoken == env('SOFT_TOKEN'))
         {
-            $journalists = Cache::remember('journalists'  . $district_id . $court_type, 30 * 24 * 60 * 60, function () use ($district_id, $court_type) {
+            $journalists = Cache::remember('journalists'  . $district_id, 30 * 24 * 60 * 60, function () use ($district_id, $court_type) {
                 $journalists = Journalist::where(function ($query) use ($district_id) {
                                     // return $query->where('district_id', $district_id);
                                     return $query->where('district_id', '<', '65');
