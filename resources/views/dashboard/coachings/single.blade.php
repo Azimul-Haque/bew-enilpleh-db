1ec0169a4164fdf7b7dbc9acaf9cf97e6de15a4b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-25 18:01:09 +0600

    Commit Fri 10/25/2024 18:01:09.26 5947

--- resources/views/dashboard/coachings/single.blade.php
+++ resources/views/dashboard/coachings/single.blade.php
@@ -84,6 +84,20 @@
                                             <div class="input-group-text"><span class="fas fa-user-tie"></span></div>
                                         </div>
                                     </div>
+
+                                    <div class="input-group mb-3">
+                                      <select name="hospital_type" class="form-control" required>
+                                          <option selected="" disabled="" value="">হাসপাতালের ধরন</option>
+                                          <option value="1" @if($hospital->hospital_type == 1) selected @endif>সরকারি হাসপাতাল</option>
+                                          <option value="2" @if($hospital->hospital_type == 2) selected @endif>প্রাইভেট ক্লিনিক ও হাসপাতাল</option>
+                                          <option value="3" @if($hospital->hospital_type == 3) selected @endif>ফিজিওথেরাপি সেন্টার</option>
+                                          <option value="4" @if($hospital->hospital_type == 4) selected @endif>কিডনি ডায়ালাইসিস</option>
+                                      </select>
+                                      <div class="input-group-append">
+                                          <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
+                                      </div>
+                                    </div>
+                                    
                                     <div class="input-group mb-3">
                                         <input type="number"
                                                name="mobile"
