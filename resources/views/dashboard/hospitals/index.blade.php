df3011dffad63dcf27f16b10a5f08b48dbeebabf
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 17:33:27 +0600

    Commit Sat 10/26/2024 17:33:27.22 10140

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -152,6 +152,19 @@
                                             </div>
                                         </div>
                                       </div>
+                                      <div class="col-md-12">
+                                        <div class="input-group mb-3">
+                                            <input type="text"
+                                                   name="location"
+                                                   value="{{ $hospital->location }}"
+                                                   autocomplete="off"
+                                                   class="form-control"
+                                                   placeholder="গুগল ম্যাপ লোকেশন লিংক" required>
+                                            <div class="input-group-append">
+                                                <div class="input-group-text"><span class="fas fa-map-marker-alt"></span></div>
+                                            </div>
+                                        </div>
+                                      </div>
                                       <div class="col-md-6">
                                         <div class="input-group mb-3">
                                           <textarea name="branch_data" class="form-control" style="min-height: 150px;" placeholder="ব্রাঞ্চের তালিকা (প্রযোজ্য ক্ষেত্রে)">{{ $hospital->branch_data }}</textarea>
@@ -162,19 +175,7 @@
                                           <textarea name="investigation_data" class="form-control" style="min-height: 150px;" placeholder="টেস্ট ইনভেস্টিগেশন তালিকা (প্রয়োজনে)">{{ $hospital->investigation_data }}</textarea>
                                         </div>
                                       </div>
-                                    </div>
-                                    <div class="input-group mb-3">
-                                        <input type="text"
-                                               name="location"
-                                               value="{{ $hospital->location }}"
-                                               autocomplete="off"
-                                               class="form-control"
-                                               placeholder="গুগল ম্যাপ লোকেশন লিংক" required>
-                                        <div class="input-group-append">
-                                            <div class="input-group-text"><span class="fas fa-map-marker-alt"></span></div>
-                                        </div>
-                                    </div>
-                                    
+                                    </div>                                    
                               </div>
                               <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
