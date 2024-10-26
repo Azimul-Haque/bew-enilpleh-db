4fff4e74730b9b4b350f007814bdc458d9922604
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 23:49:00 +0600

    Commit Sat 10/26/2024 23:49:00.20 26831

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -214,13 +214,13 @@
                                               <label for="image">ছবি/ ভিজিটিং কার্ড/ ব্যানার (প্রয়োজনে, ৩০০ h x ১৭৫ w সাইজের, ২ মেগাবাইটের মধ্যে)</label>
                                               <input type="file" name="image" accept="image/*">
                                           </div>
-                                          <center>
+                                          {{-- <center>
                                               @if($doctor->doctorimage != null)
                                                 <img src="{{ asset('images/doctors/' . $doctor->doctorimage->image)}}" style="width: 250px; height: auto;" class="img-responsive" />
                                               @else
                                                 <img src="{{ asset('images/placeholder.png')}}" style="width: 250px; height: auto;" class="img-responsive" />
                                               @endif
-                                          </center>     
+                                          </center>   --}}   
                                       
                                     </div>
                                     <div class="modal-footer">
