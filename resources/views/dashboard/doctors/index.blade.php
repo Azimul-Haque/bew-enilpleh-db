162906834fb30970ddcdfbc675b130d2e75d50a2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 23:23:02 +0600

    Commit Sat 10/26/2024 23:23:02.30 24564

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -569,6 +569,20 @@
                     @endforeach
                   </div><br/>
 
+                  <div class="col-md-6">
+                    <div class="input-group mb-3">
+                        <input type="number"
+                               name="mobile"
+                               value="{{ $hospital->mobile }}"
+                               autocomplete="off"
+                               class="form-control"
+                               placeholder="মোবাইল নম্বর" required>
+                        <div class="input-group-append">
+                            <div class="input-group-text"><span class="fas fa-mobile"></span></div>
+                        </div>
+                    </div>
+                  </div>
+
                   <div class="form-group ">
                       <label for="image">ছবি/ ভিজিটিং কার্ড/ ব্যানার (প্রয়োজনে, ৩০০ h x ১৭৫ w সাইজের, ২ মেগাবাইটের মধ্যে)</label>
                       <input type="file" id="image" name="image" accept="image/*">
