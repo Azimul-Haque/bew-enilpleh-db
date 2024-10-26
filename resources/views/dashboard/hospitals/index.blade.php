d45420581d51b72a8fb1884152e779d851455e64
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 17:17:42 +0600

    Commit Sat 10/26/2024 17:17:42.25 25535

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -318,7 +318,7 @@
                     </div>
                     <div class="col-md-6">
                       <div class="input-group mb-3">
-                        <textarea name="investigation_data" class="form-control" style="min-height: 150px;" placeholder="ব্রাঞ্চের তালিকা (প্রযোজ্য ক্ষেত্রে)"></textarea>
+                        <textarea name="investigation_data" class="form-control" style="min-height: 150px;" placeholder="টেস্ট ইনভেস্টিগেশন তালিকা"></textarea>
                       </div>
                     </div>
                   </div>
