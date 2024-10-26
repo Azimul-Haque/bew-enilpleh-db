1dc11ed9b469f43301e56ce8e881aa0bf32935dd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 23:50:20 +0600

    Commit Sat 10/26/2024 23:50:20.29 3303

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -214,7 +214,7 @@
                                             সপ্তাহে যে যে দিন রোগী দেখেন<br/>
                                             @foreach(['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] as $day)
                                               <div class="form-check form-check-inline">
-                                                <input class="form-check-input" type="checkbox" id="inlineCheckbox{{ $day }}" name="weekdays[]" value="{{ $day }}">
+                                                <input class="form-check-input" type="checkbox" id="inlineCheckbox{{ $day }}" name="weekdays[]" value="{{ $day }}" {{ in_array($day, $doctor->weekdays ?? []) ? 'checked' : '' }}>
                                                 <label class="form-check-label" for="inlineCheckbox{{ $day }}">{{ $day }}</label>
                                               </div>
                                             @endforeach
