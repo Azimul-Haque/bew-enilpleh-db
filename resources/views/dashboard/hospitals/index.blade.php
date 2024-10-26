1576512fe328468a4f1cfff86047de2c0bb694ca
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 17:05:59 +0600

    Commit Sat 10/26/2024 17:05:59.23 26994

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -310,7 +310,7 @@
                           </div>
                       </div>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 form-check form-check-inline">
                       @foreach(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] as $day)
                           <label>
                               <input type="checkbox" name="schedule[]">
