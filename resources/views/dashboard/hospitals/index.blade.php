1e02b941c28f3bd7ab2f1945adaa4f89fc7a1511
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 17:02:45 +0600

    Commit Sat 10/26/2024 17:02:45.21 19207

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -297,21 +297,24 @@
                           </div>
                       </div>
                     </div>
+                    <div class="col-md-6">
+                      <div class="input-group mb-3">
+                          <input type="text"
+                                 name="location"
+                                 value="{{ old('location') }}"
+                                 autocomplete="off"
+                                 class="form-control"
+                                 placeholder="গুগল ম্যাপ লোকেশন লিংক" required>
+                          <div class="input-group-append">
+                              <div class="input-group-text"><span class="fas fa-map-marker-alt"></span></div>
+                          </div>
+                      </div>
+                    </div>
                   </div>
 
                   
                   
-                  <div class="input-group mb-3">
-                      <input type="text"
-                             name="location"
-                             value="{{ old('location') }}"
-                             autocomplete="off"
-                             class="form-control"
-                             placeholder="গুগল ম্যাপ লোকেশন লিংক" required>
-                      <div class="input-group-append">
-                          <div class="input-group-text"><span class="fas fa-map-marker-alt"></span></div>
-                      </div>
-                  </div>            
+                              
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
