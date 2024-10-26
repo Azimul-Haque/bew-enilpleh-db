30497001de3294a2258b233e13d199aa46a6bafe
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 23:51:11 +0600

    Commit Sat 10/26/2024 23:51:11.27 16415

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -643,7 +643,7 @@
                     </div>
                     <div class="col-md-6">
                       <div class="input-group mb-3">
-                          <select name="timeto" id="timefrom" class="form-control">
+                          <select name="timeto" id="timeto" class="form-control">
                             <option value="" selected="" disabled="">রোগী দেখার সময় শেষ</option>
                               @for ($hour = 6; $hour <= 23; $hour++)
                                   @php
