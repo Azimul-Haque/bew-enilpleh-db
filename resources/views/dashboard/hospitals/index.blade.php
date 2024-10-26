850fb8ff65c784cf76a155029ae413e4b3acb50d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 17:32:03 +0600

    Commit Sat 10/26/2024 17:32:03.26 11331

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -153,16 +153,18 @@
                                         </div>
                                       </div>
                                     </div>
-                                    <div class="input-group mb-3">
-                                        <input type="text"
-                                               name="location"
-                                               value="{{ $hospital->location }}"
-                                               autocomplete="off"
-                                               class="form-control"
-                                               placeholder="গুগল ম্যাপ লোকেশন লিংক" required>
-                                        <div class="input-group-append">
-                                            <div class="input-group-text"><span class="fas fa-map-marker-alt"></span></div>
-                                        </div>
+                                    <div class="col-md-12">
+                                      <div class="input-group mb-3">
+                                          <input type="text"
+                                                 name="location"
+                                                 value="{{ $hospital->location }}"
+                                                 autocomplete="off"
+                                                 class="form-control"
+                                                 placeholder="গুগল ম্যাপ লোকেশন লিংক" required>
+                                          <div class="input-group-append">
+                                              <div class="input-group-text"><span class="fas fa-map-marker-alt"></span></div>
+                                          </div>
+                                      </div>
                                     </div>
                                     <div class="col-md-6">
                                       <div class="input-group mb-3">
