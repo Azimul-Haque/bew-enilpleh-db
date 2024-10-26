202c34ed241f2ae9003e7f4ec38eddf5bcb5a287
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 23:32:26 +0600

    Commit Sat 10/26/2024 23:32:26.28 13743

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -119,6 +119,8 @@ class DoctorController extends Controller
             'timeto'            => 'sometimes',
         ));
 
+        dd($request->all())
+
         $doctor = new Doctor;
         $doctor->district_id = $request->district_id;
         $doctor->upazilla_id = $request->upazilla_id;
