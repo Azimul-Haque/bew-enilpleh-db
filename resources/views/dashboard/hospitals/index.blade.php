ac69448e1d6b9c78396d6c3467c9f6f717e2aa58
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-10-26 17:01:31 +0600

    Commit Sat 10/26/2024 17:01:31.20 19696

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -286,20 +286,20 @@
                     </div>
                   </div>
                   <div class="col-md-6">
-                    
+                    <div class="input-group mb-3">
+                        <input type="number"
+                               name="mobile"
+                               value="{{ old('mobile') }}"
+                               autocomplete="off"
+                               class="form-control"
+                               placeholder="মোবাইল নম্বর" required>
+                        <div class="input-group-append">
+                            <div class="input-group-text"><span class="fas fa-mobile"></span></div>
+                        </div>
+                    </div>
                   </div>
                   
-                  <div class="input-group mb-3">
-                      <input type="number"
-                             name="mobile"
-                             value="{{ old('mobile') }}"
-                             autocomplete="off"
-                             class="form-control"
-                             placeholder="মোবাইল নম্বর" required>
-                      <div class="input-group-append">
-                          <div class="input-group-text"><span class="fas fa-mobile"></span></div>
-                      </div>
-                  </div>
+                  
                   <div class="input-group mb-3">
                       <input type="text"
                              name="location"
