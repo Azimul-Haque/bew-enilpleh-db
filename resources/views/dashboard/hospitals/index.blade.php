8bece5d5f2f40d7086a33043ec61455d4cb9a314
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-21 01:23:17 +0600

    Commit Thu 11/21/2024  1:23:17.20 15865

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -438,7 +438,7 @@
     {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
     <script type="text/javascript">
       $('.multiple-select').select2({
-        // theme: 'bootstrap4',
+        theme: 'bootstrap4',
       });
 
         $('.district').on('change', function() {
