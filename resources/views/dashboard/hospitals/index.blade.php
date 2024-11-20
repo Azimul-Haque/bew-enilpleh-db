db04bc00083b90b9261271f2e8ed0b721999d93e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-21 01:24:49 +0600

    Commit Thu 11/21/2024  1:24:49.22 9262

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -400,15 +400,16 @@
                     <div class="col-md-6">
                       <div class="input-group mb-3">
                         {{-- <textarea name="branch_data" class="form-control" style="min-height: 150px;" placeholder="ব্রাঞ্চের তালিকা (প্রযোজ্য ক্ষেত্রে)"></textarea> --}}
-                        <div style="margin-bottom: 15px;">
-                          <select name="branch_data[]" class="form-control multiple-select" multiple="multiple" data-placeholder="বিভাগ (প্রয়োজনে একাধিক সিলেক্ট করা যাবে)" required>
-                              
-                              @foreach($hospitals as $hospital)
-                                <option value="{{ $hospital->id }}">{{ $hospital->name }}</option>
-                              @endforeach
-                          </select>
-                        </div> 
+                        
                       </div>
+                      <div style="margin-bottom: 15px;">
+                        <select name="branch_data[]" class="form-control multiple-select" multiple="multiple" data-placeholder="বিভাগ (প্রয়োজনে একাধিক সিলেক্ট করা যাবে)" required>
+                            
+                            @foreach($hospitals as $hospital)
+                              <option value="{{ $hospital->id }}">{{ $hospital->name }}</option>
+                            @endforeach
+                        </select>
+                      </div> 
                     </div>
                     <div class="col-md-6">
                       <div class="input-group mb-3">
