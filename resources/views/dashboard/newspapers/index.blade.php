b300bb98e49e1b1151fa47ba4e5c3eb906b613f3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-23 22:35:27 +0600

    Commit Sat 11/23/2024 22:35:27.28 21727

--- resources/views/dashboard/newspapers/index.blade.php
+++ resources/views/dashboard/newspapers/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title') ড্যাশবোর্ড | ই-সেবা তালিকা @endsection
+@section('title') ড্যাশবোর্ড | দৈনিক পত্রিকা তালিকা @endsection
 
 @section('third_party_stylesheets')
    {{--  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
@@ -7,16 +7,16 @@
 @endsection
 
 @section('content')
-  @section('page-header') ই-সেবা তালিকা (মোট {{ bangla($eshebascount) }} টি) @endsection
+  @section('page-header') দৈনিক পত্রিকা তালিকা (মোট {{ bangla($eshebascount) }} টি) @endsection
     <div class="container-fluid">
     <div class="card">
           <div class="card-header">
-            <h3 class="card-title">ই-সেবা তালিকা</h3>
+            <h3 class="card-title">দৈনিক পত্রিকা তালিকা</h3>
 
             <div class="card-tools">
               <form class="form-inline form-group-lg" action="">
                 <div class="form-group">
-                  <input type="search-param" class="form-control form-control-sm" placeholder="ই-সেবা খুঁজুন" id="search-param" required>
+                  <input type="search-param" class="form-control form-control-sm" placeholder="দৈনিক পত্রিকা খুঁজুন" id="search-param" required>
                 </div>
                 <button type="button" id="search-button" class="btn btn-default btn-sm" style="margin-left: 5px;">
                   <i class="fas fa-search"></i> খুঁজুন
@@ -64,7 +64,7 @@
                         <div class="modal-dialog" role="document">
                           <div class="modal-content">
                             <div class="modal-header bg-primary">
-                              <h5 class="modal-title" id="editUserModalLabel">ই-সেবা তথ্য হালনাগাদ</h5>
+                              <h5 class="modal-title" id="editUserModalLabel">দৈনিক পত্রিকা তথ্য হালনাগাদ</h5>
                               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                               </button>
@@ -79,7 +79,7 @@
                                                name="name"
                                                class="form-control"
                                                value="{{ $esheba->name }}"
-                                               placeholder="ই-সেবার নাম" required>
+                                               placeholder="দৈনিক পত্রিকার নাম" required>
                                         <div class="input-group-append">
                                             <div class="input-group-text"><span class="fas fa-hand-holding-medical"></span></div>
                                         </div>
@@ -89,7 +89,7 @@
                                                name="url"
                                                value="{{ $esheba->url }}"
                                                class="form-control"
-                                               placeholder="ই-সেবা লিংক (URL)" required>
+                                               placeholder="দৈনিক পত্রিকা লিংক (URL)" required>
                                         <div class="input-group-append">
                                             <div class="input-group-text"><span class="fas fa-external-link-alt"></span></div>
                                         </div>
@@ -130,13 +130,13 @@
                           <div class="modal-dialog" role="document">
                             <div class="modal-content">
                               <div class="modal-header bg-danger">
-                                <h5 class="modal-title" id="deleteUserModalLabel">ই-সেবা ডিলেট</h5>
+                                <h5 class="modal-title" id="deleteUserModalLabel">দৈনিক পত্রিকা ডিলেট</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                   <span aria-hidden="true">&times;</span>
                                 </button>
                               </div>
                               <div class="modal-body">
-                                আপনি কি নিশ্চিতভাবে এই ই-সেবাকে ডিলেট করতে চান?<br/>
+                                আপনি কি নিশ্চিতভাবে এই দৈনিক পত্রিকাকে ডিলেট করতে চান?<br/>
                                 <center>
                                     <big><b>{{ $esheba->name }}</b></big><br/>
                                     <small><i class="fas fa-phone"></i> {{ $esheba->mobile }}</small>
@@ -169,7 +169,7 @@
       <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header bg-success">
-            <h5 class="modal-title" id="addUserModalLabel">নতুন ই-সেবা যোগ</h5>
+            <h5 class="modal-title" id="addUserModalLabel">নতুন দৈনিক পত্রিকা যোগ</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -184,7 +184,7 @@
                              name="name"
                              class="form-control"
                              value="{{ old('name') }}"
-                             placeholder="ই-সেবার নাম" required>
+                             placeholder="দৈনিক পত্রিকার নাম" required>
                       <div class="input-group-append">
                           <div class="input-group-text"><span class="fas fa-hand-holding-medical"></span></div>
                       </div>
@@ -194,7 +194,7 @@
                              name="url"
                              value="{{ old('url') }}"
                              class="form-control"
-                             placeholder="ই-সেবা লিংক (URL)" required>
+                             placeholder="দৈনিক পত্রিকা লিংক (URL)" required>
                       <div class="input-group-append">
                           <div class="input-group-text"><span class="fas fa-external-link-alt"></span></div>
                       </div>
