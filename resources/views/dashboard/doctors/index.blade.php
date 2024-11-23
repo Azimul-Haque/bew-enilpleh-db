0ba19e38b2678a9168a76a6dbc21f1b773c177ce
Numaira-Home-PC <orbachinujbuk@gmail.com>
2024-11-23 20:45:16 +0600

    Commit Sat 11/23/2024 20:45:16.21 4653

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -165,7 +165,7 @@
                                           </div>
                                           <div class="col-md-6">
                                             <div class="input-group mb-3">
-                                              <textarea name="degree" class="form-control" style="min-height: 110px;" placeholder="ডাক্তারের ডিগ্রি/ ডিগ্রিসমূহ (যেমন: MBBS, FCPS, MD, বিসিএস (স্বাস্থ্য) ইত্যাদি) [একাধিক লাইন এড করা যাবে]">{{ old('degree') }}</textarea>
+                                              <textarea name="degree" class="form-control" style="min-height: 110px;" placeholder="ডাক্তারের ডিগ্রি/ ডিগ্রিসমূহ (যেমন: MBBS, FCPS, MD, বিসিএস (স্বাস্থ্য) ইত্যাদি) [একাধিক লাইন এড করা যাবে]">{{ str_replace('<br />', "", $hospital->investigation_data) }}</textarea>
                                             </div>
                                           </div>
                                         </div>
