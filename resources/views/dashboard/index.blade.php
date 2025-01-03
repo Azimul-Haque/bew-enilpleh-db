87368de4933dcb3391076402881f0ac115a745b5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 18:25:30 +0600

    Commit Fri 01/03/2025 18:25:30.29 20203

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -22,7 +22,7 @@
 
           <div class="col-md-3">
             <a href="{{ route('dashboard.doctors') }}" class="info-box mb-3">
-              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-hospital"></i></span>
+              <span class="info-box-icon bg-success elevation-1"><i class="fa-user-md"></i></span>
 
               <div class="info-box-content">
                 <span class="info-box-text">ডাক্তার তালিকা</span>
