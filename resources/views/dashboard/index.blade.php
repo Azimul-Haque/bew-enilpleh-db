54ddf8fe3296ce39ee7dbb0056dfd5e82f7bc041
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 18:30:51 +0600

    Commit Fri 01/03/2025 18:30:51.26 13553

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -113,7 +113,7 @@
               <span class="info-box-icon bg-success elevation-1"><i class="fas fa-gavel"></i></span>
 
               <div class="info-box-content">
-                <span class="info-box-text">আইনজীবী তালিকা</span>
+                <span class="info-box-text">দৈনিক পত্রিকা তালিকা</span>
                 <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
               </div>
             </a>
