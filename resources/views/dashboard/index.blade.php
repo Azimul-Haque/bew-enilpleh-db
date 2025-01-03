b961be819da9223b625d0f1fc4b46c1df8f408b3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 18:23:13 +0600

    Commit Fri 01/03/2025 18:23:13.21 18135

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -15,7 +15,7 @@
 
               <div class="info-box-content">
                 <span class="info-box-text">হাসপাতাল তালিকা</span>
-                <small class="info-box-number">ক্লিক করুন</small>
+                <small class="info-box-text">ক্লিক করুন</small>
               </div>
             </a>
           </div>
