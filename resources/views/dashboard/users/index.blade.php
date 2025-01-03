3b42a4a0024b75ac88f2afb9aa0452e3c5900ff0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 16:44:45 +0600

    Commit Fri 01/03/2025 16:44:45.20 19173

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -180,9 +180,9 @@
             				                		<option disabled="" value="">ধরন নির্ধারণ করুন</option>
             				                		<option value="admin" @if($user->role == 'admin') selected="" @endif>এডমিন</option>
               													<option value="editor" @if($user->role == 'editor') selected="" @endif>এডিটর</option>
-                                        <option value="manager" @if($user->role == 'manager') selected="" @endif>ম্যানেজার</option>
+                                        {{-- <option value="manager" @if($user->role == 'manager') selected="" @endif>ম্যানেজার</option>
                                         <option value="volunteer" @if($user->role == 'volunteer') selected="" @endif>ভলান্টিয়ার</option>
-              													<option value="user" @if($user->role == 'user') selected="" @endif>ব্যবহারকারী</option>
+              													<option value="user" @if($user->role == 'user') selected="" @endif>ব্যবহারকারী</option> --}}
               													{{-- <option value="accountant" @if($user->role == 'accountant') selected="" @endif>একাউন্টেন্ট</option> --}}
             				                	</select>
             				                    <div class="input-group-append">
