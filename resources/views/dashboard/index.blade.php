fc9c1ffb5002a20f40a14c0c23dccc3a88258d5f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 18:28:06 +0600

    Commit Fri 01/03/2025 18:28:06.28 24376

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -58,7 +58,7 @@
               <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-hospital"></i></span>
 
               <div class="info-box-content">
-                <span class="info-box-text">হাসপাতাল তালিকা</span>
+                <span class="info-box-text">ই-সেবা তালিকা</span>
                 <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
               </div>
             </a>
