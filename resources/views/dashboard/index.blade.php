515d304a7ba21b5d35c2b4955859f45fd0f111d9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 18:24:20 +0600

    Commit Fri 01/03/2025 18:24:20.28 21054

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -44,7 +44,7 @@
 
           <div class="col-md-3">
             <a href="{{ route('dashboard.hospitals') }}" class="info-box mb-3">
-              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-hospital"></i></span>
+              <span class="info-box-icon bg-default elevation-1"><i class="fas fa-hospital"></i></span>
 
               <div class="info-box-content">
                 <span class="info-box-text">হাসপাতাল তালিকা</span>
