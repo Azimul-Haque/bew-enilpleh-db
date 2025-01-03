244f162117befb4dee0ee7b69e1ee5a9a5a74b1e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 22:10:04 +0600

    Commit Fri 01/03/2025 22:10:04.28 12368

--- resources/views/dashboard/blooddonors/index.blade.php
+++ resources/views/dashboard/blooddonors/index.blade.php
@@ -64,7 +64,7 @@
                     </td>
                     <td>
                       @if($blooddonor->category == 2)
-                        <a href="{{ route('dashboard.blooddonormembers', $blooddonor->id) }}" class="badge " title="সদস্য যোগ করতে বা এডিট করতে ক্লিক করুন" class=""><strong>সদস্য তালিকা দেখুন</strong></a>
+                        <a href="{{ route('dashboard.blooddonormembers', $blooddonor->id) }}" class="badge badge-warning" title="সদস্য যোগ করতে বা এডিট করতে ক্লিক করুন" class=""><strong>সদস্য তালিকা দেখুন</strong></a>
                       @endif
                     </td>
                     <td>
