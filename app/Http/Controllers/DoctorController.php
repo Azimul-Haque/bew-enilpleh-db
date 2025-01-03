c7703744d1623c7f7b00d5e910e86c9d1d36954e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 19:06:20 +0600

    Commit Fri 01/03/2025 19:06:20.22 28337

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -34,7 +34,7 @@ class DoctorController extends Controller
     public function __construct()
     {
         parent::__construct();
-        $this->middleware(['admin'])->only();
+        // $this->middleware(['admin'])->only('index', 'indexSearch');
     }
 
     public function index()
