f6e5da52970bd5bf3f211d4d9f07d8a75d1f5bab
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 22:26:28 +0600

    Commit Fri 01/03/2025 22:26:28.25 26482

--- resources/views/dashboard/blooddonors/members.blade.php
+++ resources/views/dashboard/blooddonors/members.blade.php
@@ -7,7 +7,7 @@
 @endsection
 
 @section('content')
-  @section('page-header')  রক্তদাতা তালিকা / {{ $blooddonor->name }} (মোট সদস্য - {{ bangla($blooddonormemberscount) }} জন) @endsection
+  @section('page-header')  <a href="">রক্তদাতা তালিকা</a> / {{ $blooddonor->name }} (মোট সদস্য - {{ bangla($blooddonormemberscount) }} জন) @endsection
     <div class="container-fluid">
     <div class="card">
           <div class="card-header">
