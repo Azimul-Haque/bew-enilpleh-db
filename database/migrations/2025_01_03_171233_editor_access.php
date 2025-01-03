efd272b7a70a026acc0cb0ac0b2f386afb8c8081
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 17:48:49 +0600

    Commit Fri 01/03/2025 17:48:49.26 5232

--- database/migrations/2025_01_03_171233_editor_access.php
+++ database/migrations/2025_01_03_171233_editor_access.php
@@ -17,8 +17,6 @@ class EditorAccess extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->morphs('accessible'); // polymorphic relation
-            $table->timestamps();
-
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
