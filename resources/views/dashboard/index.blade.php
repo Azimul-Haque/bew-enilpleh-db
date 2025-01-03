e479e462d40e6419e97b1a83957c7fdf02fba792
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 18:19:32 +0600

    Commit Fri 01/03/2025 18:19:32.19 31050

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,68 +9,19 @@
 	@section('page-header') ড্যাশবোর্ড @endsection
     <div class="container-fluid">
         <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h4>213<sup style="font-size: 20px">৳</sup></h4>
+          <div class="col-md-3">
+            <a href="{{ route('dashboard.deposit.getlist', [date('Y-m-d'), 'All']) }}" class="info-box mb-3">
+              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-coins"></i></span>
 
-                <p>মোট আয়</p>
+              <div class="info-box-content">
+                <span class="info-box-text">দৈনিক জমা</span>
+                <span class="info-box-number">৳ {{ 0 }}</span>
               </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              <a href="{{ route('dashboard.payments') }}" class="small-box-footer">আয় পাতা <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
+            </a>
           </div>
-          
-          {{-- <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h4>{{ $totalmonthlypayment->totalamount ? $totalmonthlypayment->totalamount : 0 }}<sup style="font-size: 20px">৳</sup></h4>
-
-                <p>মাসিক আয় ({{ date('F Y') }})</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-              </div>
-              <a href="{{ route('dashboard.payments') }}" class="small-box-footer">মাসিক আয় পাতা <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h4>{{ $totalexamsattendedtoday }} বার</h4>
-
-                <p>আজ মোট পরীক্ষায়  অংশগ্রহণ ({{ date("F d, Y") }})</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="#!" class="small-box-footer">পরীক্ষার্থী পাতা <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h4>{{ $totalusers }}</h4>
-
-                <p>মোট ব্যবহারকারী</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              <a href="{{ route('dashboard.users') }}" class="small-box-footer">ব্যবহারকারীগণ <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div> --}}
         </div>
         <div class="row">
-          <div class="col-md-6">
+          {{-- <div class="col-md-6">
             <a href="{{ route('dashboard.deposit.getlist', [date('Y-m-d'), 'All']) }}" class="info-box mb-3">
               <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-coins"></i></span>
 
@@ -89,7 +40,7 @@
                 <span class="info-box-number">৳ {{ 0 }}</span>
               </div>
             </a>
-          </div>
+          </div> --}}
         </div>
         <div class="row">
           <div class="col-md-6">
