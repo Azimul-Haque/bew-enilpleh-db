9abbb4454b5beb466bbec5f23af1bf6757c30494
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 22:20:01 +0600

    Commit Fri 01/03/2025 22:20:01.23 14398

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -215,6 +215,22 @@
                                       </select>
                                     </div>
 
+                                    <div style="margin-bottom: 15px;">
+                                      <select name="blooddonors[]" class="form-control multiple-select" multiple="multiple" data-placeholder="যে যে রক্তদাতা/সংগঠনের এক্সেস দেওয়া হবে (প্রয়োজনে একাধিক সিলেক্ট করা যাবে) [Optional]" onautocomplete="off">
+                                          @foreach($blooddonors as $blooddonor)
+                                            <option value="{{ $blooddonor->id }}">{{ $blooddonor->name }} - ({{ $blooddonor->upazilla->name_bangla }}, {{ $blooddonor->district->name_bangla }})</option>
+                                          @endforeach
+                                      </select>
+                                    </div>
+
+                                    <div style="margin-bottom: 15px;">
+                                      <select name="coachings[]" class="form-control multiple-select" multiple="multiple" data-placeholder="যে যে শিক্ষা প্রতিষ্ঠানের এক্সেস দেওয়া হবে (প্রয়োজনে একাধিক সিলেক্ট করা যাবে) [Optional]" onautocomplete="off">
+                                          @foreach($coachings as $coaching)
+                                            <option value="{{ $coaching->id }}">{{ $coaching->name }} - ({{ $coaching->district->name_bangla }})</option>
+                                          @endforeach
+                                      </select>
+                                    </div>
+
 
 
             				                <div class="input-group mb-3">
