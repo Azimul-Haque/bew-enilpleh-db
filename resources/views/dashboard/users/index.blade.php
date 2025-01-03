1d397007bb31bebd5bf2569847a3ea325105fdeb
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-03 17:40:15 +0600

    Commit Fri 01/03/2025 17:40:15.29 228

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -201,7 +201,7 @@
                                     <div style="margin-bottom: 15px;">
                                       <select name="hospitals[]" class="form-control multiple-select" multiple="multiple" data-placeholder="ডাক্তার যে হাসপাতালের সাথে সম্পৃক্ত (প্রয়োজনে একাধিক সিলেক্ট করা যাবে)" required>
                                           @foreach($hospitals as $hospital)
-                                            <option value="{{ $hospital->id }}" @if(in_array($hospital->id, $doctor->doctorhospitals->pluck('hospital_id')->toArray())) selected @endif>{{ $hospital->name }} - ({{ $hospital->upazilla->name_bangla }}, {{ $hospital->district->name_bangla }})</option>
+                                            <option value="{{ $hospital->id }}" @if($user->accessibleEntities->contains($hospital)) selected @endif>{{ $hospital->name }} - ({{ $hospital->upazilla->name_bangla }}, {{ $hospital->district->name_bangla }})</option>
                                           @endforeach
                                       </select>
                                     </div>
