dab9e07429e0a95cf2c2e4431bd58573f9767ec0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-05 01:27:56 +0600

    Commit Sun 01/05/2025  1:27:56.28 31136

--- database/migrations/2022_09_27_005558_create_messages_table.php
+++ database/migrations/2022_09_27_005558_create_messages_table.php
@@ -15,7 +15,6 @@ class CreateMessagesTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
             $table->string('message');
             $table->integer('status');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
