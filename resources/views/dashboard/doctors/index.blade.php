318b5b102fa0770a4cbfa6efc1e9efa8f67caa53
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-05 02:48:03 +0600

    Commit Sun 01/05/2025  2:48:03.22 32062

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -89,7 +89,7 @@
                               <i class="fas fa-trash-alt"></i>
                             </button><br/>
                              <button type="button" style="margin-top: 5px;" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#deleteUserModal{{ $doctor->id }}">
-                              অ্যাপয়েন্ট তালিকা
+                              <b>অ্যাপয়েন্ট তালিকা</b>
                             </button>
                           </td>
 
