18da54f822aea88fab36ea9da68d413e92cd9b48
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-05 02:57:00 +0600

    Commit Sun 01/05/2025  2:57:00.19 12503

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -394,7 +394,7 @@ class DoctorController extends Controller
 
     public function doctorSerialIndex()
     {
-        $doctorserials = Doctorserials::all();
+        $doctorserials = Doctorserial::all();
         $medicaldepartments = Medicaldepartment::all();
         $medicalsymptoms = Medicalsymptom::all();
         $hospitals = Hospital::all();
