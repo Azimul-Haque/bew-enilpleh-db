258a441e179f66a545a2dcbb5464b762eb449721
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-05 02:47:07 +0600

    Commit Sun 01/05/2025  2:47:07.26 11242

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -88,7 +88,7 @@
                             <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteUserModal{{ $doctor->id }}" disabled>
                               <i class="fas fa-trash-alt"></i>
                             </button><br/>
-                             <button type="button" style="margin-top: 5px;" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteUserModal{{ $doctor->id }}">
+                             <button type="button" style="margin-top: 5px;" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#deleteUserModal{{ $doctor->id }}">
                               <i class="fas fa-trash-alt"></i>
                             </button>
                           </td>
