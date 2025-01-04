4d1a59df971c1fb6b68f980502219ebd384fa5b2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-05 03:09:11 +0600

    Commit Sun 01/05/2025  3:09:11.26 22994

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -44,12 +44,12 @@
                       @foreach($doctorserials as $doctorserial)
                         <tr>
                           <td>
-                            {{ $doctor->name }}<br/>
-                            <span style="font-size: 12px;">{{ $doctor->degree }}</span>
-                            <span class="badge bg-primary">{{ $doctor->specialization }}</span>
+                            {{ $doctorserial->name }}<br/>
+                            <span style="font-size: 12px;">{{ $doctorserial->degree }}</span>
+                            <span class="badge bg-primary">{{ $doctorserial->specialization }}</span>
                             <br/>
-                            <small class="text-black-50"><i class="fas fa-phone"></i> {{ $doctor->serial }}</small>
-                            <small class="text-black-50"><i class="fas fa-mobile"></i> {{ $doctor->helpline }}</small>
+                            <small class="text-black-50"><i class="fas fa-phone"></i> {{ $doctorserial->serial }}</small>
+                            <small class="text-black-50"><i class="fas fa-mobile"></i> {{ $doctorserial->helpline }}</small>
                             
                           </td>
                           <td>
