ef6c105fc18e287780c592a5ecf396f54d8ac39a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-04 06:47:21 +0600

    Commit Sat 01/04/2025  6:47:21.22 10020

--- database/migrations/2024_05_31_225041_create_ambulances_table.php
+++ database/migrations/2024_05_31_225041_create_ambulances_table.php
@@ -19,6 +19,7 @@ class CreateAmbulancesTable extends Migration
             $table->integer('upazilla_id')->unsigned();
             $table->string('name');
             $table->string('mobile');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
