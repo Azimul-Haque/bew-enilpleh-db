8c9b2e15a76bc324a9437e6376cda654df3445c9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-04 07:00:40 +0600

    Commit Sat 01/04/2025  7:00:40.23 24856

--- resources/views/dashboard/ambulances/index.blade.php
+++ resources/views/dashboard/ambulances/index.blade.php
@@ -122,7 +122,7 @@
                                           <div class="input-group-text"><span class="fas fa-mobile"></span></div>
                                       </div>
                                   </div>
-                                  <textarea class="form-control" name="description" placeholder="বক্স এর জন্য বার্তা লিখুন (Optional)">{{ $ambulance->description }}</textarea>    
+                                  <textarea class="form-control" name="description" placeholder="বক্স এর জন্য বার্তা লিখুন (Optional)">{{ $ambulance->description }}</textarea><br/>    
                                   <div class="form-group">
                                       <label for="image">ছবি (প্রয়োজনে, ৩০০ x ৩০০ সাইজের, ২ মেগাবাইটের মধ্যে)</label>
                                       <input type="file" name="image" accept="image/*">
