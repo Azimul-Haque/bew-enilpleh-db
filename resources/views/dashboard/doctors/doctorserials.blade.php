6d77065d097d52844fba02ca675c81ac8c9739e1
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-05 03:13:22 +0600

    Commit Sun 01/05/2025  3:13:22.27 26370

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -6,13 +6,13 @@
 @endsection
 
 @section('content')
-  @section('page-header') {{ $doctor->name }} অ্যাপয়েন্টমেন্ট তালিকা @endsection
+  @section('page-header') {{ $doctor->name }}-এর অ্যাপয়েন্টমেন্ট তালিকা @endsection
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-9">
           <div class="card">
                 <div class="card-header">
-                  <h3 class="card-title">ডাক্তারের অ্যাপয়েন্টমেন্ট তালিকা (তারিখ: {{ $todaydate }})</h3>
+                  <h3 class="card-title">{{ $doctor->name }}-এর অ্যাপয়েন্টমেন্ট তালিকা (তারিখ: {{ $todaydate }})</h3>
 
                   <div class="card-tools">
                     <form class="form-inline form-group-lg" action="">
