a08b8d108ea003c6d5953c57b5a81d1a9d3668f4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-04 06:31:21 +0600

    Commit Sat 01/04/2025  6:31:21.27 24695

--- resources/views/dashboard/blooddonors/index.blade.php
+++ resources/views/dashboard/blooddonors/index.blade.php
@@ -144,7 +144,7 @@
                                             <div class="input-group-text"><span class="fas fa-mobile"></span></div>
                                         </div>
                                     </div>    
-                                    <textarea class="form-control" name="description" placeholder="বক্স এর জন্য বার্তা লিখুন (Optional)">{{ old('description') }}</textarea>        
+                                    <textarea class="form-control" name="description" placeholder="বক্স এর জন্য বার্তা লিখুন (Optional)">{{ $blooddonor->description }}</textarea>        
                                 
                               </div>
                               <div class="modal-footer">
