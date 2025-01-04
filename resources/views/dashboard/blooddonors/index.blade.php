598135dffc71817b459da13ff6e38849f1ce0cf9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-04 06:30:36 +0600

    Commit Sat 01/04/2025  6:30:36.27 18620

--- resources/views/dashboard/blooddonors/index.blade.php
+++ resources/views/dashboard/blooddonors/index.blade.php
@@ -266,7 +266,7 @@
                           <div class="input-group-text"><span class="fas fa-mobile"></span></div>
                       </div>
                   </div>
-                  <textarea class="form-control" name="description" placeholder="asds"></textarea>           
+                  <textarea class="form-control" name="description" placeholder="বক্স এর জন্য বার্তা লিখুন (Optional)"></textarea>           
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
