1313f54e15c046756388ca3632c5c115f18e0c0f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-06 04:30:48 +0600

    Commit Mon 01/06/2025  4:30:48.29 30066

--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -103,7 +103,7 @@ class HospitalController extends Controller
             'mobile'              => 'sometimes',
             'location'            => 'required',
             'description'            => 'sometimes|max:500',
-            'doctors'            => 'sometimes',
+            'doctors_ids'            => 'sometimes',
             'website'            => 'sometimes|max:191',
             'address'            => 'required',
             'branch_data'            => 'sometimes',
