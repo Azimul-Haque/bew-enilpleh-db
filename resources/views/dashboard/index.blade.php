e9147d3e9b657644860aa577e7a9cbeae7ae8e82
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-06 03:52:40 +0600

    Commit Mon 01/06/2025  3:52:40.21 14924

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -10,18 +10,17 @@
     <div class="container-fluid">
         <div class="row">
           @if(in_array('hospitals', Auth::user()->accessibleTables()))
+            <div class="col-md-3">
+              <a href="{{ route('dashboard.hospitals') }}" class="info-box mb-3">
+                <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-hospital"></i></span>
 
+                <div class="info-box-content">
+                  <span class="info-box-text">হাসপাতাল তালিকা</span>
+                  <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
+                </div>
+              </a>
+            </div>
           @endif
-          <div class="col-md-3">
-            <a href="{{ route('dashboard.hospitals') }}" class="info-box mb-3">
-              <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-hospital"></i></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">হাসপাতাল তালিকা</span>
-                <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
-              </div>
-            </a>
-          </div>
 
           <div class="col-md-3">
             <a href="{{ route('dashboard.doctors') }}" class="info-box mb-3">
