ff8d1be9235fa7c4835000d5e1d869dc72507470
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-06 04:54:41 +0600

    Commit Mon 01/06/2025  4:54:41.26 8509

--- resources/views/dashboard/coachings/single.blade.php
+++ resources/views/dashboard/coachings/single.blade.php
@@ -14,6 +14,7 @@
             <h3 class="card-title">শিক্ষা প্রতিষ্ঠান তালিকা (সরকারি/বেসরকারি/কোচিং)</h3>
 
             <div class="card-tools">
+              @if(Auth::user()->role == 'admin')
               <form class="form-inline form-group-lg" action="">
                 <div class="form-group">
                   <input type="search-param" class="form-control form-control-sm" placeholder="শিক্ষা প্রতিষ্ঠান খুঁজুন" id="search-param" required>
@@ -28,6 +29,7 @@
                   <i class="fas fa-plus-square"></i> নতুন
                 </button>
               </form>
+              @endif
               
             </div>
           </div>
