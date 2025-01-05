e4a1197b148396bed913348f81ae0db8a1d40188
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-06 03:21:38 +0600

    Commit Mon 01/06/2025  3:21:38.20 6592

--- resources/views/dashboard/doctors/pdf/serials.blade.php
+++ resources/views/dashboard/doctors/pdf/serials.blade.php
@@ -38,8 +38,8 @@
       <img src="{{ public_path('images/logo.png') }}" style="height: 40px; width: auto;"><br/>
       <span class="calibri_normal"><b>Infoline</b> - BD Smart Seba</span><br/>
     </h3>
-    <h3 style="color: #397736; border-bottom: 1px solid #397736;">
-        <center><b>{{ $doctor->name }}</b></center>
+    <h3 style="color: #397736; border-bottom: 1px solid #397736;" align="center">
+        <b>{{ $doctor->name }}</b>
     </h3>
     
 
