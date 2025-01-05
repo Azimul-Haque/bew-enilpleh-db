c3ecf413703c8583cf9572cb9d44de24f085ffc6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-06 04:19:30 +0600

    Commit Mon 01/06/2025  4:19:30.23 6809

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -444,8 +444,7 @@
 
                       <div style="margin-bottom: 15px;">
                         <select name="branch_ids[]" class="form-control multiple-select" multiple="multiple" data-placeholder="শাখা হাসপাতাল (প্রয়োজনে একাধিক সিলেক্ট করা যাবে)">
-                            
-                            @foreach($hospitals as $hospital)
+                            @foreach($allhospitals as $hospital)
                               <option value="{{ $hospital->id }}">{{ $hospital->name }}</option>
                             @endforeach
                         </select>
