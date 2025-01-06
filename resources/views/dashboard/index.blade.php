70f95a1910dd1117b2be6ccfe2dea3cfa0482b65
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-07 02:09:08 +0600

    Commit Tue 01/07/2025  2:09:08.22 14578

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -22,7 +22,7 @@
             </div>
           @endif
 
-          @if(Auth::user()->role == 'admin' || in_array('doctors', Auth::user()->accessibleTables()))
+          @if(Auth::user()->role == 'admin' || in_array('doctors', Auth::user()->accessibleTables()) || in_array('hospitals', Auth::user()->accessibleTables()))
           <div class="col-md-3">
             <a href="{{ route('dashboard.doctors') }}" class="info-box mb-3">
               <span class="info-box-icon bg-success elevation-1"><i class="fas fa-user-md"></i></span>
