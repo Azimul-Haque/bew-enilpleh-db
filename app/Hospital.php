49221573f7716dca79018c00a8a99726530880c8
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-07 01:50:58 +0600

    Commit Tue 01/07/2025  1:50:58.20 8395

--- app/Hospital.php
+++ app/Hospital.php
@@ -20,7 +20,7 @@ class Hospital extends Model
     }
 
     public function doctorhospitals(){
-        return $this->belongsToMany('App\Doctorhospital');
+        return $this->hasMany('App\Doctorhospital');
     }
 
     public function hospitalimages(){
