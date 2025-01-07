9d92636e861f5d8ea57e9320993f474fb85f53be
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-08 02:07:19 +0600

    Commit Wed 01/08/2025  2:07:19.22 10736

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -121,7 +121,7 @@ class DoctorController extends Controller
             'upazilla_id'            => 'required',
             'name'                => 'required|string|max:191',
             'degree'                => 'required|string|max:191',
-            'specialization'                => 'sometimes|string|max:191',
+            'specialization'                => 'sometimes|max:191',
             'serial'           => 'required',
             'address'           => 'required',
             'helpline'              => 'sometimes',
