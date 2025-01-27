9bbb22babd1f87fcb665f77d9ee5b1d45c93d625
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-28 02:06:02 +0600

    Commit Tue 01/28/2025  2:06:02.20 31587

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -540,7 +540,7 @@
                       </div>
                     </div>
                     <div class="col-md-6">
-                      <div class="input-group mb-3">
+                      {{-- <div class="input-group mb-3">
                           <input type="number"
                                  name="helpline"
                                  value="{{ old('helpline') }}"
@@ -549,17 +549,18 @@
                           <div class="input-group-append">
                               <div class="input-group-text"><span class="fas fa-mobile"></span></div>
                           </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="input-group mb-3">
+                      </div> --}}
+                      <div class="input-group mb-3">
                       <input type="text"
                              name="address"
                              value="{{ old('address') }}"
                              class="form-control"
                              placeholder="চেম্বারের ঠিকানা" required>
                   </div>
+                    </div>
+                  </div>
+
+                  
                   
                   <div style="margin-bottom: 15px;">
                     <select name="medicaldepartments[]" class="form-control multiple-select" multiple="multiple" data-placeholder="বিভাগ (প্রয়োজনে একাধিক সিলেক্ট করা যাবে)" required>
