ce42611e852d213ffb0e84f2f1a686a3e0368083
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-28 01:07:52 +0600

    Commit Tue 01/28/2025  1:07:52.21 31586

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -106,7 +106,7 @@
                                       </select>
                                     </div>
                                     <div class="col-md-6">
-                                      <select name="upazilla_id" id="upazilla" class="form-control upazilla" required>
+                                      <select name="upazilla_id" id="upazilla" class="form-control upazilla" data-placeholder="উপজেলা সিলেক্ট করুন" required>
                                           <option selected="" value="{{ $hospital->upazilla_id }}">{{ $hospital->upazilla->name_bangla }}</option>
                                       </select>
                                     </div>
