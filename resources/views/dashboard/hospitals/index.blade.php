338db196b36104f2dd179f03834b907a00046879
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-28 01:04:13 +0600

    Commit Tue 01/28/2025  1:04:13.23 25379

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -460,15 +460,7 @@
                         </select>
                       </div>
 
-                      <div class="form-group" style="margin-top: 15px;">
-                          <label for="image">ব্যানার-১ (Optional, Max 1 MB)</label>
-                          <input type="file" name="image1" accept="image/*">
-                      </div>
-
-                      <div class="form-group" style="margin-top: 15px;">
-                          <label for="image">ব্যানার-২ (Optional, Max 1 MB)</label>
-                          <input type="file" name="image2" accept="image/*">
-                      </div>
+                      
                     </div>                    
                     
                     <div class="col-md-12">
@@ -482,6 +474,20 @@
                           <div class="input-group-append">
                               <div class="input-group-text"><span class="fas fa-edit"></span></div>
                           </div>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="input-group mb-3">
+                        <textarea name="investigation_data" class="form-control" style="min-height: 150px;" placeholder="টেস্ট ইনভেস্টিগেশন তালিকা (প্রয়োজনে)"></textarea>
+                      </div>
+
+                      <div class="form-group" style="margin-top: 15px;">
+                          <label for="image">ব্যানার-৩ (Optional, Max 1 MB)</label>
+                          <input type="file" name="image3" accept="image/*">
+                      </div>
+                      <div class="form-group" style="margin-top: 15px;">
+                          <label for="image">ব্যানার-৪ (Optional, Max 1 MB)</label>
+                          <input type="file" name="image4" accept="image/*">
                       </div>
                     </div>
                     <div class="col-md-6">
