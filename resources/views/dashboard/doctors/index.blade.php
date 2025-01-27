5e7672acd53d963d623c913bb70a4c07c86ed9ad
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-28 01:44:54 +0600

    Commit Tue 01/28/2025  1:44:54.24 18061

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -522,7 +522,7 @@
 
                   <div class="row">
                     <div class="col-md-6">
-                      <select name="district_id" id="district" class="form-control district" required>
+                      <select name="district_id" id="district" class="form-control district select21" required>
                           <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
                           @foreach($districts as $district)
                             <option value="{{ $district->id }}">{{ $district->name_bangla }}</option>
@@ -782,6 +782,9 @@
 @section('third_party_scripts')
     <script type="text/javascript">
         $('.multiple-select').select2({
+          // theme: 'bootstrap4',
+        });
+        $('.select21').select2({
           // theme: 'bootstrap4',
         });
 
