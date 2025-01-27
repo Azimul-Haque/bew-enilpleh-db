05a6550bc2adef652ba391a6330064f78623f3e8
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-28 00:53:09 +0600

    Commit Tue 01/28/2025  0:53:09.21 8980

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -329,7 +329,7 @@
                   <div class="row">
                     <div class="col-md-6">
                       <select name="district_id" id="district" class="form-control district select21" data-placeholder="জেলা নির্বাচন করুন" required>
-                            <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
+                            {{-- <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option> --}}
                             @foreach($districts as $district)
                               <option value="{{ $district->id }}">{{ $district->name_bangla }}</option>
                             @endforeach
