1b4af75bfaadecb477c7d0b4248e7be637ef97f7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-28 00:57:56 +0600

    Commit Tue 01/28/2025  0:57:56.25 22060

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -12,16 +12,7 @@
          background-color: rgba(0, 123, 255) !important;
      }
 
-     .select2-selection__rendered {
-          padding: 0px 5px !important;
-      }
-      .select2-container .select2-selection--single {
-          height: 30px !important;
-      }
-      .select2-selection__arrow {
-          height: 30px !important;
-          top: 0px !important;
-      }
+     
    </style>
 @endsection
 
