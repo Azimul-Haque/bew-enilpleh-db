13bde32c05a18a29b44fc6374b34fee464cd7589
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-28 00:58:36 +0600

    Commit Tue 01/28/2025  0:58:36.25 7883

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -354,6 +354,7 @@
                         </div>
                       </div> --}}
                     </div>
+                    <br/>
                     <div class="col-md-6">
                       <div class="input-group mb-3">
                           <input type="text"
