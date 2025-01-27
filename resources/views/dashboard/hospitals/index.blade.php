3c269b51c243e23126cf140af33ff227fa09acf1
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-28 01:08:21 +0600

    Commit Tue 01/28/2025  1:08:21.26 20215

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -101,7 +101,7 @@
                                       <select name="district_id" id="district" class="form-control district select21" data-placeholder="জেলা নির্বাচন করুন" required>
                                           <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
                                           @foreach($districts as $district)
-                                            <option value="{{ $district->id }}" @if($district->id == $hospital->district_id) selected @endif>{{ $district->name_bangla }}</option>
+                                            <option value="{{ $district->id }}" @if($district->id == $hospital->district_id) selected @endif>{{ $district->name_bangla }}-{{ $district->name }}</option>
                                           @endforeach
                                       </select>
                                     </div>
