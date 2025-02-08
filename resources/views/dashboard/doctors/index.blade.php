5f27e9943236728439402bf9cd0047bfa850c666
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-09 00:30:24 +0600

    Commit Sun 02/09/2025  0:30:24.21 21808

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -618,7 +618,7 @@
                     </div>
                   </div>
                   <div style="margin-top: 15px;">
-                    <select name="medicalsymptoms[]" class="form-control multiple-select" multiple="multiple" data-placeholder="যেদিন যেদিন রোগী দেখবেন না" required>
+                    <select name="medicalsymptoms[]" class="form-control multiple-select" multiple="multiple" data-placeholder="যেদিন যেদিন রোগী দেখবেন না (প্রয়োজনে একাধিক সিলেক্ট করা যাবে) [Optional]" required>
                         
                         @foreach($medicalsymptoms as $medicalsymptom)
                           <option value="{{ $medicalsymptom->id }}">{{ $medicalsymptom->name }}</option>
