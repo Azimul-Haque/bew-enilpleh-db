b93ea467fd93ac45c6803e034f2a90e692ef2106
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-08 23:23:10 +0600

    Commit Sat 02/08/2025 23:23:10.26 18481

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1534,9 +1534,8 @@ class APIController extends Controller
                 $mobile_number = substr(Auth::user()->mobile, -11);
             }
         }
-        // $url = config('sms.url');
-        // $number = $mobile_number;
-        $text = 'Dear ' . Auth::user()->name . ', payment of tk. '. $request->amount .' is submitted successfully. We will notify you once we approve it. Customs and VAT Co-operative Society (CVCS). Login: https://cvcsbd.com/login';
+
+        $text = 'Dear ' . $request->name . ', payment of tk. '. $request->amount .' is submitted successfully. We will notify you once we approve it. Customs and VAT Co-operative Society (CVCS). Login: https://cvcsbd.com/login';
         
         // NEW PANEL
         $url = config('sms.url');
