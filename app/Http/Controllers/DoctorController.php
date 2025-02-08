257cecdbead21c16ff935a80a2ed2c2136640d39
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-09 00:41:27 +0600

    Commit Sun 02/09/2025  0:41:27.30 21444

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -148,7 +148,7 @@ class DoctorController extends Controller
         }
         $doctor->weekdays = $request->weekdays;
         if($request->offdays) {
-            dd(json($request->offdays));
+            dd(json_encode($request->offdays));
             $doctor->offdays = $request->offdays;
         }
         $doctor->onlineserial = $request->onlineserial;
