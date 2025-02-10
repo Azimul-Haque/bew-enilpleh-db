e359a4dee0808a353719e44b534d60c91a31b2b7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-11 00:55:31 +0600

    Commit Tue 02/11/2025  0:55:31.24 18906

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -68,7 +68,7 @@
                             <div class="modal-dialog modal-md" role="document">
                               <div class="modal-content">
                                 <div class="modal-header bg-warning">
-                                  <h5 class="modal-title" id="editUserModalLabel">ক্যান্সেল মেসেজ পাঠান</h5>
+                                  <h5 class="modal-title" id="editUserModalLabel"><i class="fas fa-envelope"></i> ক্যান্সেল মেসেজ পাঠান</h5>
                                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                   </button>
