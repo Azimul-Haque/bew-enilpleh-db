1b9b8f3050927ff04ae996a5b555a49cb866fdf4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-11 01:41:20 +0600

    Commit Tue 02/11/2025  1:41:20.26 6357

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -617,6 +617,13 @@ class DoctorController extends Controller
         $response = curl_exec($ch);
         curl_close($ch);
         $jsonresponse = json_decode($response);
+        if($jsonresponse->response_code == 202) {
+            // Session::flash('success', 'SMS সফলভাবে পাঠানো হয়েছে!');
+        } elseif($jsonresponse->response_code == 1007) {
+            // Session::flash('warning', 'অপর্যাপ্ত SMS ব্যালেন্সের কারণে SMS পাঠানো যায়নি!');
+        } else {
+            // Session::flash('warning', 'দুঃখিত! SMS পাঠানো যায়নি!');
+        }
 
         ////////////////
         ////////////////
