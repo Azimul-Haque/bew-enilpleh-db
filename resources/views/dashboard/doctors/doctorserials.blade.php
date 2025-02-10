75f103f100f78d9a27c3189df08b346468d2ddfe
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-11 01:57:30 +0600

    Commit Tue 02/11/2025  1:57:30.22 2762

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -122,7 +122,7 @@
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <form method="post" action="{{ route('dashboard.doctorserialcancelall', [$doctorserial->doctor_id, $todaydate]) }}" enctype="multipart/form-data">
+          <form method="post" action="{{ route('dashboard.doctorserialcancelall', [$doctor->id, $todaydate]) }}" enctype="multipart/form-data">
             <div class="modal-body">
         
               @csrf
