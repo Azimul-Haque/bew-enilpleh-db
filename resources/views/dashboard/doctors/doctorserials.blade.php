bdcf8ee26cfd19da5946cd5be7266db746652064
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-11 00:51:35 +0600

    Commit Tue 02/11/2025  0:51:35.22 8642

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -67,7 +67,7 @@
                           <div class="modal fade" id="sendCancelSMSModal{{ $doctorserial->id }}" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true" data-backdrop="static">
                             <div class="modal-dialog modal-lg" role="document">
                               <div class="modal-content">
-                                <div class="modal-header bg-success">
+                                <div class="modal-header bg-warning">
                                   <h5 class="modal-title" id="editUserModalLabel">ক্যান্সেল মেসেজ পাঠান</h5>
                                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
