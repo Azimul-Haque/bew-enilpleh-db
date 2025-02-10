2b7bf9aa5c03361348dc0f6d3ee557ca263ee38e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-11 01:13:50 +0600

    Commit Tue 02/11/2025  1:13:50.26 2442

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -523,7 +523,7 @@ class DoctorController extends Controller
         }
 
         $text = "Appointment Cancelled!\n\n" .
-                "Dear " . $request->name . ", we are sorry to inform you that, your appointment with " . $serialdoctor->name . " is booked successfully.\n" .
+                "Dear " . $request->name . ", we are sorry to inform you that, your appointment with " . $serialdoctor->name . " on 11-02-2025 has been cancelled unfortunately.\n" .
                 "Date: " . date('d-m-Y', strtotime($request->serialdate)) . "\n" .
                 "Chamber: " . $serialdoctor->address . "\n\n" .
                 "Infoline - BD Smart Seba";
