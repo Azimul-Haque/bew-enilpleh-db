605bcb105287505a5abfafa5e98d7d1cb9d5610d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-11 00:59:11 +0600

    Commit Tue 02/11/2025  0:59:11.21 16399

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -79,7 +79,7 @@
                                         @csrf
                                         
                                         <div class="input-group mb-3">
-                                          <textarea name="message" class="form-control" style="min-height: 90px;" placeholder="মেসেজ লিখুন">Appointment Cancelled!&#10;&#10;</textarea>
+                                          <textarea name="message" class="form-control" style="min-height: 90px;" placeholder="মেসেজ লিখুন">Appointment Cancelled!&#10;&#10;Dear {{ $doctorserial->name }} appointment with</textarea>
                                         </div>
                                   </div>
                                   <div class="modal-footer">
