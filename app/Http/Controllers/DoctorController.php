e1c51437ee522caab40c382037e382ae5fe229dd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-11 01:39:32 +0600

    Commit Tue 02/11/2025  1:39:32.23 12935

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -584,7 +584,7 @@ class DoctorController extends Controller
 
         foreach ($doctorserials as $doctorserial) {
             $text = "Appointment Cancelled!\n\n" .
-                "Dear " . $doctorserial->name . ", we are sorry to inform you that, your appointment with " . $doctorserial->dontor->name . " on " . date('d-m-Y', strtotime($doctorserial->serialdate)) . " has been cancelled unfortunately.\n\n" .
+                "Dear " . $doctorserial->name . ", we are sorry to inform you that, your appointment with " . $doctorserial->doctor->name . " on " . date('d-m-Y', strtotime($doctorserial->serialdate)) . " has been cancelled unfortunately.\n\n" .
                 "Infoline - BD Smart Seba";
 
             $messagesArray[] = [
