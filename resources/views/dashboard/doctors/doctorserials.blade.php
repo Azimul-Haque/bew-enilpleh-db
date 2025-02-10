298c7b7374db01d4b5bfad9ab8b49c11def13d53
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-11 01:27:45 +0600

    Commit Tue 02/11/2025  1:27:45.23 19710

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -34,7 +34,7 @@
                         <i class="fas fa-print"></i> প্রিন্ট করুন
                       </a>
                       <button type="button" class="btn btn-warning btn-sm" style="margin-left: 5px;" title="সকলকে ক্যানসেল মেসেজ পাঠান" data-toggle="modal" data-target="#sendCancelSMSALLModal">
-                       ক্যানসেল মেসেজ
+                       <i class="fas fa-envelope"></i> ক্যানসেল মেসেজ
                      </button>
                     </form>
                     
