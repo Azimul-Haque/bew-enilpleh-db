bf254848de02a17ab76e0436e79e1c20aff58d3b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-11 01:31:18 +0600

    Commit Tue 02/11/2025  1:31:18.30 10288

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -126,7 +126,7 @@
             <div class="modal-body">
         
               @csrf
-              <textarea name="message" class="form-control" style="min-height: 250px;" placeholder="মেসেজ লিখুন" readonly="">Appointment Cancelled!&#10;&#10;Dear [Patient Name], we are sorry to inform you that, your appointment with [Dr Name] on [Date] has been cancelled unfortunately.&#10;&#10;Infoline - BD Smart Seba</textarea>
+              <textarea name="message" class="form-control" style="min-height: 250px;" placeholder="মেসেজ লিখুন" readonly="">Appointment Cancelled!&#10;&#10;Dear [Patient Name], we are sorry to inform you that, your appointment with [Doctor Name] on [Date] has been cancelled unfortunately.&#10;&#10;Infoline - BD Smart Seba</textarea>
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
