fa4b4711ec9e78088f3265205f64310d7e74d7c7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-13 03:54:53 +0600

    Commit Thu 02/13/2025  3:54:53.30 18739

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::post('/dashboard/doctors/{doctor_id}/appoinments/cancel/{date}', 'DoctorC
 Route::post('/dashboard/doctors/{doctor_id}/appoinments/cancel/{date}/all', 'DoctorController@doctorSerialCancelALL')->name('dashboard.doctorserialcancelall');
 Route::get('/dashboard/doctors/appoinments/pdf/{doctor_id}/{date}', 'DoctorController@getDoctorSerialPDF')->name('dashboard.getdoctorserialpdf');
 Route::post('/dashboard/doctors/{doctor_id}/appoinments/add/{date}/manually', 'DoctorController@addDoctorSerialManually')->name('dashboard.adddoctorserialmanually');
-Route::post('/dashboard/doctors/{doctor_id}/appoinments/delete/{date}', 'DoctorController@deleteDoctorSerial')->name('dashboard.deletedoctorserial');
+Route::post('/dashboard/doctors/appoinments/delete/{serial_id}', 'DoctorController@deleteDoctorSerial')->name('dashboard.deletedoctorserial');
 
 // Blood Donors
 Route::get('/dashboard/blooddonors', 'BlooddonorController@index')->name('dashboard.blooddonors');
