67432611d484fb8d3279b6df321734870253a7e5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-13 03:23:08 +0600

    Commit Thu 02/13/2025  3:23:08.29 14532

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -145,7 +145,7 @@
       <div class="modal-dialog modal-md" role="document">
         <div class="modal-content">
           <div class="modal-header bg-warning">
-            <h5 class="modal-title" id="addNewAppointmentModal"><i class="fas fa-envelope"></i> ক্যান্সেল মেসেজ পাঠান</h5>
+            <h5 class="modal-title" id="addNewAppointmentModal"><i class="fas fa-envelope"></i> নতুন সিরিয়াল যোগ করুন</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
