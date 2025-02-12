74319f341c453cb1af015db7933b12943a0802f2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-02-13 03:25:14 +0600

    Commit Thu 02/13/2025  3:25:14.35 31952

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -154,7 +154,9 @@
             <div class="modal-body">
         
               @csrf
-              <input type="text" class="form-control" name="name" placeholder="রোগীর নাম">
+              <input type="text" class="form-control" name="name" placeholder="রোগীর নাম" required>
+              
+              <input type="text" class="form-control" name="name" placeholder="রোগীর নাম" required>
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
