cf451780c5d82b51a38bcd6679ced68b8a32af82
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-03-15 03:52:24 +0600

    Commit Sat 03/15/2025  3:52:24.29 5155

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -290,7 +290,7 @@
                                               // print_r($datesString);
                                             }
                                           @endphp
-                                          <input type="text" id="selected_offdays" name="selected_offdays" class="selected_offdays form-control" placeholder="যেদিন যেদিন রোগী দেখবেন না (প্রয়োজনে একাধিক সিলেক্ট করা যাবে) [Optional]" value="2025-03-15" readonly>
+                                          <input type="text" id="selected_offdays" name="selected_offdays" class="selected_offdays form-control" placeholder="যেদিন যেদিন রোগী দেখবেন না (প্রয়োজনে একাধিক সিলেক্ট করা যাবে) [Optional]" value="{{ $datesString }}" readonly>
 
                                           {{-- <select name="offdays[]" class="form-control multiple-select" multiple="multiple" data-placeholder="যেদিন যেদিন রোগী দেখবেন না (প্রয়োজনে একাধিক সিলেক্ট করা যাবে) [Optional]">
                                               
