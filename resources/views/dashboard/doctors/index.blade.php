91d5b5600cfa67c57ffd18a00650eab097a952c0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-03-15 02:54:15 +0600

    Commit Sat 03/15/2025  2:54:15.23 30774

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -11,7 +11,7 @@
         .ui-state-highlight { background: #ffc107 !important; } /* Highlight selected dates */
     </style>
 
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
     <script src="{{ asset('js/select2.full.min.js') }}"></script>
