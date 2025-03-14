20db1c1bea6b88c5e4b82a745f665274fb9a24d6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-03-15 02:53:50 +0600

    Commit Sat 03/15/2025  2:53:50.28 26500

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -12,6 +12,8 @@
     </style>
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
     <script src="{{ asset('js/select2.full.min.js') }}"></script>
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js" defer></script> --}}
     <style type="text/css">
@@ -743,7 +745,7 @@
 @endsection
 
 @section('third_party_scripts')
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script type="text/javascript">
       $(document).ready(function () {
