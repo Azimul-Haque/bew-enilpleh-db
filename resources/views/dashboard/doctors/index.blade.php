daa6ba73492c589c66fd50cc20eb1f0a7e4c6902
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-03-15 03:49:51 +0600

    Commit Sat 03/15/2025  3:49:51.22 22902

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -288,7 +288,7 @@
                                           <select name="offdays[]" class="form-control multiple-select" multiple="multiple" data-placeholder="যেদিন যেদিন রোগী দেখবেন না (প্রয়োজনে একাধিক সিলেক্ট করা যাবে) [Optional]">
                                               @php
                                                 $decodedoffdays = json_decode($doctor->offdays, true);
-                                                // print_r($decodedoffdays);
+                                                print_r($decodedoffdays);
                                               @endphp
                                               @foreach($optiondates as $date)
                                                   <option value="{{ $date->format('Y-m-d') }}" @if(!empty($decodedoffdays) && in_array($date->format('Y-m-d'), $decodedoffdays, true)) selected @endif>
