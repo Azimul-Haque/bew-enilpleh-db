058d4357eca1676eac2360f013e31be317b65d35
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-03-15 03:08:49 +0600

    Commit Sat 03/15/2025  3:08:49.23 646

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -758,58 +758,40 @@
         autoclose: false,
         multidate: true,
       });
-      $(document).ready(function () {
-              // Ensure the modal and datepicker are initialized properly
-              $('#addUserModal').on('shown.bs.modal', function () {
-                  if (!$("#datepicker").data('datepicker')) {
-                      $("#datepicker").datepicker({
-                          format: "dd-mm-yyyy",
-                          multidate: true,
-                          autoclose: false
-                      });
-                  }
-              });
+      
+      $('#addUserModal').on('shown.bs.modal', function () {
+        console.log('asdasd');
+        $("#datepicker").datepicker({
+            dateFormat: "dd-mm-yy",
+            multidate: true,
+            beforeShowDay: function (date) {
+                let dateString = $.datepicker.formatDate('dd-mm-yy', date);
+                return [true, selectedDates.includes(dateString) ? "ui-state-highlight" : ""];
+            },
+            onSelect: function (dateText) {
+                let index = selectedDates.indexOf(dateText);
+                if (index === -1) {
+                    selectedDates.push(dateText); // Add selected date
+                } else {
+                    selectedDates.splice(index, 1); // Remove date if clicked again
+                }
+                $("#selected_offdays").val(selectedDates.join(", ")); // Show selected dates in text field
+                $(this).datepicker("refresh"); // Refresh to update highlights
+            }
+        });
 
-              // Save Selected Dates
-              $("#saveDates").click(function () {
-                  var selectedDates = $("#datepicker").val();
-                  $("#selected_offdays").val(selectedDates);
-                  $("#myModal").modal('hide');
-              });
-          });
-      // $('#addUserModal').on('shown.bs.modal', function () {
-      //   console.log('asdasd');
-      //   $("#datepicker").datepicker({
-      //       dateFormat: "dd-mm-yy",
-      //       multidate: true,
-      //       beforeShowDay: function (date) {
-      //           let dateString = $.datepicker.formatDate('dd-mm-yy', date);
-      //           return [true, selectedDates.includes(dateString) ? "ui-state-highlight" : ""];
-      //       },
-      //       onSelect: function (dateText) {
-      //           let index = selectedDates.indexOf(dateText);
-      //           if (index === -1) {
-      //               selectedDates.push(dateText); // Add selected date
-      //           } else {
-      //               selectedDates.splice(index, 1); // Remove date if clicked again
-      //           }
-      //           $("#selected_offdays").val(selectedDates.join(", ")); // Show selected dates in text field
-      //           $(this).datepicker("refresh"); // Refresh to update highlights
-      //       }
-      //   });
+        // Show datepicker when clicking the text field
+        $("#selected_offdays").focus(function() {
+            $("#datepicker").toggle();
+        });
 
-      //   // Show datepicker when clicking the text field
-      //   $("#selected_offdays").focus(function() {
-      //       $("#datepicker").toggle();
-      //   });
-
-      //   // Hide datepicker when clicking outside
-      //   $(document).click(function(event) {
-      //       if (!$(event.target).closest("#datepicker, #selected_offdays").length) {
-      //           $("#datepicker").hide();
-      //       }
-      //   });
-      // });
+        // Hide datepicker when clicking outside
+        $(document).click(function(event) {
+            if (!$(event.target).closest("#datepicker, #selected_offdays").length) {
+                $("#datepicker").hide();
+            }
+        });
+      });
       
 
 
