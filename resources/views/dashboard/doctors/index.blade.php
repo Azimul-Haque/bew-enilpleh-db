342b3281d03d9477cfdf13cf20f489ba889e52a6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-03-15 03:52:17 +0600

    Commit Sat 03/15/2025  3:52:17.23 30705

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -283,14 +283,15 @@
                                         </div>
 
                                         <div style="margin-top: 15px;">
-                                          <input type="text" id="selected_offdays" name="selected_offdays" class="selected_offdays form-control" placeholder="যেদিন যেদিন রোগী দেখবেন না (প্রয়োজনে একাধিক সিলেক্ট করা যাবে) [Optional]" value="2025-03-15" readonly>
                                           @php
                                             if($doctor->offdays) {
                                               $decodedoffdays = json_decode($doctor->offdays, true);
                                               $datesString = implode(',', $decodedoffdays);
-                                              print_r($datesString);
+                                              // print_r($datesString);
                                             }
                                           @endphp
+                                          <input type="text" id="selected_offdays" name="selected_offdays" class="selected_offdays form-control" placeholder="যেদিন যেদিন রোগী দেখবেন না (প্রয়োজনে একাধিক সিলেক্ট করা যাবে) [Optional]" value="2025-03-15" readonly>
+
                                           {{-- <select name="offdays[]" class="form-control multiple-select" multiple="multiple" data-placeholder="যেদিন যেদিন রোগী দেখবেন না (প্রয়োজনে একাধিক সিলেক্ট করা যাবে) [Optional]">
                                               
                                               @foreach($optiondates as $date)
