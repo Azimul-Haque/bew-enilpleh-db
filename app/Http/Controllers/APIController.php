3b328eccb27a399a01ddd3eaaf7a0ce0e472ca71
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-09 00:16:22 +0600

    Commit Wed 04/09/2025  0:16:22.30 9400

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -111,6 +111,7 @@ class APIController extends Controller
                         // $hospital->push($hospitalimagetemp);
                      }
                      $hospital->images = $imagestemp;
+                     dd($hospital->allBranches()->count());
                      $hospital->branchcount = $hospital->allBranches()->count();
                      $hospital->makeHidden('hospitalimages', 'district', 'upazilla', 'created_at', 'updated_at');
                      // dd($hospital);
