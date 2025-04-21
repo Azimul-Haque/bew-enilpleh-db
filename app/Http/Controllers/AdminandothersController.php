43d0f97c60c47138bb969da2f21cab53d3b26bf5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-22 02:28:55 +0600

    Commit Tue 04/22/2025  2:28:55.24 27911

--- app/Http/Controllers/AdminandothersController.php
+++ app/Http/Controllers/AdminandothersController.php
@@ -1132,25 +1132,11 @@ class AdminandothersController extends Controller
     {
         $this->validate($request,array(
             'counter_name'      => 'required|string|max:191',
-            'route_info'       => 'required|string',
-            'bus_type'         => 'required|string|max:191',
-            'fare'             => 'required|string|max:191',
-            'starting_time'    => 'required|string|max:191',
-            'counter_address'    => 'required|string|max:191',
-            'contact'          => 'required|string|max:191',
         ));
 
-        $bus = new Bus;
-        $bus->district_id = $district_id;
-        $bus->to_district = $request->to_district;
-        $bus->bus_name = $request->bus_name;
-        $bus->route_info = $request->route_info;
-        $bus->bus_type = $request->bus_type;
-        $bus->fare = $request->fare;
-        $bus->starting_time = $request->starting_time;
-        $bus->counter_address = $request->counter_address;
-        $bus->contact = $request->contact;
-        $bus->save();
+        $buscounter = new Buscounter;
+        $buscounter->counter_name = $request->counter_name;
+        $buscounter->save();
 
 
         Cache::forget('busesfrom' . $district_id);
