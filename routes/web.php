9a51c94291b9cdbf27d7f9bf9ec6062aef22b0b9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-22 02:25:48 +0600

    Commit Tue 04/22/2025  2:25:48.29 26716

--- routes/web.php
+++ routes/web.php
@@ -159,6 +159,7 @@ Route::get('/dashboard/buses/{district_id}/{search}', 'AdminandothersController@
 Route::post('/dashboard/buses/{district_id}/store', 'AdminandothersController@storeBus')->name('dashboard.buses.store');
 Route::post('/dashboard/buses/{district_id}/{id}/update', 'AdminandothersController@updateBus')->name('dashboard.buses.update');
 Route::get('/dashboard/buses/{district_id}/{id}/delete', 'AdminandothersController@updateBus')->name('dashboard.buses.delete');
+Route::get('/dashboard/buses/counter/store', 'AdminandothersController@updateBus')->name('dashboard.buses.delete');
 
 
 
