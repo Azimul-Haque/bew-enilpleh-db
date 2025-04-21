22f632870bbb30c8f59f42e202b6f503248fbb27
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-22 02:23:01 +0600

    Commit Tue 04/22/2025  2:23:01.29 24627

--- resources/views/dashboard/buses/index.blade.php
+++ resources/views/dashboard/buses/index.blade.php
@@ -93,7 +93,7 @@
                             <div class="modal-dialog" role="document">
                               <div class="modal-content">
                                 <div class="modal-header bg-warning">
-                                  <h5 class="modal-title" id="editDeptModalLabel">বিভাগ তথ্য হালনাগাদ</h5>
+                                  <h5 class="modal-title" id="editDeptModalLabel">কাউন্টার তথ্য হালনাগাদ</h5>
                                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                   </button>
