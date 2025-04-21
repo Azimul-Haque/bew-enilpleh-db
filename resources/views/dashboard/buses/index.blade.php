ce4dfabda6f18a9e747ad0ee7916ebf077be6d5a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-22 02:23:08 +0600

    Commit Tue 04/22/2025  2:23:08.26 17033

--- resources/views/dashboard/buses/index.blade.php
+++ resources/views/dashboard/buses/index.blade.php
@@ -143,7 +143,7 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header bg-warning">
-            <h5 class="modal-title" id="addCounterModalLabel">নতুন বিভাগ যোগ</h5>
+            <h5 class="modal-title" id="addCounterModalLabel">নতুন কাউন্টার যোগ</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
