52f8679b5c67700e627c440151bfd7ba0b91122b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-28 02:10:41 +0600

    Commit Mon 04/28/2025  2:10:41.21 14474

--- resources/views/dashboard/coachings/single.blade.php
+++ resources/views/dashboard/coachings/single.blade.php
@@ -232,7 +232,7 @@
                       </div>
                   </div>
                   <div class="input-group mb-3">
-                    <select name="type" class="form-control" required>
+                    <select name="type" id="typeSelect" class="form-control" required>
                         <option selected="" disabled="" value="">শিক্ষা প্রতিষ্ঠানের ধরন</option>
                         <option value="1">সরকারি শিক্ষা প্রতিষ্ঠান</option>
                         <option value="2">বেসরকারি শিক্ষা প্রতিষ্ঠান</option>
