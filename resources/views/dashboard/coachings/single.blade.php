1677a72f37ea6eb89596f0ca1143bc4ed0eb10dc
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-28 02:49:16 +0600

    Commit Mon 04/28/2025  2:49:16.24 31702

--- resources/views/dashboard/coachings/single.blade.php
+++ resources/views/dashboard/coachings/single.blade.php
@@ -201,7 +201,7 @@
                                     <option value="একাডেমিক (প্রাথমিক/ মাধ্যমিক/ উচ্চমাধ্যমিক)">একাডেমিক (প্রাথমিক/ মাধ্যমিক/ উচ্চমাধ্যমিক)</option>
                                     <option value="ভর্তি কোচিং">ভর্তি কোচিং</option>
                                     <option value="স্কিল ডেভেলপমেন্ট">স্কিল ডেভেলপমেন্ট</option>
-                                    <option value="IELTS/ GRE/ Tofel">IELTS/ GRE/ Tofel</option>
+                                    <option value="IELTS/ GRE/ Toefl">IELTS/ GRE/ Toefl</option>
                                 `;
                             }
 
