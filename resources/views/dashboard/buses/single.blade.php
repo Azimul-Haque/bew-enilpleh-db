23fc36917fe0a747f07ec92baabe3549b00107e6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-02 01:28:44 +0600

    Commit Fri 05/02/2025  1:28:44.23 17865

--- resources/views/dashboard/buses/single.blade.php
+++ resources/views/dashboard/buses/single.blade.php
@@ -374,7 +374,7 @@
 
 @section('third_party_scripts')
     {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
-    <script>
+    <script type="text/javascript">
         const buscounters = @json($buscounters);
         let counterIndex = 0;
 
