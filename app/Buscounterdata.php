1adecb2cd29346cfe96ce9ccf3da95f3e3092de4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-02 00:46:47 +0600

    Commit Fri 05/02/2025  0:46:47.32 13816

--- app/Buscounterdata.php
+++ app/Buscounterdata.php
@@ -9,8 +9,8 @@ class Buscounterdata extends Model
     public function bus(){
         return $this->belongsTo('App\Bus');
     }
-    
-    public function bus(){
-        return $this->belongsTo('App\Bus');
+
+    public function buscounter(){
+        return $this->belongsTo('App\Buscounter');
     }
 }
