4257f1b587cd58603c542c082176249ab6d62606
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-02 16:56:04 +0600

    Commit Fri 05/02/2025 16:56:04.29 24502

--- resources/views/dashboard/buses/single.blade.php
+++ resources/views/dashboard/buses/single.blade.php
@@ -161,7 +161,6 @@
                                 </div>
 
 
-
                                 <div class="row">
                                   <div class="col-md-6">
                                     <div class="input-group mb-3">
