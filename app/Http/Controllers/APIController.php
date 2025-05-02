6fca28610014c8909f3d98c127780aa6af17bc19
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-03 01:57:13 +0600

    Commit Sat 05/03/2025  1:57:13.25 9291

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -879,7 +879,7 @@ class APIController extends Controller
                               'address' => $buscounterdata->address,
                               'mobile' => $buscounterdata->mobile,
                           ]);
-                          $coachingimage->makeHidden('id', 'coaching_id', 'created_at', 'updated_at');
+                          $coachingimage->makeHidden('id', 'bus_id', 'buscounter_id', 'created_at', 'updated_at');
                           // $coaching->push($coachingimagetemp);
                        }
                        $coaching->images = $bustmp;
