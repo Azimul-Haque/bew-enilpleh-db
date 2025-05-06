346f2758811baac428b5375cf6832c9b1728bbe8
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-07 00:43:57 +0600

    Commit Wed 05/07/2025  0:43:57.23 30059

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -348,31 +348,23 @@
               
                   @csrf
                   <div class="row">
-                    <div class="col-md-6">
-                      <select name="district_id" id="district" class="form-control district select21" data-placeholder="জেলা নির্বাচন করুন" required>
-                          <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
-                          @foreach($districts as $district)
-                            <option value="{{ $district->id }}">{{ $district->name_bangla }}-{{ $district->name }}</option>
-                          @endforeach
-                      </select>
-                      {{-- <div class="input-group mb-3">
-                        
-                        <div class="input-group-append">
-                            <div class="input-group-text"><span class="fas fa-map"></span></div>
-                        </div>
-                      </div> --}}
-                    </div>
-                    <div class="col-md-6">
-                      <select name="upazilla_id" id="upazilla" class="form-control upazilla select21" data-placeholder="উপজেলা নির্বাচন করুন" required>
-                          <option selected="" disabled="" value="">উপজেলা নির্বাচন করুন</option>
-                      </select>
-                      {{-- <div class="input-group mb-3">
-                        
-                        <div class="input-group-append">
-                            <div class="input-group-text"><span class="fas fa-map-marked-alt"></span></div>
-                        </div>
-                      </div> --}}
-                    </div>
+                    @if()
+                      <div class="col-md-6">
+                        <select name="district_id" id="district" class="form-control district select21" data-placeholder="জেলা নির্বাচন করুন" required>
+                            <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
+                            @foreach($districts as $district)
+                              <option value="{{ $district->id }}">{{ $district->name_bangla }}-{{ $district->name }}</option>
+                            @endforeach
+                        </select>
+                      </div>
+                      <div class="col-md-6">
+                        <select name="upazilla_id" id="upazilla" class="form-control upazilla select21" data-placeholder="উপজেলা নির্বাচন করুন" required>
+                            <option selected="" disabled="" value="">উপজেলা নির্বাচন করুন</option>
+                        </select>
+                      </div>
+                    @else
+
+                    @endif
                     <br/><br/>
                     <div class="col-md-6">
                       <div class="input-group mb-3">
