d59ca27a2c26e417fa2db1958cf2efee817edd56
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-07 02:24:31 +0600

    Commit Wed 05/07/2025  2:24:31.20 3916

--- resources/views/dashboard/lawyers/single.blade.php
+++ resources/views/dashboard/lawyers/single.blade.php
@@ -158,7 +158,7 @@
                               </div>
                               <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                                <a href="{{ route('dashboard.lawyers.delete', $lawyer->id) }}" class="btn btn-danger">ডিলেট করুন</a>
+                                <a href="{{ route('dashboard.lawyers.delete', [$district->id, $lawyer->id]) }}" class="btn btn-danger">ডিলেট করুন</a>
                               </div>
                             </div>
                           </div>
