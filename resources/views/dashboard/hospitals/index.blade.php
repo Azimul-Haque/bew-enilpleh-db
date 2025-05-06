2456eb87367a4cef0e38dde74d2f313a46fa8358
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-07 00:40:39 +0600

    Commit Wed 05/07/2025  0:40:39.25 2513

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -586,6 +586,29 @@
           });
         });
 
+        function EditorSelected() {
+          $('.upazilla').prop('disabled', true);
+          $('.upazilla').append('<option value="" selected disabled>উপজেলা লোড হচ্ছে...</option>');
+
+          $.ajax({
+            url: "/api/getupazillas/{{ env('SOFT_TOKEN') }}/" +$(this).val(), 
+            type: "GET",
+            success: function(result){
+              $('.upazilla')
+                  .find('option')
+                  .remove()
+                  .end()
+                  .prop('disabled', false)
+                  .append('<option value="" selected disabled>উপজেলা নির্ধারণ করুন</option>')
+              ;
+              for(var countupazilla = 0; countupazilla < result.length; countupazilla++) {
+                console.log(result[countupazilla]);
+                $('.upazilla').append('<option value="'+result[countupazilla]['id']+'">'+result[countupazilla]['name_bangla']+'-'+result[countupazilla]['name']+'</option>')
+              }
+            }
+          });
+        }
+
         $(document).on('click', '#search-button', function() {
           if($('#search-param').val() != '') {
             var urltocall = '{{ route('dashboard.hospitals') }}' +  '/' + $('#search-param').val();
