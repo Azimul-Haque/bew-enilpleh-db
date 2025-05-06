bbbb21b6ba172f913879bae3a333ed3165179584
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-07 01:10:08 +0600

    Commit Wed 05/07/2025  1:10:08.26 23200

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -85,6 +85,9 @@ class DoctorController extends Controller
             );
 
             // dd($doctors);
+            $doctorscount = Doctor::where('district_id', Auth::user()->district_id)->count();
+            $doctors = Doctor::orderBy('id', 'desc')->paginate(10);
+            $hospitals = Hospital::all();
 
         } else {
             $doctorscount = Doctor::count();
