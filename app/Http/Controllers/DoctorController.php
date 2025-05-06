da3c0f278d9b723be56b001ba82fa31b2f20041d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-07 01:24:30 +0600

    Commit Wed 05/07/2025  1:24:30.32 3537

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -516,7 +516,7 @@ class DoctorController extends Controller
             $doctor = Doctor::where('id', $doctor_id)
                             ->where('district_id', Auth::user()->district_id)
                             ->first();
-            $doctorserials = Doctorserial::where('doctor_id', $doctor_id)
+            $doctorserials = Doctorserial::where('doctor_id', $doctor->id)
                                        ->where('serialdate', $serialdate)
                                        ->get();
         }
