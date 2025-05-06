f67433a4a537b271d73ed5f56236589e4373badc
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-07 01:46:21 +0600

    Commit Wed 05/07/2025  1:46:21.24 10451

--- resources/views/dashboard/ambulances/index.blade.php
+++ resources/views/dashboard/ambulances/index.blade.php
@@ -84,11 +84,12 @@
                               
                                   @csrf
 
+                                  @if(Auth::user()->role == 'admin')
                                   <div class="input-group mb-3">
                                     <select name="district_id" id="district" class="form-control district" required>
                                         <option selected="" disabled="" value="">জেলা নির্বাচন করুন</option>
                                         @foreach($districts as $district)
-                                          <option value="{{ $district->id }}" @if($district->id == $ambulance->district_id) selected @endif>{{ $district->name_bangla }}</option>
+                                          <option value="{{ $district->id }}" @if($district->id == $blooddonor->district_id) selected @endif>{{ $district->name_bangla }}</option>
                                         @endforeach
                                     </select>
                                     <div class="input-group-append">
@@ -97,12 +98,26 @@
                                   </div>
                                   <div class="input-group mb-3">
                                     <select name="upazilla_id" id="upazilla" class="form-control upazilla" required>
-                                        <option selected="" value="{{ $ambulance->upazilla_id }}">{{ $ambulance->upazilla->name_bangla }}</option>
+                                        <option selected="" value="{{ $blooddonor->upazilla_id }}">{{ $blooddonor->upazilla->name_bangla }}</option>
                                     </select>
                                     <div class="input-group-append">
                                         <div class="input-group-text"><span class="fas fa-map-marked-alt"></span></div>
                                     </div>
                                   </div>
+                                  @else
+                                  জেলা: {{ Auth::user()->district->name_bangla }}
+                                  <input type="hidden" name="district_id" value="{{ Auth::user()->district_id }}">
+                                  <div class="input-group mb-3" style="margin-top: 15px;">
+                                    <select name="upazilla_id" id="upazilla" class="form-control upazilla" required>
+                                        <option selected="" value="{{ $blooddonor->upazilla_id }}">{{ $blooddonor->upazilla->name_bangla }}</option>
+                                    </select>
+                                    <div class="input-group-append">
+                                        <div class="input-group-text"><span class="fas fa-map-marked-alt"></span></div>
+                                    </div>
+                                  </div>
+                                  @endif
+
+
                                   <div class="input-group mb-3">
                                       <input type="text"
                                              name="name"
