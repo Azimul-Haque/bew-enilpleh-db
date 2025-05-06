2c73420ee532fe75e2cc1f24b02937ec06b78be3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-07 02:45:59 +0600

    Commit Wed 05/07/2025  2:45:59.26 11671

--- app/Http/Controllers/AdminandothersController.php
+++ app/Http/Controllers/AdminandothersController.php
@@ -1121,8 +1121,8 @@ class AdminandothersController extends Controller
         $district = District::find($district_id);
         
         if(Auth::user()->role == 'editor') {
-            $busescount = Bus::where('district_id', $district_id)->count();
-            $buses = Bus::where('district_id', $district_id)->orderBy('id', 'asc')->paginate(10);
+            $busescount = Bus::where('district_id', Auth::user()->district_id)->count();
+            $buses = Bus::where('district_id', Auth::user()->district_id)->orderBy('id', 'asc')->paginate(10);
         } else {
             $busescount = Bus::where('district_id', $district_id)->count();
             $buses = Bus::where('district_id', $district_id)->orderBy('id', 'asc')->paginate(10);
