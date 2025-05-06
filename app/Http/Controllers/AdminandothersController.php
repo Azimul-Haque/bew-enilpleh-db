fdad3dc2bc7e495021e1680bf4af1049f9e25fca
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-06 14:57:53 +0600

    Commit Tue 05/06/2025 14:57:53.24 6604

--- app/Http/Controllers/AdminandothersController.php
+++ app/Http/Controllers/AdminandothersController.php
@@ -42,7 +42,8 @@ class AdminandothersController extends Controller
     {
         parent::__construct();
         ini_set('memory_limit', '-1');
-        $this->middleware(['admin'])->only('index', 'indexSearch', 'lawyerIndex', 'busIndex', 'rentacarIndex', 'newspaperIndex', 'journalistIndex');
+        $this->middleware(['admin'])->only('index', 'indexSearch');
+        // 'lawyerIndex', 'busIndex', 'rentacarIndex', 'newspaperIndex', 'journalistIndex'
     }
 
     public function index()
