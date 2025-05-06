d6951c0a47328474481b057a7f37c1ed7e9bd1a0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-06 15:07:04 +0600

    Commit Tue 05/06/2025 15:07:04.28 12084

--- app/Http/Controllers/AdminandothersController.php
+++ app/Http/Controllers/AdminandothersController.php
@@ -572,9 +572,9 @@ class AdminandothersController extends Controller
 
     public function coachingIndexSingle($district_id)
     {
-        if(Auth::user()->role == 'editor') {
-            abort(403, 'Access Denied');
-        }        
+        // if(Auth::user()->role == 'editor') {
+        //     abort(403, 'Access Denied');
+        // }        
         $district = District::find($district_id);
         $coachingscount = Coaching::where('district_id', $district_id)->count();
             $coachings = Coaching::where('district_id', $district_id)->orderBy('id', 'asc')->paginate(10);
