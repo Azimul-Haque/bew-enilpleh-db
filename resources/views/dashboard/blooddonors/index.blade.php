a2223bd74073eb3708c3baa2ed3be55555180394
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-07 01:30:18 +0600

    Commit Wed 05/07/2025  1:30:18.32 14972

--- resources/views/dashboard/blooddonors/index.blade.php
+++ resources/views/dashboard/blooddonors/index.blade.php
@@ -242,8 +242,8 @@
                   </div>
                   @else
                   জেলা: {{ Auth::user()->district->name_bangla }}
-                  <input type="hidden" name="district_id" value="{{ Auth::user()->district_id }}" style="margin-bottom: 15px;">
-                  <div class="input-group mb-3">
+                  <input type="hidden" name="district_id" value="{{ Auth::user()->district_id }}">
+                  <div class="input-group mb-3" style="margin-top: 15px;">
                     <select name="upazilla_id" id="upazilla" class="form-control upazilla" required>
                         <option selected="" disabled="" value="">উপজেলা নির্বাচন করুন</option>
                     </select>
