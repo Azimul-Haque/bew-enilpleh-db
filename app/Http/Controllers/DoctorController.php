47b7681ebe876b2afd5b62c185349f700e2f8084
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-28 01:14:50 +0600

    Commit Wed 05/28/2025  1:14:50.28 25924

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -498,23 +498,23 @@ class DoctorController extends Controller
 
     public function getDoctorSerialPDF($doctor_id, $serialdate)
     {
-        if(Auth::user()->role == 'editor') {
-            // if(!in_array('doctors', Auth::user()->accessibleTables())) {
-            //     abort(403, 'Access Denied');
-            // }
-            // if(!in_array($doctor_id, Auth::user()->accessibleDoctors()->pluck('accessible_id')->toArray())) {
-            //     // if not in accessed doctors list, check if hospital is accessed atleast
-            //     $accessedhospitals = Auth::user()->accessibleHospitals()->get();
-            //     $hospitaldoctorslist = collect();
-            //     foreach($accessedhospitals as $hospital) {
-            //         foreach($hospital->doctorhospitals as $doctor) {
-            //             $hospitaldoctorslist->push($doctor->doctor);
-            //         }
-            //     }
-            //     if(!in_array($doctor_id, $hospitaldoctorslist->pluck('id')->toArray())) {
-            //         abort(403, 'Access Denied');
-            //     }
-            // }
+        if(Auth::user()->role == 'manager') {
+            if(!in_array('doctors', Auth::user()->accessibleTables())) {
+                abort(403, 'Access Denied');
+            }
+            if(!in_array($doctor_id, Auth::user()->accessibleDoctors()->pluck('accessible_id')->toArray())) {
+                // if not in accessed doctors list, check if hospital is accessed atleast
+                $accessedhospitals = Auth::user()->accessibleHospitals()->get();
+                $hospitaldoctorslist = collect();
+                foreach($accessedhospitals as $hospital) {
+                    foreach($hospital->doctorhospitals as $doctor) {
+                        $hospitaldoctorslist->push($doctor->doctor);
+                    }
+                }
+                if(!in_array($doctor_id, $hospitaldoctorslist->pluck('id')->toArray())) {
+                    abort(403, 'Access Denied');
+                }
+            }
             $doctor = Doctor::where('id', $doctor_id)
                             ->where('district_id', Auth::user()->district_id)
                             ->first();
