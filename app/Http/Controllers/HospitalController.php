24d6086ee9daf13f7cb8cc1bb7f4172cbd2bd9eb
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-28 01:00:54 +0600

    Commit Wed 05/28/2025  1:00:54.20 19181

--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -35,16 +35,14 @@ class HospitalController extends Controller
     public function index()
     {
         if(Auth::user()->role == 'editor') {
-            
-
             $hospitalscount = Hospital::where('district_id', Auth::user()->district_id)->count();
             $hospitals = Hospital::where('district_id', Auth::user()->district_id)->paginate(10);
         } elseif(Auth::user()->role == 'manager') {
-            // if(!in_array('hospitals', Auth::user()->accessibleTables())) {
-            //     abort(403, 'Access Denied');
-            // }
-            // $hospitalscount = Auth::user()->accessibleHospitals()->count();
-            // $hospitals = Auth::user()->accessibleHospitals()->paginate(10);
+            if(!in_array('hospitals', Auth::user()->accessibleTables())) {
+                abort(403, 'Access Denied');
+            }
+            $hospitalscount = Auth::user()->accessibleHospitals()->count();
+            $hospitals = Auth::user()->accessibleHospitals()->paginate(10);
         } else {
             $hospitalscount = Hospital::count();
             $hospitals = Hospital::orderBy('id', 'desc')->paginate(10);
