1cf12f2a8f52e609e6ead89b1cdb34f5d3c66c65
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-28 01:58:29 +0600

    Commit Wed 05/28/2025  1:58:29.28 12222

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -87,7 +87,7 @@
           </div>
           @endif
 
-          @if(Auth::user()->role == 'admin' || Auth::user()->role == 'editor')
+          @if(Auth::user()->role == 'admin')
           <div class="col-md-3">
             <a href="{{ route('dashboard.coachings') }}" class="info-box mb-3">
               <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-chalkboard-teacher"></i></span>
@@ -98,7 +98,7 @@
               </div>
             </a>
           </div>
-          @elseif(Auth::user()->role == 'editor')
+          @elseif(Auth::user()->role == 'editor' || Auth::user()->role == 'manager')
           <div class="col-md-3">
             <a href="{{ route('dashboard.coachings.singleforeditor') }}" class="info-box mb-3">
               <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-chalkboard-teacher"></i></span>
