bc4fb05f08a30c5a0ec3f4b77f73bd9f41ef7b29
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-11 19:00:29 +0600

    Commit Fri 07/11/2025 19:00:29.30 17618

--- resources/views/dashboard/hospitals/index.blade.php
+++ resources/views/dashboard/hospitals/index.blade.php
@@ -64,7 +64,7 @@
                       <small class="text-black-50"><i class="fas fa-mobile"></i> {{ $hospital->mobile }}</small><br/>
                     </td>
                     <td>
-                      <span class="badge bg-success">{{ hospital_type($hospital->hospital_type) }}</span></td>
+                      <span class="badge bg-{{ hospital_badge }}">{{ hospital_type($hospital->hospital_type) }}</span></td>
                     <td>
                       {{ $hospital->address }}<br/>
                       {{ $hospital->upazilla->name_bangla }}, {{ $hospital->district->name_bangla }}
