92ec87792e928a71b25b396fcb207ed3732f02f5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-11 19:10:11 +0600

    Commit Fri 07/11/2025 19:10:11.22 9092

--- resources/views/partials/_buseditmodal.blade.php
+++ resources/views/partials/_buseditmodal.blade.php
@@ -2,7 +2,7 @@
 {{-- Edit User Modal Code --}}
 <!-- Modal -->
 <div class="modal fade" id="editUserModal{{ $bus->id }}" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true" data-backdrop="static">
-  <div class="modal-dialog modal-lg" role="document">
+  <div class="modal-dialog modal-XL" role="document">
     
       <div class="modal-content">
         <div class="modal-header bg-primary">
