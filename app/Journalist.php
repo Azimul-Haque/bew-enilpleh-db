67a2f5e48297a6826a62785d0d30a9a0c39aaf9d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-19 19:15:43 +0600

    Commit Sat 07/19/2025 19:15:43.23 29793

--- app/Journalist.php
+++ app/Journalist.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Journalist extends Model
 {
+    protected $casts = [
+        'top_news_links' => 'array',
+    ];
+    
     public function district(){
         return $this->belongsTo('App\District');
     }
