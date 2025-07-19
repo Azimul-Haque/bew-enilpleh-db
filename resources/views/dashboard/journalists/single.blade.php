15bc3e50eb62ceeba291c478bae96992cb1561ee
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-19 19:25:53 +0600

    Commit Sat 07/19/2025 19:25:53.27 32009

--- resources/views/dashboard/journalists/single.blade.php
+++ resources/views/dashboard/journalists/single.blade.php
@@ -234,8 +234,7 @@
                           <input type="url"
                                  name="top_news_links[]"
                                  class="form-control"
-                                 placeholder="শীর্ষ প্রকাশিত সংবাদের লিংক -  {{ $i + 1 }}"
-                          >
+                                 placeholder="শীর্ষ প্রকাশিত সংবাদের লিংক -  {{ $i + 1 }}">
                           <div class="input-group-append">
                               <div class="input-group-text"><span class="fas fa-link"></span></div>
                           </div>
