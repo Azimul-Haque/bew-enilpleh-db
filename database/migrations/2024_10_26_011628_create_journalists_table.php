8ca001377d69681082567bc3d3cda4cf00e9de64
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-20 00:42:14 +0600

    Commit Sun 07/20/2025  0:42:14.24 2483

--- database/migrations/2024_10_26_011628_create_journalists_table.php
+++ database/migrations/2024_10_26_011628_create_journalists_table.php
@@ -19,7 +19,7 @@ class CreateJournalistsTable extends Migration
             $table->string('name');
             $table->string('affiliation');
             $table->string('mobile');
-            $table->json('top_news_links');
+            $table->json('top_news_links')->nullable();
             $table->timestamps();
         });
     }
