a660dbb9576f3d04a3a23fffb33fbced1e84397c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 03:19:24 +0600

    Commit Sat 08/30/2025  3:19:24.21 14457

--- resources/views/dashboard/buses/single.blade.php
+++ resources/views/dashboard/buses/single.blade.php
@@ -305,7 +305,8 @@
 @section('third_party_scripts')
     {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
     <script type="text/javascript">
-        const buscounters = @json($buscounters);
+        {{-- const buscounters = @json($buscounters); --}}
+        const buscounters = @json($district->buscounters);
         let counterIndex = 0;
 
         $('#addCounterBtn').on('click', function () {
