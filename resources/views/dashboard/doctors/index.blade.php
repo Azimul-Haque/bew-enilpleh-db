451c1465351dc1bafc8c81ac632078f1692fd5d0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-29 16:57:41 +0600

    Commit Fri 08/29/2025 16:57:41.28 17555

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -61,7 +61,7 @@
                       <select id="districtsearch" class="form-control form-control-sm" data-placeholder="জেলা ফিল্টার করুন" style="margin-right: 20px;">
                           <option selected="" disabled="" value="">জেলা ফিল্টার করুন</option>
                           @foreach($districts as $district)
-                            <option value="{{ $district->name_bangla }}" @isset($search) @if() @endif @endisset>{{ $district->name_bangla }}-{{ $district->name }}</option>
+                            <option value="{{ $district->name_bangla }}" @isset($search) @if($district->name_bangla == $search) @endif @endisset>{{ $district->name_bangla }}-{{ $district->name }}</option>
                           @endforeach
                       </select>
                       <div class="form-group">
