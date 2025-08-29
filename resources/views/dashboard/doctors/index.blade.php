61257d028c205dcdebb9ae288cf890482dd4578e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-29 16:53:45 +0600

    Commit Fri 08/29/2025 16:53:45.31 21878

--- resources/views/dashboard/doctors/index.blade.php
+++ resources/views/dashboard/doctors/index.blade.php
@@ -929,6 +929,17 @@
               }, 1000);
             }
         });
+        $("#districtsearch").change(function(e) {
+          if($('#districtsearch').val() != '') {
+            var urltocall = '{{ route('dashboard.hospitals') }}' +  '/' + $('#districtsearch').val();
+            location.href= urltocall;
+          } else {
+            $('#districtsearch').css({ "border": '#FF0000 2px solid'});
+            Toast.fire({
+                icon: 'warning',
+                title: 'কিছু লিখে খুঁজুন!'
+            })
+          }
 
 
     </script>
