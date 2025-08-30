b4a1ccfbf8d0e833cf0945cf066de2b70f4fb84e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 17:54:39 +0600

    Commit Sat 08/30/2025 17:54:39.32 7536

--- resources/views/dashboard/hospitals/single.blade.php
+++ resources/views/dashboard/hospitals/single.blade.php
@@ -17,7 +17,7 @@
 @endsection
 
 @section('content')
-  @section('page-header') হাসপাতাল - ডাক্তার তালিকা ({{ bangla($hospitalscount) }}) @endsection
+  @section('page-header') হাসপাতাল - ডাক্তার তালিকা ({{ $hospital }}) @endsection
     <div class="container-fluid">
     <div class="card">
           <div class="card-header">
