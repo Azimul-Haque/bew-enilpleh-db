f35cdeaaa210bc5291d23eeeffcabaa467a6e1f0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-10 01:20:04 +0600

    Commit Tue 03/10/2026  1:20:04.25 1264

--- app/Http/Controllers/AdminandothersController.php
+++ app/Http/Controllers/AdminandothersController.php
@@ -645,7 +645,7 @@ class AdminandothersController extends Controller
 
     public function coachingIndex()
     {
-        if(Auth::user()->role == 'editor' || Auth::user()->role == 'manager') {
+        if(Auth::user()->role == 'editor' || Auth::user()->role == 'manager' || Auth::user()->role == 'doctor') {
             abort(403, 'Access Denied');
         }
         $districts = District::all();
