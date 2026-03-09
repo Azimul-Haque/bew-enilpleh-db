aab63dff2230cef88c3727d1c025816b2b5879cf
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-10 02:17:04 +0600

    Commit Tue 03/10/2026  2:17:04.31 1173

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -182,26 +182,28 @@
       {{-- doctor role এর জন্য এই নতুন if else কোড --}}
       {{-- doctor role এর জন্য এই নতুন if else কোড --}}
       {{-- doctor role এর জন্য এই নতুন if else কোড --}}
-      <div class="col-md-3">
-        <a href="{{ route('dashboard.doctors.chambers') }}" class="info-box mb-3">
-          <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-hospital"></i></span>
+      <div class="row">
+        <div class="col-md-3">
+          <a href="{{ route('dashboard.doctors.chambers') }}" class="info-box mb-3">
+            <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-hospital"></i></span>
 
-          <div class="info-box-content">
-            <span class="info-box-text">চেম্বার তালিকা</span>
-            <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
-          </div>
-        </a>
-      </div>
+            <div class="info-box-content">
+              <span class="info-box-text">চেম্বার তালিকা</span>
+              <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
+            </div>
+          </a>
+        </div>
 
-      <div class="col-md-3">
-        <a href="{{ route('dashboard.doctors.chambers') }}" class="info-box mb-3">
-          <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-hospital"></i></span>
+        <div class="col-md-3">
+          <a href="{{ route('dashboard.doctors.chambers') }}" class="info-box mb-3">
+            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-hospital"></i></span>
 
-          <div class="info-box-content">
-            <span class="info-box-text">প্রোফাইল</span>
-            <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
-          </div>
-        </a>
+            <div class="info-box-content">
+              <span class="info-box-text">প্রোফাইল</span>
+              <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
+            </div>
+          </a>
+        </div>
       </div>
 
       @endif
