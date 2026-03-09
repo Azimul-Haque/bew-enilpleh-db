40fd4c6db525ce4c8c0f3c9b0a34765569008715
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-10 02:17:30 +0600

    Commit Tue 03/10/2026  2:17:30.25 10152

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -185,17 +185,6 @@
       <div class="row">
         <div class="col-md-3">
           <a href="{{ route('dashboard.doctors.chambers') }}" class="info-box mb-3">
-            <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-hospital"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">চেম্বার তালিকা</span>
-              <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
-            </div>
-          </a>
-        </div>
-
-        <div class="col-md-3">
-          <a href="{{ route('dashboard.doctors.chambers') }}" class="info-box mb-3">
             <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user-md"></i></span>
 
             <div class="info-box-content">
@@ -204,6 +193,16 @@
             </div>
           </a>
         </div>
+        <div class="col-md-3">
+          <a href="{{ route('dashboard.doctors.chambers') }}" class="info-box mb-3">
+            <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-hospital"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">চেম্বার তালিকা</span>
+              <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
+            </div>
+          </a>
+        </div>        
       </div>
 
       @endif
