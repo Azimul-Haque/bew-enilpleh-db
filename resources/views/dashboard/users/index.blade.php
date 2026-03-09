4da6cb203e8c75ce5bc8d16cccd43c468b21b4d3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-09 23:35:23 +0600

    Commit Mon 03/09/2026 23:35:23.22 20411

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -190,6 +190,7 @@
             				                		<option value="admin" @if($user->role == 'admin') selected="" @endif>এডমিন</option>
               													<option value="editor" @if($user->role == 'editor') selected="" @endif>জেলা এডমিন</option>
                                         <option value="manager" @if($user->role == 'manager') selected="" @endif>প্রতিষ্ঠান ম্যানেজার</option>
+                                        <option value="doctor" @if($user->role == 'doctor') selected="" @endif>প্রতিষ্ঠান ম্যানেজার</option>
                                         {{-- <option value="volunteer" @if($user->role == 'volunteer') selected="" @endif>ভলান্টিয়ার</option>
               													<option value="user" @if($user->role == 'user') selected="" @endif>ব্যবহারকারী</option> --}}
               													{{-- <option value="accountant" @if($user->role == 'accountant') selected="" @endif>একাউন্টেন্ট</option> --}}
