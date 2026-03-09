36d0a8ff5e1db2ba12c3392ff29f4e9e6239c0d4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-10 02:56:55 +0600

    Commit Tue 03/10/2026  2:56:55.25 5354

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,8 @@ Route::get('/dashboard/doctors/appoinments/pdf/{doctor_id}/{date}', 'DoctorContr
 Route::post('/dashboard/doctors/{doctor_id}/appoinments/add/{date}/manually', 'DoctorController@addDoctorSerialManually')->name('dashboard.adddoctorserialmanually');
 Route::post('/dashboard/doctors/appoinments/delete/{serial_id}', 'DoctorController@deleteDoctorSerial')->name('dashboard.deletedoctorserial');
 
+Route::get('/dashboard/doctors/{id}/chambers', 'DoctorController@doctorChambers')->name('dashboard.doctors.chambers');
+
 Route::get('/dashboard/chambers', 'DoctorController@doctorChambers')->name('dashboard.doctors.chambers');
 Route::post('/dashboard/chambers/store', 'DoctorController@doctorChamberStore')->name('dashboard.doctors.chambers.store');
 Route::post('/dashboard/chambers/{id}/update', 'DoctorController@doctorChamberUpdate')->name('dashboard.doctors.chambers.update');
