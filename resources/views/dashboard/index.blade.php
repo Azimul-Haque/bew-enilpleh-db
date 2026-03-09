4763bf631a74ba7b3030a6b27d35a18441747206
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-10 02:16:32 +0600

    Commit Tue 03/10/2026  2:16:32.26 7849

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -193,6 +193,17 @@
         </a>
       </div>
 
+      <div class="col-md-3">
+        <a href="{{ route('dashboard.doctors.chambers') }}" class="info-box mb-3">
+          <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-hospital"></i></span>
+
+          <div class="info-box-content">
+            <span class="info-box-text">চেম্বার তালিকা</span>
+            <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
+          </div>
+        </a>
+      </div>
+
       @endif
 
         @if(Auth::user()->role == 'admin')
