36b9eea6bf4ac393ca66331817607ea111f90c89
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-10 00:56:47 +0600

    Commit Tue 03/10/2026  0:56:47.28 32404

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -56,7 +56,7 @@ class DashboardController extends Controller
         } elseif(Auth::user()->role == 'doctor') {
             if(Auth::user()->is_active == 0) {
                 Session::flash('warning', 'আপনার একাউন্ট এখনও একটিভেট করা হয়নি! SmartBD এডমিনের সাথে যোগাযোগ করুন!');
-                return redirect()->route('dashboard.index');
+                return redirect()->route('index.login');
             }
         }
 
