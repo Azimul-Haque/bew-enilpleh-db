daea2465335f59dcf09ce1c00afe9b12842e137f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-10 00:53:12 +0600

    Commit Tue 03/10/2026  0:53:12.27 25597

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -70,33 +70,6 @@
                         <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#activateModal{{ $user->id }}" title="একটিভেট করুন">
                           <i class="fas fa-check"></i>
                         </button>
-                        {{-- Activate Modal Code --}}
-                        {{-- Activate Modal Code --}}
-                        <!-- Modal -->
-                        <div class="modal fade" id="activateModal{{ $user->id }}" tabindex="-1" role="dialog" aria-labelledby="activateModalLabel" aria-hidden="true" data-backdrop="static">
-                          <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                              <div class="modal-header bg-warning">
-                                <h5 class="modal-title" id="activateModalLabel">একটিভেট করুন</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                                </button>
-                              </div>
-                              <form method="post" action="{{ route('dashboard.users.activate', $user->id) }}">
-                                <div class="modal-body">
-                                      @csrf
-                                      আপনি কি নিশ্চিতভাবে <strong>{{ $user->name }}</strong>-কে একটিভেট করতে চান?
-                                </div>
-                                <div class="modal-footer">
-                                  <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                                  <button type="submit" class="btn btn-warning">একটিভেট করুন</button>
-                                </div>
-                              </form>
-                            </div>
-                          </div>
-                        </div>
-                        {{-- Activate Modal Code --}}
-                        {{-- Activate Modal Code --}}
                       @endif
 
                 			<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editUserModal{{ $user->id }}">
