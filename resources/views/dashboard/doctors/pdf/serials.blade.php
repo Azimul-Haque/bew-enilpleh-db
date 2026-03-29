f58559e9d2fb5a92590abbf7ddc12e037e380bd4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-30 03:12:59 +0600

    Commit Mon 03/30/2026  3:12:59.29 3573

--- resources/views/dashboard/doctors/pdf/serials.blade.php
+++ resources/views/dashboard/doctors/pdf/serials.blade.php
@@ -42,7 +42,7 @@
         <b>{{ $doctor->name }}-এর সিরিয়ালসমূহ</b>
     </h3>
     <span class="calibri_normal"><b>Date: {{ date('d F Y', strtotime($serialdate)) }}</b></span><br/>
-    <span class=""><b>চেম্বার: {{ $hospital->name }}, {{ $hospital->upzilla->name_bangla }}, {{ $hospital->district->name_bangla }}</b></span><br/><br/>
+    <span class=""><b>চেম্বার: {{ $hospital->name }}, {{ $hospital->upazilla->name_bangla }}, {{ $hospital->district->name_bangla }}</b></span><br/><br/>
 
     <table class="bordertable">
         <thead>
