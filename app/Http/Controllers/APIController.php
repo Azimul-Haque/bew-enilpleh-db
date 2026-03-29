c3fde7c2abf74399368b93c0aeee08c8cc04e196
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-30 00:55:55 +0600

    Commit Mon 03/30/2026  0:55:55.26 22117

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -412,7 +412,7 @@ class APIController extends Controller
                     $chamber->hospital = $chamber->hospital->name;
                     $chamber->upazilla = $chamber->hospital->upazilla->name_bangla;
                     $chamber->district = $chamber->hospital->district->name_bangla;
-                    $doctor->makeHidden('doctormedicaldepartments', 'doctorimage', 'district_id', 'bmdc_number', 'serial', 'address', 'helpline', 'weekdays', 'offdays', 'onlineserial', 'created_at', 'updated_at');
+                    $chamber->makeHidden('doctormedicaldepartments', 'doctorimage', 'district_id', 'bmdc_number', 'serial', 'address', 'helpline', 'weekdays', 'offdays', 'onlineserial', 'created_at', 'updated_at');
                     // dd($doctorstoreturn);
                 }
                 return $chambers;
