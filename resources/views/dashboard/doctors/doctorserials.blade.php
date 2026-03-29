3b5abe732de11d6547412b84ef5422d49f62ed51
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-30 02:48:30 +0600

    Commit Mon 03/30/2026  2:48:30.31 21033

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -191,7 +191,6 @@
               @csrf
               <input type="text" class="form-control" name="doctor_name" placeholder="ডাক্তার" value="ডাক্তার: {{ $doctor->name }}" disabled required><br/>
               <input type="hidden" name="doctor_id" placeholder="ডাক্তার" value="{{ $doctor->id }}"required>
-              <input type="hidden" name="hospital_id" placeholder="ডাক্তার" value="{{ $doctor->id }}"required>
               <input type="text" class="form-control" name="name" placeholder="রোগীর নাম" required><br/>
               <input type="number" class="form-control" name="mobile" placeholder="রোগীর মোবাইল নং" required><br/>
               <input type="text" class="form-control" name="serialdate" id="addserialdate" placeholder="তারিখ" placeholder="তারিখ সিলেক্ট করুন" readonly required><br/>
