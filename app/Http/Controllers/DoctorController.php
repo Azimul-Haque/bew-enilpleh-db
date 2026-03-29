5b46d01d94a2558edd09c8e3f1db71e37e19f67a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-30 02:51:43 +0600

    Commit Mon 03/30/2026  2:51:43.23 11618

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -629,7 +629,7 @@ class DoctorController extends Controller
 
         $text = "Appointment Cancelled!\n\n" .
                 "Dear " . $doctorserial->name . ", we are sorry to inform you that, your appointment with " . $doctorserial->doctor->name . " on " . date('d-m-Y', strtotime($doctorserial->serialdate)) . " has been cancelled unfortunately.\n\n" .
-                "Infoline - BD Smart Seba";
+                "SmartBD - ২৪/৭ ই-সেবা";
         
         // NEW PANEL
         $url = config('sms.url');
