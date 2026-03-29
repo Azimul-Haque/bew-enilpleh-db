3d6354ee13f72eb654be801bd5587af42e9f76f5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-03-30 02:55:00 +0600

    Commit Mon 03/30/2026  2:55:00.26 19547

--- resources/views/dashboard/doctors/doctorserials.blade.php
+++ resources/views/dashboard/doctors/doctorserials.blade.php
@@ -33,10 +33,14 @@
                   </a>
                   <button type="button" class="btn btn-warning btn-sm" style="margin-left: 5px;" title="সকলকে ক্যানসেল মেসেজ পাঠান" data-toggle="modal" data-target="#sendCancelSMSALLModal">
                    <i class="fas fa-envelope"></i> ক্যানসেল মেসেজ
-                 </button>
-                 <button type="button" class="btn btn-primary btn-sm" style="margin-left: 5px;" title="নতুন সিরিয়াল যোগ করুন" data-toggle="modal" data-target="#addNewAppointmentModal">
-                   <i class="fas fa-user-plus"></i> সিরিয়াল যোগ
-                 </button>
+                  </button>
+
+                  <button type="button" class="btn btn-warning btn-sm" style="margin-left: 5px;" title="সকলকে ক্যানসেল মেসেজ পাঠান" data-toggle="modal" data-target="#sendCancelSMSALLModal">
+                   <i class="fas fa-envelope"></i> ক্যানসেল মেসেজ
+                  </button>
+                   <button type="button" class="btn btn-primary btn-sm" style="margin-left: 5px;" title="নতুন সিরিয়াল যোগ করুন" data-toggle="modal" data-target="#addNewAppointmentModal">
+                     <i class="fas fa-user-plus"></i> সিরিয়াল যোগ
+                   </button>
                 </form>
                 
               </div>
