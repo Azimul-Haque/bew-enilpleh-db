990154a443cb299642ea97dadca1a8b72129b035
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-29 23:42:45 +0600

    Commit Mon 06/29/2026 23:42:45.26 25712

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -46,16 +46,12 @@
 @endif
 
 @if(Auth::user()->role == 'admin' || in_array('doctors', Auth::user()->accessibleTables()) || in_array('hospitals', Auth::user()->accessibleTables()))
-<div class="col-md-3">
-  <a href="{{ route('dashboard.doctors') }}" class="info-box mb-3">
-    <span class="info-box-icon bg-success elevation-1"><i class="fas fa-user-md"></i></span>
-
-    <div class="info-box-content">
-      <span class="info-box-text">ডাক্তার তালিকা</span>
-      <small class="info-box-text" style="margin-top: 10px;">ক্লিক করুন</small>
-    </div>
-  </a>
-</div>
+<li class="nav-item">
+    <a href="{{ route('dashboard.doctors') }}" class="nav-link {{ Request::is('dashboard/doctors') ? 'active' : '' }} {{ Request::is('dashboard/doctors/*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-user-md"></i>
+        <p>ডাক্তার তালিকা</p>
+    </a>
+</li>
 @endif
 
 {{-- @if(Auth::user()->role == 'admin' || in_array('blooddonors', Auth::user()->accessibleTables())) --}}
