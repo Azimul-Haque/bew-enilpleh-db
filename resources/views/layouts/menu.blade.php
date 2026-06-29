d2c47fb1753c8c9262c7719cb2d5b35af970f63c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-29 23:31:55 +0600

    Commit Mon 06/29/2026 23:31:55.26 27931

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -47,6 +47,14 @@
 @else
 {{-- doctor role এর জন্য --}}
 {{-- doctor role এর জন্য --}}
+@if(Auth::user()->role == 'manager')
+<li class="nav-item">
+    <a href="{{ route('dashboard.doctors') }}" class="nav-link {{ Request::is('dashboard/doctors') ? 'active' : '' }} {{ Request::is('dashboard/doctors/*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-user-md"></i>
+        <p>ডাক্তার তালিকা</p>
+    </a>
+</li>
+@endif
 <li class="nav-item">
     <a href="{{ route('dashboard.doctors.chambers', Auth::user()->accessibleDoctors()->first()->id) }}" class="nav-link {{ Request::is('dashboard/chambers') ? 'active' : '' }} {{ Request::is('dashboard/chambers/*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-hospital"></i>
