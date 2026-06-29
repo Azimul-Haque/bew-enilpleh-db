18dab94e44032695899b2ca5adbc3be94e64951a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-06-29 23:39:46 +0600

    Commit Mon 06/29/2026 23:39:46.22 22972

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -134,12 +134,14 @@
     </a>
 </li>
 @elseif(Auth::user()->role == 'editor' || Auth::user()->role == 'manager')
-<li class="nav-item">
-    <a href="{{ route('dashboard.coachings.singleforeditor') }}" class="nav-link {{ Request::is('dashboard/coachings') ? 'active' : '' }} {{ Request::is('dashboard/coachings/*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-chalkboard-teacher"></i>
-        <p>শিক্ষা প্রতিষ্ঠান</p>
-    </a>
-</li>
+  @if(in_array('coachings', Auth::user()->accessibleTables()))
+  <li class="nav-item">
+      <a href="{{ route('dashboard.coachings.singleforeditor') }}" class="nav-link {{ Request::is('dashboard/coachings') ? 'active' : '' }} {{ Request::is('dashboard/coachings/*') ? 'active' : '' }}">
+          <i class="nav-icon fas fa-chalkboard-teacher"></i>
+          <p>শিক্ষা প্রতিষ্ঠান</p>
+      </a>
+  </li>
+  @endif
 @endif
 
 
